c61e3e1da6655d0f7f9cdbab221b62f14cbb8002
robrollenberg <rob@jepr.nl>
2017-12-01 11:23:04 +0100

    Public js verwijderd en .gitignore aangepast

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>ECO</title>
+    <title>Econobis</title>
 
 </head>
 <body>
 <div id="root"></div>
-<script type="text/javascript" src="js/manifest.b5c2d0bb2ac1262a0b6e.js"></script>
-<script type="text/javascript" src="js/vendor.81a1f099ab2fe082e4d3.js"></script>
-<script type="text/javascript" src="js/bundle.02e5f8a4bcf7881726ab.js"></script>
-</body>
+<script type="text/javascript" src="../../public/js/manifest.0cdd696d22d570b89bfd.js"></script><script type="text/javascript" src="../../public/js/vendor.c0cba677b165fbf58da9.js"></script><script type="text/javascript" src="../../public/js/bundle.1cb1ed0007d782025a15.js"></script></body>
 </html>

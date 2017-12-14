918afbadd1c98f4977958255da1385467f501957
Jos Kolenberg <joskolenberg@gmail.com>
2017-12-14 14:33:06 +0100

    Audit trail aan attachments en properties van Tasks toegevoegd

--- app/Eco/Task/TaskAttachment.php
+++ app/Eco/Task/TaskAttachment.php
@@ -4,11 +4,12 @@ namespace App\Eco\Task;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Venturecraft\Revisionable\RevisionableTrait;
 
 class TaskAttachment extends Model
 {
 
-    use SoftDeletes;
+    use SoftDeletes, RevisionableTrait;
 
     public function task()
     {
--- app/Eco/Task/TaskPropertyValue.php
+++ app/Eco/Task/TaskPropertyValue.php
@@ -4,10 +4,11 @@ namespace App\Eco\Task;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Venturecraft\Revisionable\RevisionableTrait;
 
 class TaskPropertyValue extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, RevisionableTrait;
 
     protected $guarded = ['id'];
 

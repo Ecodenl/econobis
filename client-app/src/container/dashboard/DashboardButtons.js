78b0197b662b23c4c1ccc6575734bf18da29cd99
Fren <fren.dehaan@xaris.nl>
2017-12-20 11:35:40 +0100

    onclick dashboard buttons

--- client-app/src/container/dashboard/DashboardButtons.js
+++ client-app/src/container/dashboard/DashboardButtons.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { hashHistory } from 'react-router';
 
 import OpportunityAPI from './../../api/OpportunityAPI';
 import TaskAPI from './../../api/task/TasksAPI';
@@ -28,7 +29,7 @@ class DashboardButtons extends Component {
     render() {
         return (
             <div className="row">
-                <div className="col-md-3">
+                <div className="col-md-3" onClick={() => hashHistory.push(`/emails`)}>
                     <div className="panel panel-default" id="dashboardbutton-red">
                         <div className="panel-body">
                             <h4 className="text-center text-bold">EMAIL</h4>
@@ -36,7 +37,7 @@ class DashboardButtons extends Component {
                         </div>
                     </div>
                 </div>
-                <div className="col-md-3">
+                <div className="col-md-3" onClick={() => hashHistory.push(`/aanmeldingen`)}>
                     <div className="panel panel-default" id="dashboardbutton-blue">
                         <div className="panel-body">
                             <h4 className="text-center text-bold">AANMELDINGEN</h4>
@@ -44,7 +45,7 @@ class DashboardButtons extends Component {
                         </div>
                     </div>
                 </div>
-                <div className="col-md-3">
+                <div className="col-md-3" onClick={() => hashHistory.push(`/kansen`)}>
                     <div className="panel panel-default" id="dashboardbutton-green">
                         <div className="panel-body">
                             <h4 className="text-center text-bold">KANSEN</h4>
@@ -52,7 +53,7 @@ class DashboardButtons extends Component {
                         </div>
                     </div>
                 </div>
-                <div className="col-md-3">
+                <div className="col-md-3" onClick={() => hashHistory.push(`/taken`)}>
                     <div className="panel panel-default" id="dashboardbutton-yellow">
                         <div className="panel-body">
                             <h4 className="text-center text-bold">TAKEN</h4>

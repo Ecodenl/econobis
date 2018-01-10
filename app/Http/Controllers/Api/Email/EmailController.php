46cc2d8c5773408d0650cc30ec92e179827bedc4
Fren <fren.dehaan@xaris.nl>
2018-01-10 15:31:14 +0100

    Fix for empty string in array

--- app/Http/Controllers/Api/Email/EmailController.php
+++ app/Http/Controllers/Api/Email/EmailController.php
@@ -97,13 +97,13 @@ class EmailController
             array_push($sendVariations, 'cc');
         }
         else{
-            $emails['cc'][0] = '';
+            $emails['cc'] = [];
         }
         if ($data['bcc'] != '') {
             array_push($sendVariations, 'bcc');
         }
         else{
-            $emails['bcc'][0] = '';
+            $emails['bcc'] = [];
         }
 
         foreach ($sendVariations as $sendVariation){
@@ -178,13 +178,13 @@ class EmailController
             array_push($sendVariations, 'cc');
         }
         else{
-            $emails['cc'][0] = '';
+            $emails['cc'] = [];
         }
         if ($data['bcc'] != '') {
             array_push($sendVariations, 'bcc');
         }
         else{
-            $emails['bcc'][0] = '';
+            $emails['bcc'] = [];
         }
 
         foreach ($sendVariations as $sendVariation){

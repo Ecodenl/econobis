6045f4786eb259f085746b542569f020853fc5a9
Fren <fren.dehaan@xaris.nl>
2018-01-29 14:45:47 +0100

    remove empty items in contact new

--- client-app/src/container/contact/new/ContactNewApp.js
+++ client-app/src/container/contact/new/ContactNewApp.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 import ContactNewToolbar from './ContactNewToolbar';
 import ContactNewForm from './ContactNewForm';
-import Panel from '../../../components/panel/Panel';
-import PanelBody from '../../../components/panel/PanelBody';
 
 const ContactNewApp = props => {
     return (
@@ -17,31 +15,6 @@ const ContactNewApp = props => {
                     <ContactNewForm type={props.params.type} organisationId={props.params.id}/>
                 </div>
             </div>
-            <Panel className="col-md-3">
-                <PanelBody>
-                    <div className="col-md-12 margin-10-top">
-                        Buttons met openstaande items, zoals email, taken, etc.
-                    </div>
-
-                    <div className="col-md-12 margin-10-top">
-                        <div className="panel panel-default harmonica-button">
-                            <div className="panel-body">
-                                <p className="text-center">AANMELDINGEN <span className="badge">0</span></p>
-                            </div>
-                        </div>
-                        <div className="panel panel-default harmonica-button">
-                            <div className="panel-body">
-                                <p className="text-center">KANSEN <span className="badge">0</span></p>
-                            </div>
-                        </div>
-                        <div className="panel panel-default harmonica-button">
-                            <div className="panel-body">
-                                <p className="text-center">TAKEN <span className="badge">0</span></p>
-                            </div>
-                        </div>
-                    </div>
-                </PanelBody>
-            </Panel>
         </div>
     )
 };

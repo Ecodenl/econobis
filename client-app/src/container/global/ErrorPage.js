a1480d98277d50cae82389918da5db7ef5162eea
Rob Rollenberg <rob@jepr.nl>
2018-01-29 09:39:04 +0100

    - Link toegevoegd om uit te loggen bij niet laden van econobis

--- client-app/src/container/global/ErrorPage.js
+++ client-app/src/container/global/ErrorPage.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { hashHistory } from 'react-router';
 
 const ErrorPage = () => (
     <div className="loader">
         <h1>Oeps...</h1>
-        <h4>...er is helaas iets mis gegaan. Probeer de pagina <a role="button" onClick={() => {location.reload();}}>opnieuw</a> te laden.</h4>
+        <h3>...er is helaas iets mis gegaan.</h3>
+        <p className={"h4"}>Probeer de pagina <a role="button" onClick={() => {location.reload();}}>opnieuw</a> te laden.</p>
+        <p className={"h4"}>Of log <a role="button" onClick={() => {hashHistory.push('loguit');location.reload();}}> opnieuw</a> in.</p>
     </div>
 );
 

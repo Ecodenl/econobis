db9a3777173437d36876bda8b5dde6bcd13d5308
Rob Rollenberg <rob@jepr.nl>
2018-02-15 15:28:30 +0100

    Input Toggle toegevoegd

---
+++ client-app/src/components/form/InputToggle.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Toggle from 'react-toggle'
+
+const InputToggle = props => {
+    const { label, size, id, name, value, onChangeAction, required } = props;
+
+    return (
+        <div className="form-group col-sm-6">
+            <div><label htmlFor={ id } className={`col-sm-6 ${required}`}>{ label }</label></div>
+            <div className={`${size}`}>
+                <Toggle
+                    defaultChecked={value}
+                    name={name}
+                    value={value.toString()}
+                    onChange={onChangeAction}
+                />
+            </div>
+        </div>
+    );
+};
+
+InputToggle.defaultProps = {
+    className: '',
+    size: 'col-sm-6',
+    required: '',
+    readOnly: false,
+    value: null,
+};
+
+InputToggle.propTypes = {
+    label: PropTypes.string.isRequired,
+    type: PropTypes.string,
+    size: PropTypes.string,
+    id: PropTypes.string,
+    name: PropTypes.string.isRequired,
+    value: PropTypes.bool,
+    onChangeAction: PropTypes.func,
+    required: PropTypes.string,
+    error: PropTypes.bool,
+};
+
+export default InputToggle;

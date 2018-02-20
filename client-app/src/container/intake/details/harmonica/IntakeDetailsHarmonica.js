3c31c9a6cc4f6d880b66bb774ba3840885a23915
Rob Rollenberg <rob@jepr.nl>
2018-02-20 09:38:31 +0100

    Email harmonica toegevoegd aan intake

--- client-app/src/container/intake/details/harmonica/IntakeDetailsHarmonica.js
+++ client-app/src/container/intake/details/harmonica/IntakeDetailsHarmonica.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import TaskHarmonica from "./TaskHarmonica";
 import NoteHarmonica from "./NoteHarmonica";
 import DocumentHarmonica from "./DocumentHarmonica";
+import EmailHarmonica from "./EmailHarmonica";
 
 class IntakeDetailsHarmonica extends Component {
     constructor(props){
@@ -16,11 +17,13 @@ class IntakeDetailsHarmonica extends Component {
                 documents: false,
                 tasks: false,
                 notes: false,
+                emails: false,
             },
         };
 
         this.newTask = this.newTask.bind(this);
         this.newDocument = this.newDocument.bind(this);
+        this.newEmail = this.newEmail.bind(this);
         this.toggleShowList = this.toggleShowList.bind(this);
     };
 
@@ -30,6 +33,10 @@ class IntakeDetailsHarmonica extends Component {
 
     newDocument(type) {
         hashHistory.push(`/document/nieuw/${type}/intake/${this.props.id}`);
+    };
+
+    newEmail() {
+        hashHistory.push(`/email/nieuw/intake/${this.props.id}`);
     };
 
     toggleShowList(name) {
@@ -62,6 +69,12 @@ class IntakeDetailsHarmonica extends Component {
                     showDocumentsList={this.state.toggleShowList.documents}
                     newDocument={this.newDocument}
                     documentCount={this.props.intakeDetails.documentCount}
+                />
+                <EmailHarmonica
+                    toggleShowList={() => this.toggleShowList('emails')}
+                    showEmailsList={this.state.toggleShowList.emails}
+                    newEmail={this.newEmail}
+                    emailCount={this.props.intakeDetails.emailCount}
                 />
             </div>
         )

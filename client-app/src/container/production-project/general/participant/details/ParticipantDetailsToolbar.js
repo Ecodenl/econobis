5e4d4fe110b432ca44a275b981d1edf4d080fb99
Fren <fren.dehaan@xaris.nl>
2018-03-06 11:30:19 +0100

    Fix is transferable participations

--- client-app/src/container/production-project/general/participant/details/ParticipantDetailsToolbar.js
+++ client-app/src/container/production-project/general/participant/details/ParticipantDetailsToolbar.js
@@ -26,7 +26,7 @@ class ParticipantDetailsToolbar extends Component {
     render() {
         const { participantProductionProject, productionProject = {} }  = this.props;
 
-        const isTranferable = (productionProject.isParticipationTransferable && (participantProductionProject.participationsCurrent !== 0) && (participantProductionProject.participationsCurrent) && this.props.permissions.manageFinancial);
+        const isTransferable = (productionProject.isParticipationTransferable && (participantProductionProject.participationsCurrent > 0) && (participantProductionProject.participationsCurrent) && this.props.permissions.manageFinancial);
 
         return (
             <div className="row">
@@ -40,7 +40,7 @@ class ParticipantDetailsToolbar extends Component {
                                     {this.props.permissions.manageParticipation &&
                                     <ButtonIcon iconName={"glyphicon-trash"} onClickAction={this.toggleDelete}/>
                                     }
-                                    {isTranferable ?
+                                    {isTransferable ?
                                         <ButtonText buttonText={`Participaties overdragen`}
                                                     onClickAction={() => hashHistory.push(`/productie-project/participant/${participantProductionProject.id}/overdragen`)}/>
                                         :

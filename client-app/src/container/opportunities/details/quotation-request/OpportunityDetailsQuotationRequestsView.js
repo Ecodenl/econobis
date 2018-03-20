384cef3cd8113e35b1bc5993304ff9dda3938706
Fren <fren.dehaan@xaris.nl>
2018-03-20 15:42:42 +0100

    Double click for quotationrequest at opportunities

--- client-app/src/container/opportunities/details/quotation-request/OpportunityDetailsQuotationRequestsView.js
+++ client-app/src/container/opportunities/details/quotation-request/OpportunityDetailsQuotationRequestsView.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import moment from 'moment';
 import {connect} from "react-redux";
+import {hashHistory} from "react-router";
 
 const OpportunityDetailsQuotationRequestsView = props => {
-    const {organisation, createdAt, dateRecorded, status, dateReleased, dateValid} = props.quotationRequest;
+    const {id, organisation, createdAt, dateRecorded, status, dateReleased, dateValid} = props.quotationRequest;
 
     return (
 
         <div className={`row border ${props.highlightLine}`} onMouseEnter={() => props.onLineEnter()}
-             onMouseLeave={() => props.onLineLeave()}>
+             onMouseLeave={() => props.onLineLeave()} onDoubleClick={() => hashHistory.push(`/offerteverzoek/${id}`)}>
             <div className="col-sm-2">{organisation && organisation.name}</div>
             <div className="col-sm-2">{createdAt ? moment(createdAt.date).format('L') : ''}</div>
             <div className="col-sm-2">{dateRecorded ? moment(dateRecorded).format('L') : ''}</div>

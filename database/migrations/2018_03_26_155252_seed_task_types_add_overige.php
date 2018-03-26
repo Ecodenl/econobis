7492c509a2bf4b68bd387e03160bc0771052590b
Fren <fren.dehaan@xaris.nl>
2018-03-26 15:53:49 +0200

    Add overige task type

---
+++ database/migrations/2018_03_26_155252_seed_task_types_add_overige.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class SeedTaskTypesAddOverige extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('task_types', function (Blueprint $table) {
+            $types = [
+                'Overige',
+            ];
+
+            foreach ($types as $type) {
+                DB::table('task_types')->insert([
+                        ['name' => $type],
+                    ]
+                );
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+}

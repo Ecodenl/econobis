cc4aa3693f87ccd25476989f584676cfe7a5140f
Fren <fren.dehaan@xaris.nl>
2018-03-26 16:11:07 +0200

    Order by name occupations contact

--- app/Eco/Contact/Contact.php
+++ app/Eco/Contact/Contact.php
@@ -199,12 +199,12 @@ class Contact extends Model
 
     public function primaryOccupations()
     {
-        return $this->hasMany(OccupationContact::class, 'primary_contact_id');
+        return $this->hasMany(OccupationContact::class, 'primary_contact_id')->join('contacts', 'contact_id', '=', 'contacts.id')->orderBy('contacts.full_name');
     }
 
     public function occupations()
     {
-        return $this->hasMany(OccupationContact::class);
+        return $this->hasMany(OccupationContact::class)->join('contacts', 'primary_contact_id', '=', 'contacts.id')->orderBy('contacts.full_name');
     }
 
     public function contactPerson()

de18404869bc48221060da0384a58f599f48366a
Fren <fren.dehaan@xaris.nl>
2018-04-09 15:19:57 +0200

    test remove contact

--- client-app/testcafe/pages/model-general.js
+++ client-app/testcafe/pages/model-general.js
@@ -8,5 +8,7 @@ export default class ModelGeneral {
         this.firstRow = Selector('tbody').nth(0).child('tr').nth(0);
         this.option = Selector('option');
         this.back = Selector('span.glyphicon.glyphicon-arrow-left');
+        this.delete = Selector('span.glyphicon.glyphicon-trash');
+        this.deleteConfirm = Selector('button').withExactText('Verwijder');
     }
 }

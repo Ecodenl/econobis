f49b0a1b6adfc79d9a5c52973df77b57418309fe
Fren <fren.dehaan@xaris.nl>
2018-05-08 12:37:36 +0200

    Migrations for invoices

---
+++ database/migrations/2018_05_08_122522_create_invoices_tables.php
@@ -0,0 +1,113 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInvoicesTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('invoice_number');
+            $table->string('number');
+
+            $table->unsignedInteger('order_id');
+            $table->foreign('order_id')
+                ->references('id')->on('orders')
+                ->onDelete('restrict');
+
+            $table->string('payment_type_id');
+            $table->string('status_id');
+            $table->string('mollie_status_id')->default('not_used');
+            $table->string('send_method_id');
+
+            $table->date('date_collection')->nullable();
+            $table->date('date_reminder_1')->nullable();
+            $table->date('date_reminder_2')->nullable();
+            $table->date('date_reminder_3')->nullable();
+            $table->date('date_exhortation')->nullable();
+            $table->date('date_requested')->nullable();
+
+            $table->unsignedInteger('created_by_id');
+            $table->foreign('created_by_id')
+                ->references('id')->on('users')
+                ->onDelete('restrict');
+
+            $table->softdeletes();
+            $table->timestamps();
+        });
+
+
+        Schema::create('invoice_product', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->unsignedInteger('product_id');
+            $table->foreign('product_id')
+                ->references('id')->on('products')
+                ->onDelete('restrict');
+
+            $table->unsignedInteger('invoice_id');
+            $table->foreign('invoice_id')
+                ->references('id')->on('invoices')
+                ->onDelete('restrict');
+
+            $table->integer('amount');
+            $table->float('amount_reduction')->nullable();
+            $table->float('percentage_reduction')->nullable();
+            $table->float('price');
+            $table->integer('vat_percentage')->nullable();
+
+            $table->timestamps();
+        });
+
+        Schema::create('invoice_document', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->unsignedInteger('invoice_id');
+            $table->foreign('invoice_id')
+                ->references('id')->on('invoices')
+                ->onDelete('restrict');
+
+            $table->string('filename');
+            $table->string('name');
+
+            $table->timestamps();
+        });
+
+        Schema::create('invoice_payment', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->unsignedInteger('invoice_id');
+            $table->foreign('invoice_id')
+                ->references('id')->on('invoices')
+                ->onDelete('restrict');
+
+            $table->float('amount');
+            $table->string('type_id');
+
+            $table->date('date_paid');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoice_payment');
+        Schema::dropIfExists('invoice_document');
+        Schema::dropIfExists('invoice_product');
+        Schema::dropIfExists('invoices');
+    }
+}

8fd1a84e70cd4e650c1ec4e29c9e9f925a8ac8b0
Fren <fren.dehaan@xaris.nl>
2018-05-14 13:38:22 +0200

    Mail invoice reminder/exhortation

--- app/Helpers/Invoice/InvoiceHelper.php
+++ app/Helpers/Invoice/InvoiceHelper.php
@@ -8,13 +8,19 @@
 
 namespace App\Helpers\Invoice;
 
+use App\Eco\EmailTemplate\EmailTemplate;
 use App\Eco\Invoice\Invoice;
 use App\Eco\Invoice\InvoiceDocument;
 use App\Eco\Invoice\InvoicePayment;
 use App\Eco\Invoice\InvoiceProduct;
 use App\Eco\Order\Order;
+use App\Http\Controllers\Api\Order\OrderController;
+use App\Http\Resources\Email\Templates\GenericMail;
+use App\Http\Resources\Invoice\Templates\InvoiceMail;
+use App\Http\Resources\ParticipantProductionProject\Templates\ParticipantReportMail;
 use Barryvdh\DomPDF\Facade as PDF;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Storage;
 
 class InvoiceHelper
@@ -68,8 +74,39 @@ class InvoiceHelper
 
     public static function send(Invoice $invoice){
         if($invoice->send_method_id === 'mail') {
-            //todo
+            $orderController = new OrderController();
+            $contactInfo = $orderController->getContactInfoForOrder($invoice->order->contact);
+
+            if($contactInfo['email'] === 'Geen e-mail bekend'){
+                return false;
+            }
+
+            $subject = 'Factuur';
+            $htmlBody = 'Beste ' . $contactInfo['contactPerson'] . ',';
+            $htmlBody .= '<p>&nbsp;</p>';
+            $htmlBody .= 'Hierbij uw factuur: ' . $invoice->number . '.';
+            $htmlBody .= '<p>&nbsp;</p>';
+            $htmlBody .= 'Met vriendelijke groet,';
+            $htmlBody .= '<p></p>';
+            $htmlBody .= $invoice->administration->name;
+
+            if($invoice->order->emailTemplate){
+                $subject = $invoice->order->emailTemplate->subject ? $invoice->order->emailTemplate->subject : $subject;
+                $htmlBody = $invoice->order->emailTemplate->html_body;
+
+            }
+
+            $htmlBody = '<!DOCTYPE html><html><head><meta http-equiv="content-type" content="text/html;charset=UTF-8"/><title>'
+                . $subject . '</title></head>'
+                . $htmlBody . '</html>';
         }
+
+        $mail = Mail::to($contactInfo['email']);
+
+        $mail->subject = $subject;
+        $mail->html_body = $htmlBody;
+
+        $mail->send(new InvoiceMail($mail, $htmlBody, Storage::disk('administrations')->getDriver()->getAdapter()->applyPathPrefix($invoice->document->filename), $invoice->document->name));
 
         $invoice->status_id = 'sent';
         $invoice->date_sent = Carbon::today();;
@@ -79,24 +116,65 @@ class InvoiceHelper
     }
 
     public static function sendNotification(Invoice $invoice){
-        //todo
 
         if($invoice->date_reminder_3){
+            InvoiceHelper::sendNotificationEmail($invoice->order->emailTemplateExhortation, $invoice);
             $invoice->date_exhortation = Carbon::today();
         }
         elseif($invoice->date_reminder_2){
+            InvoiceHelper::sendNotificationEmail($invoice->order->emailTemplateReminder, $invoice);
             $invoice->date_reminder_3 = Carbon::today();
         }
         elseif($invoice->date_reminder_1){
+            InvoiceHelper::sendNotificationEmail($invoice->order->emailTemplateReminder, $invoice);
             $invoice->date_reminder_2 = Carbon::today();
         }
         else{
+            InvoiceHelper::sendNotificationEmail($invoice->order->emailTemplateReminder, $invoice);
             $invoice->date_reminder_1 = Carbon::today();
         }
 
         $invoice->save();
 
         return $invoice;
+    }
+
+    public static function sendNotificationEmail(EmailTemplate $emailTemplate = null, Invoice $invoice){
+        $orderController = new OrderController();
+        $contactInfo = $orderController->getContactInfoForOrder($invoice->order->contact);
+
+        if($contactInfo['email'] === 'Geen e-mail bekend'){
+            return false;
+        }
+
+        $mail = Mail::to($contactInfo['email']);
+
+        $subject = 'Betalingsherinnering';
+
+        if(!$emailTemplate){
+            $htmlBody = 'Beste ' . $contactInfo['contactPerson'] . ',';
+            $htmlBody .= '<p>&nbsp;</p>';
+            $htmlBody .= 'Uw heeft nog een openstaand factuur: ' . $invoice->number . '.';
+            $htmlBody .= '<p>&nbsp;</p>';
+            $htmlBody .= 'Met vriendelijke groet,';
+            $htmlBody .= '<p></p>';
+            $htmlBody .= $invoice->administration->name;
+        }
+        else{
+            $subject = $emailTemplate->subject ? $emailTemplate->subject : $subject;
+            $htmlBody = $emailTemplate->html_body;
+        }
+
+        $htmlBody = '<!DOCTYPE html><html><head><meta http-equiv="content-type" content="text/html;charset=UTF-8"/><title>'
+            . $subject . '</title></head>'
+            . $htmlBody . '</html>';
+
+        $mail->subject = $subject;
+        $mail->html_body = $htmlBody;
+
+        $mail->send(new InvoiceMail($mail, $htmlBody, Storage::disk('administrations')->getDriver()->getAdapter()->applyPathPrefix($invoice->document->filename), $invoice->document->name));
+
+        return true;
     }
 
     public static function createInvoiceDocument(Invoice $invoice){

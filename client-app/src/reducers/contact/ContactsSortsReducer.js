57b244791a3ccd10be3c6a14c2e95f0a7aeda613
Fren <fren.dehaan@xaris.nl>
2018-05-25 10:21:27 +0200

    Default created at filter for contacts

--- client-app/src/reducers/contact/ContactsSortsReducer.js
+++ client-app/src/reducers/contact/ContactsSortsReducer.js
@@ -1,16 +1,19 @@
-export default (state = [], action) => {
-    switch(action.type) {
+export default (state = [{
+    field: 'createdAt',
+    order: 'desc',
+}], action) => {
+    switch (action.type) {
         case 'SET_CONTACTS_SORTS_FILTER':
-            if(state.length === 3) state.shift();
+            if (state.length === 3) state.shift();
 
-        return [
-            ...state,
-            {
-                field: action.field,
-                order: action.order,
-            },
-        ];
-    default:
-        return state;
+            return [
+                ...state,
+                {
+                    field: action.field,
+                    order: action.order,
+                },
+            ];
+        default:
+            return state;
     }
 };

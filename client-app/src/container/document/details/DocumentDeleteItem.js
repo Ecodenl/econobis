ba09e44906d29003f004cf5fe960688311771e8f
Fren <fren.dehaan@xaris.nl>
2018-06-04 12:20:31 +0200

    Delete document from details

---
+++ client-app/src/container/document/details/DocumentDeleteItem.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Modal from '../../../components/modal/Modal';
+import DocumentDetailsAPI from '../../../api/document/DocumentDetailsAPI';
+import {hashHistory} from "react-router";
+
+const DocumentDeleteItem = (props) => {
+    const confirmAction = () => {
+        DocumentDetailsAPI.deleteDocument(props.id).then((payload) => {
+            hashHistory.push(`/documenten`);
+        });
+    };
+
+    return (
+        <Modal
+            buttonConfirmText="Verwijder"
+            buttonClassName={'btn-danger'}
+            closeModal={props.closeDeleteItemModal}
+            confirmAction={() => confirmAction()}
+            title="Verwijderen"
+        >
+            Verwijder document: <strong> { props.filename } </strong>
+        </Modal>
+    );
+};
+
+export default DocumentDeleteItem;
--- client-app/src/container/document/details/DocumentDetailsToolbar.js
+++ client-app/src/container/document/details/DocumentDetailsToolbar.js
@@ -1,33 +1,63 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import {browserHistory, hashHistory} from 'react-router';
 
 import ButtonIcon from '../../../components/button/ButtonIcon';
-import { Link } from 'react-router';
+import {Link} from 'react-router';
+import DocumentDeleteItem from "./DocumentDeleteItem";
 
-const DocumentDetailsToolbar = props => {
-    const {documentFilename = ''} = props;
-    return (
-        <div className="row">
-            <div className="col-md-4">
-                <div className="btn-group" role="group">
-                    <ButtonIcon iconName={"glyphicon-arrow-left"} onClickAction={browserHistory.goBack} />
-                    <ButtonIcon iconName={"glyphicon-download-alt"} onClickAction={props.download} />
-                    {documentFilename.endsWith('.pdf') &&
-                    <ButtonIcon iconName={"glyphicon-eye-open"} onClickAction={() => hashHistory.push(`/document/inzien/${props.documentId}`)} />
-                    }
-                    <ButtonIcon iconName={"glyphicon-envelope"} onClickAction={() => hashHistory.push(`/email/nieuw/document/${props.documentId}`)} />
+class DocumentDetailsToolbar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            showDelete: false,
+        };
+    }
+
+    toggleDelete = () => {
+        this.setState({showDelete: !this.state.showDelete});
+    };
+
+    render() {
+        const {documentFilename = ''} = this.props;
+
+        return (
+            <div className="row">
+                <div className="col-md-4">
+                    <div className="btn-group" role="group">
+                        <ButtonIcon iconName={"glyphicon-arrow-left"} onClickAction={browserHistory.goBack}/>
+                        <ButtonIcon iconName={"glyphicon-download-alt"} onClickAction={this.props.download}/>
+                        {documentFilename.endsWith('.pdf') &&
+                        <ButtonIcon iconName={"glyphicon-eye-open"}
+                                    onClickAction={() => hashHistory.push(`/document/inzien/${this.props.documentId}`)}/>
+                        }
+                        <ButtonIcon iconName={"glyphicon-envelope"}
+                                    onClickAction={() => hashHistory.push(`/email/nieuw/document/${this.props.documentId}`)}/>
+                        <ButtonIcon iconName={"glyphicon-trash"} onClickAction={this.toggleDelete}/>
+                    </div>
                 </div>
+                {documentFilename.endsWith('.pdf') ?
+                    <div className="col-md-4"><h4 className="text-center">{'Document: '} <Link
+                        to={`/document/inzien/${this.props.documentId}`} className="link-underline">{documentFilename}</Link>
+                    </h4></div>
+                    :
+                    <div className="col-md-4"><h4 className="text-center">{'Document: ' + documentFilename}</h4></div>
+                }
+                <div className="col-md-4"/>
+                {
+                    this.state.showDelete &&
+                <DocumentDeleteItem
+                    id={this.props.documentId}
+                    filename={documentFilename}
+                    closeDeleteItemModal={this.toggleDelete}
+                />
+                }
+
             </div>
-            {documentFilename.endsWith('.pdf') ?
-                <div className="col-md-4"><h4 className="text-center">{'Document: '}  <Link to={`/document/inzien/${props.documentId}`} className="link-underline">{documentFilename}</Link></h4></div>
-                :
-                <div className="col-md-4"><h4 className="text-center">{'Document: ' + documentFilename}</h4></div>
-            }
-            <div className="col-md-4" />
-        </div>
-    );
-};
+        );
+    };
+}
 
 const mapStateToProps = (state) => {
     return {

f4bf9401258768c35b5f83c2f300ffdb606452c1
Fren <fren.dehaan@xaris.nl>
2018-08-28 08:21:24 +0200

    Update participant export

---
+++ app/Helpers/CSV/Export.php
@@ -0,0 +1,143 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: StagiarSoftware
+ * Date: 27-8-2018
+ * Time: 16:48
+ */
+
+namespace App\Helpers\CSV;
+
+use League\Csv\Writer;
+use SplTempFileObject;
+use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Collection;
+use League\Csv\AbstractCsv as LeagueCsvWriter;
+
+class Export
+{
+    /**
+     * The applied callback.
+     *
+     * @var callable|null
+     */
+    protected $beforeEachCallback;
+
+    /**
+     * The CSV writer.
+     *
+     * @var \League\Csv\Writer
+     */
+    protected $csv;
+
+    /**
+     * Export constructor.
+     *
+     * @param \League\Csv\AbstractCsv|null $writer
+     *
+     * @return void
+     */
+    public function __construct(LeagueCsvWriter $writer = null)
+    {
+        $this->csv = $writer ?: Writer::createFromFileObject(new SplTempFileObject);
+    }
+
+    /**
+     * Build the writer.
+     *
+     * @param \Illuminate\Database\Eloquent\Collection $collection
+     * @param array                                    $fields
+     *
+     * @return $this
+     */
+    public function build($collection, array $fields, $showHeaders)
+    {
+        $csv = $this->csv;
+        $headers = [];
+
+        foreach ($fields as $key => $field) {
+            $headers[] = $field;
+
+            if (!is_numeric($key)) {
+                $fields[$key] = $key;
+            }
+        }
+        if ($showHeaders) {
+            // Add first line, the header
+            $csv->insertOne($headers);
+        }
+        $this->addCsvRows($collection, $fields, $csv);
+
+        return $this;
+    }
+
+    /**
+     * Download the CSV file.
+     *
+     * @param string|null $filename
+     *
+     * @return void
+     */
+    public function download($filename = null)
+    {
+        $filename = $filename ?: date('Y-m-d_His') . '.csv';
+        $this->csv->output($filename);
+    }
+
+    /**
+     * Set the callback.
+     *
+     * @param callable $callback
+     *
+     * @return $this
+     */
+    public function beforeEach(callable $callback)
+    {
+        $this->beforeEachCallback = $callback;
+        return $this;
+    }
+
+    /**
+     * Get the CSV writer.
+     *
+     * @return \League\Csv\Writer
+     */
+    public function getCsv()
+    {
+        return $this->csv;
+    }
+
+    /**
+     * Add rows to the CSV.
+     *
+     * @param \Illuminate\Database\Eloquent\Collection $collection
+     * @param array                                    $fields
+     * @param \League\Csv\Writer                       $csv
+     *
+     * @return void
+     */
+    private function addCsvRows(Collection $collection, array $fields, Writer $csv)
+    {
+        $collection->makeVisible($fields);
+
+        foreach ($collection as $model) {
+            $beforeEachCallback = $this->beforeEachCallback;
+
+            // Call hook
+            if ($beforeEachCallback) {
+                $return = $beforeEachCallback($model);
+                if ($return === false) {
+                    continue;
+                }
+            }
+
+            $model->toArray();
+            $csvRow = [];
+            foreach ($fields as $field) {
+                $csvRow[] = Arr::get($model, $field);
+            }
+
+            $csv->insertOne($csvRow);
+        }
+    }
+}
--- app/Helpers/CSV/ParticipantCSVHelper.php
+++ app/Helpers/CSV/ParticipantCSVHelper.php
@@ -8,6 +8,7 @@
 
 namespace App\Helpers\CSV;
 
+use App\Eco\Address\AddressType;
 use App\Eco\EmailAddress\EmailAddressType;
 use Carbon\Carbon;
 
@@ -18,7 +19,7 @@ class ParticipantCSVHelper
 
     public function __construct($participants)
     {
-        $this->csvExporter = new \Laracsv\Export();
+        $this->csvExporter = new Export();
         $this->csvExporter->getCsv()->setDelimiter(';');
         $this->participants = $participants;
     }
@@ -26,9 +27,8 @@ class ParticipantCSVHelper
     public function downloadCSV()
     {
         $csv = '';
-
-        foreach ($this->participants->chunk(100) as $chunk) {
-
+        $headers = true;
+        foreach ($this->participants->chunk(500) as $chunk) {
             $chunk->load([
                 'contact.person.title',
                 'contact.organisation',
@@ -45,29 +45,42 @@ class ParticipantCSVHelper
             ]);
 
             foreach ($chunk as $participant) {
-                // E-mail
-                if ($participant->contact->emailAddresses) {
-                    foreach (EmailAddressType::collection() as $type) {
-                        $emailAddresses = $participant->contact->emailAddresses()->where('type_id', $type->id)->get();
-                        $first = true;
-                        foreach ($emailAddresses as $emailAddress) {
-                            if ($first) {
-                                $participant['email_' . $type->id] = ($emailAddress ? $emailAddress->email : '');
-                                $first = false;
-                            } else {
-                                $repParticipant = $participant->replicate();
-                                $repParticipant->id = $participant->id;
-                                $repParticipant['email_' . $type->id] = ($emailAddress ? $emailAddress->email : '');
+                // Addresses
+                if ($participant->contact->addresses) {
+                    foreach (AddressType::collection() as $type) {
+                        $address = $participant->contact->addresses()->where('type_id', $type->id)->first();
 
-                                $index = $this->participants->search(function ($item, $key) use ($participant) {
-                                    return $item->id == $participant->id;
-                                });
-                                $this->participants->splice($index, 0, [$repParticipant]);
-                            }
-                        }
+                        $addressArr = [];
 
+                        $addressArr['street'] = ($address ? $address->street : '');
+                        $addressArr['number'] = ($address ? $address->number : '');
+                        $addressArr['addition'] = ($address ? $address->addition : '');
+                        $addressArr['postal_code'] = ($address ? $address->postal_code : '');
+                        $addressArr['city'] = ($address ? $address->city : '');
+                        $addressArr['country'] = ($address ? $address->country->name : '');
+
+                        $participant['address_' . $type->id] = $addressArr;
                     }
                 }
+
+                // Other e-mail addresses
+                if ($participant->contact->emailAddresses) {
+                    $emailAddresses = $participant->contact->emailAddresses()->where('primary', false)->limit(5)->get();
+
+                    $participant['emailAddress_2'] = (isset($emailAddresses[0]) ? $emailAddresses[0]->email : '');
+                    $participant['emailAddress_3'] = (isset($emailAddresses[1]) ? $emailAddresses[1]->email : '');
+                    $participant['emailAddress_4'] = (isset($emailAddresses[2]) ? $emailAddresses[2]->email : '');
+                    $participant['emailAddress_5'] = (isset($emailAddresses[3]) ? $emailAddresses[3]->email : '');
+                }
+
+                // Other phonenumbers
+                if ($participant->contact->phoneNumbers) {
+                    $phoneNumbers = $participant->contact->phoneNumbers()->where('primary', false)->limit(2)->get();
+
+                    $participant['phonenumber_2'] = (isset($phoneNumbers[0]) ? $phoneNumbers[0]->number : '');
+                    $participant['phonenumber_3'] = (isset($phoneNumbers[1]) ? $phoneNumbers[1]->number : '');
+                }
+
                 $this->csvExporter->beforeEach(function ($participant) {
                     // person/organisation fields
                     if ($participant->contact->type_id === 'person') {
@@ -105,7 +118,7 @@ class ParticipantCSVHelper
                         ->format('d-m-Y')
                         : '';
                 });
-
+            }
                 $csv = $this->csvExporter->build($chunk, [
                     'id' => '#',
                     'contact.id' => 'Contactnummer',
@@ -118,17 +131,38 @@ class ParticipantCSVHelper
                     'last_name_prefix' => 'Tussenvoegsel',
                     'last_name' => 'Achternaam',
                     'date_of_birth' => 'Geboortedatum',
-                    'contact.primaryAddress.street' => 'Primair adres',
-                    'contact.primaryAddress.number' => 'Primair huisnummer',
-                    'contact.primaryAddress.addition' => 'Primair toevoeging',
-                    'contact.primaryAddress.postal_code' => 'Primair postcode',
-                    'contact.primaryAddress.city' => 'Primair plaats',
-                    'contact.primaryAddress.country.name' => 'Primair land',
-                    'email_general' => 'Email algemeen',
-                    'email_home' => 'Email prive',
-                    'email_work' => 'Email werk',
-                    'email_administration' => 'Email administratie',
-                    'email_invoice' => 'Email factuur',
+                    'address_deliver.street' => 'Bezorg adres',
+                    'address_deliver.number' => 'Bezorg huisnummer',
+                    'address_deliver.addition' => 'Bezorg toevoeging',
+                    'address_deliver.postal_code' => 'Bezorg postcode',
+                    'address_deliver.city' => 'Bezorg plaats',
+                    'address_deliver.country' => 'Bezorg land',
+                    'address_visit.street' => 'Bezoek adres',
+                    'address_visit.number' => 'Bezoek huisnummer',
+                    'address_visit.addition' => 'Bezoek toevoeging',
+                    'address_visit.postal_code' => 'Bezoek postcode',
+                    'address_visit.city' => 'Bezoek plaats',
+                    'address_visit.country' => 'Bezoek land',
+                    'address_postal.street' => 'Post adres',
+                    'address_postal.number' => 'Post huisnummer',
+                    'address_postal.addition' => 'Post toevoeging',
+                    'address_postal.postal_code' => 'Post postcode',
+                    'address_postal.city' => 'Post plaats',
+                    'address_postal.country' => 'Post land',
+                    'address_invoice.street' => 'Factuur adres',
+                    'address_invoice.number' => 'Factuur huisnummer',
+                    'address_invoice.addition' => 'Factuur toevoeging',
+                    'address_invoice.postal_code' => 'Factuur postcode',
+                    'address_invoice.city' => 'Factuur plaats',
+                    'address_invoice.country' => 'Factuur land',
+                    'primaryEmailAddress.email' => 'Email primair',
+                    'emailAddress_2' => 'Email 2',
+                    'emailAddress_3' => 'Email 3',
+                    'emailAddress_4' => 'Email 4',
+                    'emailAddress_5' => 'Email 5',
+                    'primaryphoneNumber.number' => 'Telefoonnummer primair',
+                    'phonenumber_2' => 'Telefoonnummer 2',
+                    'phonenumber_3' => 'Telefoonnummer 3',
                     'contact.primaryphoneNumber.number' => 'Primair telefoonnummer',
                     'energy_supplier_name' => 'Energieleverancier',
                     'contact.primaryContactEnergySupplier.es_number' => 'Klantnummer',
@@ -152,9 +186,9 @@ class ParticipantCSVHelper
                     'date_register' => 'Inschrijfdatum',
                     'date_end' => 'Einddatum',
                     'participantProductionProjectPayoutType.name' => 'Uitkeren op',
-                ])->getCsv();
+                ], $headers);
+                $headers = false;
             }
+        return $csv->getCsv();
         }
-        return $csv;
-    }
 }

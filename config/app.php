79cefe3d441a74a4ea63c1563d7b49ba9c70b6c1
Rob Rollenberg <rob@jepr.nl>
2018-08-28 13:00:02 +0200

    Ophoging versienummer

--- config/app.php
+++ config/app.php
@@ -4,7 +4,7 @@ return [
 
     //Version number displayed in the header, change this with every update
     'version_major' => '1',
-    'version_minor' => '9',
+    'version_minor' => '10',
     'version_fix' => '0',
 
     //Alfresco envs

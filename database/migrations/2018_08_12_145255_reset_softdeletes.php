584320e963bacf4b068e0d077d780bac96d113be
Fren <fren.dehaan@xaris.nl>
2018-08-30 10:56:33 +0200

    Fix migration - do not send mail for new coops

--- database/migrations/2018_08_12_145255_reset_softdeletes.php
+++ database/migrations/2018_08_12_145255_reset_softdeletes.php
@@ -29,73 +29,83 @@ class ResetSoftdeletes extends Migration
 
         Auth::setUser(User::find(1));
 
+        $didResetSomething = false;
         foreach (Task::onlyTrashed()->get() as $item){
+            $didResetSomething = true;
             $item->deleted_at = null;
             $item->save();
             $mailContent .=('<li>Taak met id: '. $item->id . ', ' . $item->note . ' Teruggezet!' . '</li>');
         }
 
         foreach (Order::onlyTrashed()->get() as $item){
+            $didResetSomething = true;
             $item->deleted_at = null;
             $item->save();
             $mailContent .=('<li>Order met id: '. $item->id . ', ' . $item->subject . ' Teruggezet!' . '</li>');
         }
 
         foreach (Person::onlyTrashed()->get() as $item){
+            $didResetSomething = true;
             $item->deleted_at = null;
             $item->save();
         }
 
         foreach (Contact::onlyTrashed()->get() as $item){
+            $didResetSomething = true;
             $item->deleted_at = null;
             $item->save();
             $mailContent .=('<li>Contact met id: '. $item->id . ', ' . $item->full_name .  ' Teruggezet!' . '</li>');
         }
 
         foreach (Product::onlyTrashed()->get() as $item){
+            $didResetSomething = true;
             $item->deleted_at = null;
             $item->save();
             $mailContent .=('<li>Product met id: '. $item->id . ', ' . $item->name . ' Teruggezet!' . '</li>');
         }
 
         foreach (Organisation::onlyTrashed()->get() as $item){
+            $didResetSomething = true;
             $item->deleted_at = null;
             $item->save();
         }
 
         foreach (Administration::onlyTrashed()->get() as $item){
+            $didResetSomething = true;
             $item->deleted_at = null;
             $item->save();
             $mailContent .=('<li>Administratie met id: '. $item->id . ', ' . $item->name . ' Teruggezet!' . '</li>');
         }
 
         foreach (ParticipantTransaction::whereNotNull('deleted_at')->get() as $item){
+            $didResetSomething = true;
             $mailContent .=('<li>Participant transactie '. $item->id . ' verwijderd!' . '</li>');
             $item->delete();
         }
 
         foreach (\App\Eco\ParticipantProductionProject\ParticipantProductionProject::onlyTrashed()->get() as $item){
+            $didResetSomething = true;
             $item->deleted_at = null;
             $item->save();
             $mailContent .=('<li>Participatie met id: '. $item->id . ', ' . ($item->contact ? $item->contact->full_name : '') . ' in project: ' . $item->production_project_id . ' Teruggezet!' . '</li>');
         }
 
         foreach (Address::onlyTrashed()->get() as $item){
+            $didResetSomething = true;
             $item->deleted_at = null;
             $item->save();
             $mailContent .=('<li>Adres met id: '. $item->id . ', ' . $item->street . ' ' . $item->number . $item->addition . ', ' . $item->city . ' van contact: ' . ($item->contact ? $item->contact->full_name : '') . ' Teruggezet!' . '</li>');
         }
 
         $mailContent .= '</ul>';
-        if (\Config::get('app.env') == 'production') { // alleen mail versturen in productie
+        if (\Config::get('app.env') == 'production' && $didResetSomething) { // alleen mail versturen in productie
             Mail::send('emails.generic', ['html_body' => $mailContent], function ($message) {
                 $message->subject('Softdelete teruggezet voor ' . config('app.name'));
                 $message->to(['fren.dehaan@xaris.nl', 'rene.van.vliet@outlook.com']);
             });
         }
 
-
-//        //Participant transacties zijn geen softdelete meer
+        //Participant transacties zijn geen softdelete meer
         Schema::table('participant_transactions', function (Blueprint $table) {
             $table->dropColumn('deleted_at');
         });

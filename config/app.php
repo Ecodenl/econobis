2677317cc4757a1e61b902d77574e78755de1eee
Rob Rollenberg <rob@jepr.nl>
2018-09-11 15:28:53 +0200

    Update version number

--- config/app.php
+++ config/app.php
@@ -5,7 +5,7 @@ return [
     //Version number displayed in the header, change this with every update
     'version_major' => '1',
     'version_minor' => '10',
-    'version_fix' => '1',
+    'version_fix' => '2',
 
 
     //Alfresco envs

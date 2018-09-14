21240d6f80a56ed712b898843731552eceb59a72
Jos Kolenberg <joskolenberg@gmail.com>
2018-09-14 11:42:21 +0200

    Dynamische groepen nummer obv laatste groepnaam ipv id

--- app/Http/Controllers/Api/ParticipationProductionProject/ParticipationProductionProjectController.php
+++ app/Http/Controllers/Api/ParticipationProductionProject/ParticipationProductionProjectController.php
@@ -71,7 +71,7 @@ class ParticipationProductionProjectController extends ApiController
         $contactGroup = new ContactGroup();
         $contactGroup->type_id = 'dynamic';
         $contactGroup->composed_of = 'participants';
-        $contactGroup->name = 'Dynamische groep ' . (ContactGroup::max('id') + 1);
+        $contactGroup->name =  ContactGroup::getAutoIncrementedName('Dynamische groep');
         $contactGroup->description = '';
         $contactGroup->dynamic_filter_type = $request->input('filterType') ? $request->input('filterType') : 'and';
         $contactGroup->save();

50dbd2a24b8c25d53cc7b87e4b31ad490336c182
Rob Rollenberg <rob@jepr.nl>
2018-09-19 09:57:08 +0200

    Standaard filter op Open en sortering op naam

--- client-app/src/reducers/contact-group/ContactGroupsSortsReducer.js
+++ client-app/src/reducers/contact-group/ContactGroupsSortsReducer.js
@@ -1,4 +1,4 @@
-export default (state = [], action) => {
+export default (state = [{field: 'name', order: 'asc'}], action) => {
     switch(action.type) {
         case 'SET_CONTACT_GROUP_SORTS':
             if(state.length === 3) state.pop();

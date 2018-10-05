57e167f2347a61a2a45cec4dee5f4310b462a028
Fren <fren.dehaan@xaris.nl>
2018-10-05 15:05:38 +0200

    GeneralSearchModal.js updates

--- client-app/src/components/modal/Modal.js
+++ client-app/src/components/modal/Modal.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Draggable from 'react-draggable';
+import ReactDOM from 'react-dom'
 
 class Modal extends Component {
     constructor(props) {
--- client-app/src/container/general-search/list/GeneralSearchModal.js
+++ client-app/src/container/general-search/list/GeneralSearchModal.js
@@ -1,34 +1,54 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import GeneralSearchModalModelList from "./GeneralSearchModalModelList";
+import Draggable from 'react-draggable';
 
 class GeneralSearchModal extends Component {
     constructor(props) {
         super(props);
-    }
+
+        this.state = {
+            activeDrags: 0,
+        };
+
+        this.onStart = this.onStart.bind(this);
+        this.onStop = this.onStop.bind(this);
+    };
+
+    onStart() {
+        this.setState({activeDrags: ++this.state.activeDrags});
+    };
+
+    onStop() {
+        this.setState({activeDrags: --this.state.activeDrags});
+    };
 
     render() {
         return (
-            <div className="modal search-modal">
-                <div className="modal-dialog search-modal-dialog">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h4 className="modal-title">Zoekresultaten</h4>
-                        </div>
-                        <div className="modal-body search-modal-body">
-                            <div className="col-md-12 margin-30-top">
-                                <GeneralSearchModalModelList
-                                    closeModal={this.props.closeModal}
-                                    records={this.props.records}
-                                />
+            <Draggable handle=".modal-header" onStart={this.onStart} onStop={this.onStop}>
+                <div className="modal search-modal">
+                    <div className="modal-dialog search-modal-dialog">
+                        <div className="modal-content">
+                            <div className={`modal-header draggable-header`}>
+                                <h4 className="modal-title">Zoekresultaten<span className="close-modal" onClick={this.props.closeModal}>X</span></h4>
                             </div>
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-default" onClick={this.props.closeModal}>Sluit</button>
+                            <div className="modal-body search-modal-body">
+                                <div className="col-md-12 margin-30-top">
+                                    <GeneralSearchModalModelList
+                                        closeModal={this.props.closeModal}
+                                        records={this.props.records}
+                                    />
+                                </div>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-default"
+                                        onClick={this.props.closeModal}>Sluit
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </Draggable>
         )
     }
 }

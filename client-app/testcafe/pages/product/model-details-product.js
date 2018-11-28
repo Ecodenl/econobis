379f1514976694311dc085bf184ce5865ed6ba15
Fren <fren.dehaan@xaris.nl>
2018-11-28 16:10:08 +0100

    nieuw product met variabele prijs test

--- client-app/testcafe/pages/product/model-details-product.js
+++ client-app/testcafe/pages/product/model-details-product.js
@@ -6,7 +6,13 @@ export default class ModelDetailsProduct {
         this.newPriceHistory = Selector('span').withExactText('Prijshistorie').parent().child('a');
         this.dateStart = Selector('input[name="dateStart"]');
         this.price = Selector('input[name="price"]');
+
+        //werkt niet?
+        //this.hasVariablePrice = Selector('input[name="hasVariablePrice"]');
+        this.hasVariablePrice = Selector('.react-toggle');
+
         this.vatPercentage = Selector('select[name="vatPercentage"]');
+
         this.priceHistoryRows = Selector('span').withExactText('Prijshistorie').parent().parent().child().nth(1).child().child().child();
     }
 }

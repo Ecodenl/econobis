a35d7f672cdb9e94f4f32b725243c6e65c521e3a
Fren <fren.dehaan@xaris.nl>
2018-12-04 10:15:15 +0100

    decrypten van audit trail

--- app/Eco/AuditTrail/AuditTrail.php
+++ app/Eco/AuditTrail/AuditTrail.php
@@ -9,7 +9,9 @@ use App\Eco\Organisation\Organisation;
 use App\Eco\Intake\Intake;
 use App\Eco\Task\Task;
 use App\Eco\User\User;
+use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Crypt;
 use Venturecraft\Revisionable\RevisionableTrait;
 
 class AuditTrail extends Model
@@ -19,6 +21,30 @@ class AuditTrail extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function getOldValueAttribute($value)
+    {
+        try {
+            if($value) {
+                $value = Crypt::decrypt($value);
+                return $value;
+            }
+        } catch (DecryptException $e) {
+            return $value;
+        }
+    }
+
+    public function getNewValueAttribute($value)
+    {
+        try {
+            if($value) {
+                $value = Crypt::decrypt($value);
+                return $value;
+            }
+        } catch (DecryptException $e) {
+            return $value;
+        }
     }
 
 }

72de3554bfa8e477fd8c6003a31e94ba075e7b38
Fren <fren.dehaan@xaris.nl>
2018-12-07 13:17:33 +0100

    Nog meer fonts weg :( hebben geen effect

--- client-app/src/components/form/InputTinyMCEUpdateable.js
+++ client-app/src/components/form/InputTinyMCEUpdateable.js
@@ -54,7 +54,7 @@ class InputTinyMCEUpdateable extends Component {
                             toolbar: 'undo redo | formatselect fontselect | bold italic forecolor | alignleft aligncenter alignright | bullist numlist outdent indent | table | link image | code',
                             height: "300",
                             browser_spellcheck : true,
-                            font_formats: 'Arial=arial;Courier New=courier new;Helvetica=helvetica;Tahoma=tahoma;Times New Roman=times new roman;Verdana=verdana;'
+                            font_formats: 'Courier New=courier new;Tahoma=tahoma;Times New Roman=times new roman;Verdana=verdana;'
                         }}
                         onChange={onChangeAction}
                     />

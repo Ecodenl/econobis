d73a156eec8df85837030604e42c341b63b4feda
Fren <fren.dehaan@xaris.nl>
2018-12-14 09:14:06 +0100

    Loading state voor overige modules

--- client-app/src/sagas/team/TeamsSaga.js
+++ client-app/src/sagas/team/TeamsSaga.js
@@ -3,10 +3,13 @@ import TeamsAPI from '../../api/team/TeamsAPI';
 
 export function* fetchTeamsSaga() {
     try {
+        yield put({ type: 'IS_LOADING' });
         const teams = yield call(TeamsAPI.fetchTeams);
         yield put({ type: 'FETCH_TEAMS_SUCCESS', teams });
+        yield put({ type: 'IS_LOADING_COMPLETE' });
     } catch (error) {
         yield put({ type: 'FETCH_TEAMS_ERROR', error });
+        yield put({ type: 'LOADING_ERROR', error });
     }
 }
 

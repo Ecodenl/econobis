48c77f152e99393fdd89430fd69f787ff857da8e
Jos Kolenberg <joskolenberg@gmail.com>
2018-12-14 16:49:43 +0100

    Versienummer 2.2.10 + compile (2)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,5 +19,5 @@
 </script>
 
 <div id="root"></div>
-<script type="text/javascript" src="./js/vendors~bundle.76b0ce729e2548f9eeb8.js"></script><script type="text/javascript" src="./js/bundle.fe88cb01d8d78137406d.js"></script></body>
+<script type="text/javascript" src="./js/vendors~bundle.884047db6dbefd842829.js"></script><script type="text/javascript" src="./js/bundle.24c28d9078d57ae1e341.js"></script></body>
 </html>

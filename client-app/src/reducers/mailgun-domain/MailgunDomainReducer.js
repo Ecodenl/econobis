ac60cda99cebe476e8e53640f1a32256363af982
Jos Kolenberg <joskolenberg@gmail.com>
2019-01-11 09:36:08 +0100

    Tussencommit frontend mailgun domains (bestand vergeten)

---
+++ client-app/src/reducers/mailgun-domain/MailgunDomainReducer.js
@@ -0,0 +1,13 @@
+export default function (state= [], action) {
+    switch (action.type) {
+        case 'FETCH_MAILGUN_DOMAINS_SUCCESS':
+            return [
+                ...state,
+                ...action.mailgunDomains.data.data
+            ];
+        case 'CLEAR_MAILGUN_DOMAINS':
+            return state.mailgunDomains = [];
+        default:
+            return state;
+    }
+}

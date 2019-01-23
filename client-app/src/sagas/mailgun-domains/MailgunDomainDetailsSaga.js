bf4d5837d6bb91bc281e1b31985e1aea10e15548
Rob Rollenberg <rob@jepr.nl>
2019-01-23 11:47:14 +0100

    When added or edit a mailgun domain the systemdata will be refreshed

--- client-app/src/sagas/mailgun-domains/MailgunDomainDetailsSaga.js
+++ client-app/src/sagas/mailgun-domains/MailgunDomainDetailsSaga.js
@@ -21,6 +21,8 @@ export function* updateMailgunDomainDetailsSaga({mailgunDomain, switchToView}) {
 
         yield put({type: 'UPDATE_MAILGUN_DOMAIN_SUCCESS', mailgunDomainDetails});
 
+        // Reload system data after updating user
+        yield put({ type: 'FETCH_SYSTEM_DATA'});
         // Switch back to view callback fn
         yield switchToView();
     } catch (error) {

4fd80dfe7f6650102486923d5185ad52c99a4f60
Rob Rollenberg <rob@jepr.nl>
2019-02-07 12:20:34 +0100

    Fix migration class name

--- database/migrations/2019_02_06_135801_add_code_ref_to_project_type.php
+++ database/migrations/2019_02_06_135801_add_code_ref_to_project_type.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddRefColumnToProjectType extends Migration
+class AddCodeRefToProjectType extends Migration
 {
     /**
      * Run the migrations.

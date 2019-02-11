d0f8f762774bde63f79030d40d370c6e852e5694
Rob Rollenberg <rob@jepr.nl>
2019-02-11 08:06:40 +0100

    Compiled js files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,5 +19,5 @@
 </script>
 
 <div id="root"></div>
-<script type="text/javascript" src="./js/vendors~bundle.e82112d173abdbb4a335.js"></script><script type="text/javascript" src="./js/bundle.66808f03bf7dcd36030b.js"></script></body>
+<script type="text/javascript" src="./js/vendors~bundle.e82112d173abdbb4a335.js"></script><script type="text/javascript" src="./js/bundle.63643828135344be5b74.js"></script></body>
 </html>

eb2e5d73134d0dd1ceac8c6288e672e18f8ba6e4
robrollenberg <rob@jepr.nl>
2019-02-15 12:55:13 +0100

    Added default form for new and edit

--- client-app/src/container/participant-project/details/mutation/MutationFormEdit.js
+++ client-app/src/container/participant-project/details/mutation/MutationFormEdit.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
-import InputText from '../../../../components/form/InputText';
-import ButtonText from '../../../../components/button/ButtonText';
-import Panel from '../../../../components/panel/Panel';
-import PanelBody from '../../../../components/panel/PanelBody';
-import InputDate from '../../../../components/form/InputDate';
 import moment from 'moment/moment';
 moment.locale('nl');
+import MutationFormDefault from './MutationFormDefault';
 
 const MutationFormEdit = props => {
     const {
         type,
-        dateTransaction,
+        dateMutation,
         amount,
         iban,
         referral,
@@ -24,98 +19,25 @@ const MutationFormEdit = props => {
 
     return (
         <div>
-            <form className="form-horizontal" onSubmit={props.handleSubmit}>
-                <Panel className={'panel-grey'}>
-                    <PanelBody>
-                        <div className="row">
-                            <InputText label={'Type'} id={'type'} name={'type'} value={type.name} readOnly={true} />
-                            <InputDate
-                                label="M datum"
-                                name="dateTransaction"
-                                value={dateTransaction}
-                                onChangeAction={props.handleInputChangeDate}
-                                required={'required'}
-                            />
-                        </div>
-
-                        <div className="row">
-                            <InputText
-                                type={'number'}
-                                label={'Bedrag'}
-                                id={'amount'}
-                                name={'amount'}
-                                value={amount}
-                                onChangeAction={props.handleInputChange}
-                                required={'required'}
-                                error={props.errors.amount}
-                            />
-                            <InputText
-                                label={'IBAN'}
-                                id={'iban'}
-                                name={'iban'}
-                                value={iban ? iban : ''}
-                                onChangeAction={props.handleInputChange}
-                                error={props.errors.iban}
-                            />
-                        </div>
-
-                        <div className="row">
-                            <InputText
-                                label={'Kenmerk'}
-                                id={'referral'}
-                                name={'referral'}
-                                value={referral ? referral : ''}
-                                onChangeAction={props.handleInputChange}
-                            />
-                            <InputText
-                                label={'Boekstuk'}
-                                id={'entry'}
-                                name={'entry'}
-                                value={entry ? entry : ''}
-                                onChangeAction={props.handleInputChange}
-                            />
-                        </div>
-
-                        <div className="row">
-                            <InputDate
-                                label="Boek datum"
-                                name="dateBooking"
-                                value={dateBooking ? dateBooking : ''}
-                                onChangeAction={props.handleInputChangeDate}
-                            />
-                        </div>
-
-                        <div className="row">
-                            <InputText
-                                label={'Gemaakt op'}
-                                name={'createdAt'}
-                                value={createdAt ? moment(createdAt.date).format('L') : ''}
-                                readOnly={true}
-                            />
-                            <InputText
-                                label={'Gemaakt door'}
-                                name={'createdBy'}
-                                value={createdBy ? createdBy.fullName : ''}
-                                readOnly={true}
-                            />
-                        </div>
-
-                        <div className="pull-right btn-group" role="group">
-                            <ButtonText
-                                buttonClassName={'btn-default'}
-                                buttonText={'Annuleren'}
-                                onClickAction={props.cancelEdit}
-                            />
-                            <ButtonText
-                                buttonText={'Opslaan'}
-                                onClickAction={props.handleSubmit}
-                                type={'submit'}
-                                value={'Submit'}
-                            />
-                        </div>
-                    </PanelBody>
-                </Panel>
-            </form>
+            <MutationFormDefault
+                editForm={true}
+                typeId={type.id}
+                type={type}
+                dateMutation={dateMutation}
+                amount={amount}
+                iban={iban}
+                referral={referral}
+                entry={entry}
+                dateBooking={dateBooking}
+                createdAt={createdAt}
+                createdBy={createdBy}
+                errors={props.errors}
+                participantMutationTypes={props.participantMutationTypes}
+                handleSubmit={props.handleSubmit}
+                handleInputChange={props.handleInputChange}
+                handleInputChangeDate={props.handleInputChangeDate}
+                toggleShow={props.cancelEdit}
+            />
         </div>
     );
 };

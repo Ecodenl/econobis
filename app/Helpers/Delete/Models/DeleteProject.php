b1decc9dd5582d915ca0d971fa25c9aedce4adf1
Rob Rollenberg <rob@jepr.nl>
2019-04-18 09:25:54 +0200

    Delete project fixed

--- app/Helpers/Delete/Models/DeleteProject.php
+++ app/Helpers/Delete/Models/DeleteProject.php
@@ -13,7 +13,7 @@ use App\Helpers\Delete\DeleteInterface;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * Class DeleteProductionProject
+ * Class DeleteProject
  *
  * Relation: 1-n Participations. Action: call DeleteParticipation
  * Relation: 1-n Documents. Action: dissociate
@@ -22,19 +22,19 @@ use Illuminate\Database\Eloquent\Model;
  *
  * @package App\Helpers\Delete\Models
  */
-class DeleteProductionProject implements DeleteInterface
+class DeleteProject implements DeleteInterface
 {
     private $errorMessage = [];
-    private $productionProject;
+    private $project;
 
     /** Sets the model to delete
      *
-     * @param Model $productionProject the model to delete
+     * @param Model $project the model to delete
      */
 
-    public function __construct(Model $productionProject)
+    public function __construct(Model $project)
     {
-        $this->productionProject = $productionProject;
+        $this->project = $project;
     }
 
     /** Main method for deleting this model and all it's relations
@@ -49,7 +49,7 @@ class DeleteProductionProject implements DeleteInterface
         $this->dissociateRelations();
         $this->deleteRelations();
         $this->customDeleteActions();
-        $this->productionProject->delete();
+        $this->project->delete();
 
         return $this->errorMessage;
     }
@@ -66,12 +66,12 @@ class DeleteProductionProject implements DeleteInterface
      */
     public function deleteModels()
     {
-        foreach ($this->productionProject->participantsProductionProject as $participation){
+        foreach ($this->project->participantsProject as $participation){
             $deleteParticipation = new DeleteParticipation($participation);
             $this->errorMessage = array_merge($this->errorMessage, $deleteParticipation->delete());
         }
 
-        foreach ($this->productionProject->tasks as $task) {
+        foreach ($this->project->tasks as $task) {
             $deleteTask = new DeleteTask($task);
             $this->errorMessage = array_merge($this->errorMessage, $deleteTask->delete());
         }
@@ -81,13 +81,13 @@ class DeleteProductionProject implements DeleteInterface
      */
     public function dissociateRelations()
     {
-        foreach ($this->productionProject->emails as $email){
-            $email->productionProject()->dissociate();
+        foreach ($this->project->emails as $email){
+            $email->project()->dissociate();
             $email->save();
         }
 
-        foreach ($this->productionProject->documents as $document){
-            $document->productionProject()->dissociate();
+        foreach ($this->project->documents as $document){
+            $document->project()->dissociate();
             $document->save();
         }
     }

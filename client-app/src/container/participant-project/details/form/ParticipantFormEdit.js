2a76ccb2be60285fce510c4892560c5d31897268
robrollenberg <rob@jepr.nl>
2019-05-10 10:00:09 +0200

    Fix edit participant project general

--- client-app/src/container/participant-project/details/form/ParticipantFormEdit.js
+++ client-app/src/container/participant-project/details/form/ParticipantFormEdit.js
@@ -9,8 +9,16 @@ import ParticipantProjectDetailsAPI from '../../../../api/participant-project/Pa
 import { fetchParticipantProjectDetails } from '../../../../actions/participants-project/ParticipantProjectDetailsActions';
 import ContactsAPI from '../../../../api/contact/ContactsAPI';
 import * as ibantools from 'ibantools';
-import { browserHistory } from 'react-router';
-import ParticipantFormDefaultGeneral from '../../form-default/ParticipantFormDefaultGeneral';
+import ViewText from '../../../../components/form/ViewText';
+import InputToggle from '../../../../components/form/InputToggle';
+import InputSelect from '../../../../components/form/InputSelect';
+import InputText from '../../../../components/form/InputText';
+import moneyPresenter from '../../../../helpers/MoneyPresenter';
+import PanelFooter from '../../../../components/panel/PanelFooter';
+import ButtonText from '../../../../components/button/ButtonText';
+import ParticipantFormEditPostalcodeLinkCapital from './edit/ParticipantFormEditPostalcodeLinkCapital';
+import ParticipantFormEditCapital from './edit/ParticipantFormEditCapital';
+import ParticipantFormEditObligation from './edit/ParticipantFormEditObligation';
 
 class ParticipantFormEdit extends Component {
     constructor(props) {
@@ -18,25 +26,11 @@ class ParticipantFormEdit extends Component {
 
         const {
             id,
-            contact,
-            statusId,
-            project,
-            dateRegister,
-            participationsRequested,
-            participationsDefinitive,
-            participationsSold,
-            participationsRestSale,
-            participationsDefinitiveWorth,
-            dateContractSend,
-            dateContractRetour,
-            datePayed,
             didAcceptAgreement,
             giftedByContactId,
             ibanPayout,
             legalRepContactId,
             ibanPayoutAttn,
-            updatedAt,
-            dateEnd,
             typeId,
             powerKwhConsumption,
         } = props.participation;
@@ -45,38 +39,19 @@ class ParticipantFormEdit extends Component {
             contacts: [],
             participation: {
                 id,
-                contactName: contact.fullName,
-                statusId,
-                projectName: project.name,
-                projectAdministrationName: project.administration ? project.administration.name : '',
-                dateRegister: dateRegister ? dateRegister : '',
-                participationsRequested: participationsRequested ? participationsRequested : '',
-                participationsDefinitive: participationsDefinitive ? participationsDefinitive : '',
-                participationsSold: participationsSold ? participationsSold : '',
-                participationsRestSale: participationsRestSale ? participationsRestSale : '',
-                participationsDefinitiveWorth: participationsDefinitiveWorth ? participationsDefinitiveWorth : '',
-                dateContractSend: dateContractSend ? dateContractSend : '',
-                dateContractRetour: dateContractRetour ? dateContractRetour : '',
-                datePayed: datePayed ? datePayed : '',
-                didAcceptAgreement: !!didAcceptAgreement,
+                didAcceptAgreement: Boolean(didAcceptAgreement),
                 giftedByContactId: giftedByContactId ? giftedByContactId : '',
                 ibanPayout: ibanPayout ? ibanPayout : '',
                 legalRepContactId: legalRepContactId ? legalRepContactId : '',
                 ibanPayoutAttn: ibanPayoutAttn ? ibanPayoutAttn : '',
-                updatedAt: updatedAt ? updatedAt : '',
-                dateEnd: dateEnd ? dateEnd : '',
                 typeId,
                 powerKwhConsumption: powerKwhConsumption ? powerKwhConsumption : '',
             },
             errors: {
-                contactId: false,
-                statusId: false,
-                projectId: false,
                 typeId: false,
                 ibanPayout: false,
             },
         };
-        this.handleInputChangeDate = this.handleInputChangeDate.bind(this);
     }
 
     componentDidMount() {
@@ -101,7 +76,7 @@ class ParticipantFormEdit extends Component {
         });
     };
 
-    handleInputChangeDate(value, name) {
+    handleInputChangeDate = (value, name) => {
         this.setState({
             ...this.state,
             participation: {
@@ -109,7 +84,7 @@ class ParticipantFormEdit extends Component {
                 [name]: value,
             },
         });
-    }
+    };
 
     handleSubmit = event => {
         event.preventDefault();
@@ -152,21 +127,166 @@ class ParticipantFormEdit extends Component {
     };
 
     render() {
+        const { didAcceptAgreement, giftedByContactId, ibanPayout, ibanPayoutAttn, typeId } = this.state.participation;
+
+        const {
+            contact,
+            uniqueMutationStatuses,
+            project,
+            participationsDefinitive,
+            participationsDefinitiveWorth,
+            amountDefinitive,
+            powerKwhConsumption,
+        } = this.props.participation;
+
+        const projectTypeCodeRef = project.projectType.codeRef;
+
         return (
-            <ParticipantFormDefaultGeneral
-                editForm={true}
-                participation={this.state.participation}
-                errors={this.state.errors}
-                contacts={this.state.contacts}
-                handleInputChange={this.handleInputChange}
-                handleInputChangeDate={this.handleInputChangeDate}
-                handleCancel={this.props.switchToView}
-                handleSubmit={this.handleSubmit}
-                participationWorth={this.props.participation.project.participationWorth}
-                valueCourses={this.props.participation.project.valueCourses}
-                handleProjectChange={this.handleProjectChange}
-                projectTypeCodeRef={this.props.participation.project.projectType.codeRef}
-            />
+            <form className="form-horizontal col-md-12" onSubmit={this.handleSubmit}>
+                <div className="row">
+                    <ViewText
+                        label={'Contact'}
+                        value={contact ? contact.fullName : ''}
+                        link={contact ? 'contact/' + contact.id : ''}
+                        className={'col-sm-6 form-group'}
+                    />
+                    <ViewText
+                        label={'Status'}
+                        value={uniqueMutationStatuses.map(item => item.name).join(', ')}
+                        className={'col-sm-6 form-group'}
+                    />
+                </div>
+
+                <div className="row">
+                    <ViewText
+                        label={'Project'}
+                        value={project ? project.name : ''}
+                        link={project ? 'project/' + project.id : ''}
+                        className={'col-sm-6 form-group'}
+                    />
+                    <ViewText
+                        label={'Administratie'}
+                        value={project.administration ? project.administration.name : ''}
+                        className={'col-sm-6 form-group'}
+                    />
+                </div>
+
+                <div className="row">
+                    <InputToggle
+                        label={'Akkoord reglement'}
+                        name={'didAcceptAgreement'}
+                        id={'didAcceptAgreement'}
+                        value={didAcceptAgreement}
+                        onChangeAction={this.handleInputChange}
+                    />
+                </div>
+
+                <div className="row">
+                    <InputSelect
+                        label={'Schenker'}
+                        name={'giftedByContactId'}
+                        id={'giftedByContactId'}
+                        options={this.state.contacts}
+                        optionName={'fullName'}
+                        value={giftedByContactId}
+                        onChangeAction={this.handleInputChange}
+                    />
+                    <InputText
+                        label={'IBAN uitkeren'}
+                        name={'ibanPayout'}
+                        id={'ibanPayout'}
+                        value={ibanPayout}
+                        onChangeAction={this.handleInputChange}
+                        error={this.state.errors.ibanPayout}
+                    />
+                </div>
+
+                <div className="row">
+                    {projectTypeCodeRef === 'obligation' ? (
+                        <div className={'form-group col-md-6'} />
+                    ) : (
+                        <InputText
+                            label={`Huidig saldo ${projectTypeCodeRef === 'loan' ? 'lening' : 'kapitaal'} rekening`}
+                            name={'amountDefinitive'}
+                            id={'amountDefinitive'}
+                            value={amountDefinitive}
+                            onChangeAction={() => {}}
+                            readOnly={true}
+                        />
+                    )}
+                    <InputText
+                        label={'IBAN uitkeren t.n.v.'}
+                        name={'ibanPayoutAttn'}
+                        id={'ibanPayoutAttn'}
+                        value={ibanPayoutAttn}
+                        onChangeAction={this.handleInputChange}
+                    />
+                </div>
+
+                <div className="row">
+                    <ViewText
+                        label={'Totale opbrengsten'}
+                        id={'totalWorthParticipations'}
+                        className={'col-sm-6 form-group'}
+                        value={moneyPresenter(0)}
+                    />
+                    <InputSelect
+                        label={'Uitkeren op'}
+                        name={'typeId'}
+                        id={'typeId'}
+                        options={this.props.participantProjectPayoutTypes}
+                        value={typeId}
+                        onChangeAction={this.handleInputChange}
+                        required={'required'}
+                        error={this.state.errors.typeId}
+                    />
+                </div>
+
+                {projectTypeCodeRef === 'obligation' ? (
+                    <ParticipantFormEditObligation
+                        participationWorth={project.participationWorth}
+                        participationsDefinitive={participationsDefinitive}
+                        participationsDefinitiveWorth={participationsDefinitiveWorth}
+                        valueCourses={project.valueCourses}
+                    />
+                ) : null}
+
+                {projectTypeCodeRef === 'capital' ? (
+                    <ParticipantFormEditCapital
+                        participationWorth={project.participationWorth}
+                        participationsDefinitive={participationsDefinitive}
+                        participationsDefinitiveWorth={participationsDefinitiveWorth}
+                        valueCourses={project.valueCourses}
+                    />
+                ) : null}
+
+                {projectTypeCodeRef === 'postalcode_link_capital' ? (
+                    <ParticipantFormEditPostalcodeLinkCapital
+                        participationWorth={project.participationWorth}
+                        participationsDefinitive={participationsDefinitive}
+                        participationsDefinitiveWorth={participationsDefinitiveWorth}
+                        valueCourses={project.valueCourses}
+                        powerKwhConsumption={powerKwhConsumption}
+                        handleInputChange={this.handleInputChange}
+                    />
+                ) : null}
+
+                <PanelFooter>
+                    <div className="pull-right btn-group" role="group">
+                        <ButtonText
+                            buttonClassName={'btn-default'}
+                            buttonText={'Annuleren'}
+                            onClickAction={this.handleCancel}
+                        />
+                        <ButtonText
+                            buttonText={'Opslaan'}
+                            onClickAction={this.handleSubmit}
+                            type={'submit'}
+                            value={'Submit'}
+                        />
+                    </div>
+                </PanelFooter>
+            </form>
         );
     }
 }
---
+++ client-app/src/container/participant-project/details/form/edit/ParticipantFormEditCapital.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import moment from 'moment';
+
+moment.locale('nl');
+import InputText from '../../../../../components/form/InputText';
+
+function ParticipantFormEditCapital({
+    participationWorth,
+    participationsDefinitiveWorth,
+    participationsDefinitive,
+    valueCourses,
+}) {
+    const activeValueCourse = valueCourses ? valueCourses.find(valueCourse => valueCourse.active) : [];
+
+    return (
+        <React.Fragment>
+            <hr style={{ margin: '10px 0' }} />
+            <h4>Kapitaal</h4>
+            <div className="row">
+                <InputText
+                    label={'Huidig aantal participaties'}
+                    name={'participationsDefinitive'}
+                    id={'participationsDefinitive'}
+                    value={participationsDefinitive}
+                    readOnly={true}
+                />
+                {/*<InputText*/}
+                {/*type={'number'}*/}
+                {/*label={'Participaties aangevraagd'}*/}
+                {/*name={'participationsRequested'}*/}
+                {/*id={'participationsRequested'}*/}
+                {/*value={participationsRequested}*/}
+                {/*onChangeAction={handleInputChange}*/}
+                {/*/>*/}
+            </div>
+            <div className="row">
+                <InputText
+                    label={'Nominale waarde per participatie'}
+                    name={'participationWorth'}
+                    id={'participationWorth'}
+                    value={participationWorth}
+                    readOnly={true}
+                />
+                {/*<InputText*/}
+                {/*type={'number'}*/}
+                {/*label={'Participaties toegekend'}*/}
+                {/*name={'participationsDefinitive'}*/}
+                {/*id={'participationsDefinitive'}*/}
+                {/*value={participationsDefinitive}*/}
+                {/*onChangeAction={handleInputChange}*/}
+                {/*/>*/}
+            </div>
+            <div className="row">
+                <InputText
+                    label={'Huidige boekwaarde per participatie'}
+                    name={'valueCourseBookWorth'}
+                    id={'valueCourseBookWorth'}
+                    value={activeValueCourse && activeValueCourse.bookWorth}
+                    readOnly={true}
+                />
+            </div>
+            <div className="row">
+                <InputText
+                    label={'Huidige totale waarde'}
+                    name={'participationsDefinitiveWorth'}
+                    id={'participationsDefinitiveWorth'}
+                    value={participationsDefinitiveWorth}
+                    readOnly={true}
+                />
+            </div>
+        </React.Fragment>
+    );
+}
+
+ParticipantFormEditCapital.propTypes = {
+    participationWorth: PropTypes.number.isRequired,
+    participationsDefinitive: PropTypes.number.isRequired,
+    participationsDefinitiveWorth: PropTypes.number.isRequired,
+};
+
+export default ParticipantFormEditCapital;

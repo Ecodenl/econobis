286fac49c412795cb27eea12e0d225b2a4f090e4
Wim <wim@mosmania.nl>
2019-05-10 08:37:53 +0200

    When Twinfield data is changed in Administratie for now we do no actions. No new syncronize customers, no delete of invoice twinfield codes (for matching with twinfield transactions), etc.

--- app/Eco/Administration/AdministrationObserver.php
+++ app/Eco/Administration/AdministrationObserver.php
@@ -31,25 +31,25 @@ class AdministrationObserver
         //Als er iets in de twinfield instelling veranderd is moeten we dit opnieuw synchroniseren
         if(($administration->isDirty('twinfield_username') || $administration->isDirty('twinfield_organization_code') || $administration->isDirty('twinfield_office_code')) && $administration->twinfield_is_valid){
 
-            //TODO moet dit?
-            foreach ($administration->twinfieldNumbers as $twinfieldNumber) {
-                $twinfieldNumber->delete();
-            }
-
-            foreach ($administration->invoices()->whereNotNull('twinfield_number')->where('status_id', 'exported')->get() as $invoice) {
-                $invoice->twinfield_number = null;
-                $invoice->status_id = 'sent';
-                $invoice->save();
-            }
-
-            foreach ($administration->invoices()->whereNotNull('twinfield_number')->get() as $invoice) {
-                $invoice->twinfield_number = null;
-                $invoice->save();
-            }
-
-            //alle contacten als klanten in Twinfield zetten
-            $twinfieldCustomerHelper = new TwinfieldCustomerHelper($administration, null);
-            $twinfieldCustomerHelper->createAllCustomers();
+            //TODO moet dit? Niet in eerste versie met Twinfield aanpassingen. We bekijken dit opnieuw in volgende sprint met Twinfield aanpassingen
+//            foreach ($administration->twinfieldNumbers as $twinfieldNumber) {
+//                $twinfieldNumber->delete();
+//            }
+//
+//            foreach ($administration->invoices()->whereNotNull('twinfield_number')->where('status_id', 'exported')->get() as $invoice) {
+//                $invoice->twinfield_number = null;
+//                $invoice->status_id = 'sent';
+//                $invoice->save();
+//            }
+//
+//            foreach ($administration->invoices()->whereNotNull('twinfield_number')->get() as $invoice) {
+//                $invoice->twinfield_number = null;
+//                $invoice->save();
+//            }
+//
+//            //alle contacten als klanten in Twinfield zetten
+//            $twinfieldCustomerHelper = new TwinfieldCustomerHelper($administration, null);
+//            $twinfieldCustomerHelper->createAllCustomers();
         }
     }
 }

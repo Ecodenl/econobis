d9a4b7cabc60af858bc92bd5146f02dd0eebe162
robrollenberg <rob@jepr.nl>
2019-05-14 16:32:42 +0200

    Added loan project type for new participant

--- client-app/src/container/participant-project/new/ParticipantSubmitHelper.js
+++ client-app/src/container/participant-project/new/ParticipantSubmitHelper.js
@@ -1,31 +1,48 @@
-export default function(participation, statusCodeRef) {
+export default function(participation, statusCodeRef, projectTypeCodeRef) {
     const values = {
         contactId: participation.contactId,
         statusId: participation.statusId,
         projectId: participation.projectId,
     };
 
-    if (statusCodeRef === 'interest') {
-        values.quantityInterest = participation.quantityInterest;
-        values.dateInterest = participation.dateInterest;
-    }
-
-    if (statusCodeRef === 'option') {
-        values.quantityOption = participation.quantityOption;
-        values.dateOption = participation.dateOption;
-    }
-
-    if (statusCodeRef === 'granted') {
-        values.quantityGranted = participation.quantityGranted;
-        values.dateGranted = participation.dateGranted;
-    }
-
-    if (statusCodeRef === 'final') {
-        values.quantityFinal = participation.quantityFinal;
-        values.dateGranted = participation.dateGranted;
-        values.dateContractRetour = participation.dateContractRetour;
-        values.datePayment = participation.datePayment;
-        values.dateEntry = participation.dateEntry;
+    switch (statusCodeRef) {
+        case 'interest':
+            values.dateInterest = participation.dateInterest;
+            if (projectTypeCodeRef === 'loan') {
+                values.amountInterest = participation.amountInterest;
+            } else {
+                values.quantityInterest = participation.quantityInterest;
+            }
+            break;
+        case 'option':
+            values.dateOption = participation.dateOption;
+            if (projectTypeCodeRef === 'loan') {
+                values.amountOption = participation.amountOption;
+            } else {
+                values.quantityOption = participation.quantityOption;
+            }
+            break;
+        case 'granted':
+            values.dateGranted = participation.dateGranted;
+            if (projectTypeCodeRef === 'loan') {
+                values.amountGranted = participation.amountGranted;
+            } else {
+                values.quantityGranted = participation.quantityGranted;
+            }
+            break;
+        case 'final':
+            values.dateGranted = participation.dateGranted;
+            values.dateContractRetour = participation.dateContractRetour;
+            values.datePayment = participation.datePayment;
+            values.dateEntry = participation.dateEntry;
+            if (projectTypeCodeRef === 'loan') {
+                values.amountFinal = participation.amountFinal;
+            } else {
+                values.quantityFinal = participation.quantityFinal;
+            }
+            break;
+        default:
+            break;
     }
 
     return values;

6f1858029a376232c5d6fa16d5ac0cccc32edcff
robrollenberg <rob@jepr.nl>
2019-05-22 14:22:06 +0200

    Added compiled js

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,5 +19,5 @@
 </script>
 
 <div id="root"></div>
-<script type="text/javascript" src="./js/vendors~bundle.79780f989aa0924de5da.js"></script><script type="text/javascript" src="./js/bundle.f8420eaa91a1a75870f4.js"></script></body>
+<script type="text/javascript" src="./js/vendors~bundle.79780f989aa0924de5da.js"></script><script type="text/javascript" src="./js/bundle.93423bb94f6a4b12f852.js"></script></body>
 </html>

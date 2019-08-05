4a9a1357de4f3efd017979b965b5e7f7407a4c3c
Wim <wim@mosmania.nl>
2019-08-05 15:44:18 +0200

    npm build

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,5 +19,5 @@
 </script>
 
 <div id="root"></div>
-<script type="text/javascript" src="./js/vendors~bundle.1aa387c28aa09c149ebc.js"></script><script type="text/javascript" src="./js/bundle.21c067259591a53b0048.js"></script></body>
+<script type="text/javascript" src="./js/vendors~bundle.56fa349af2c3a6a1f659.js"></script><script type="text/javascript" src="./js/bundle.7a73d22e372d3dd3e9dd.js"></script></body>
 </html>

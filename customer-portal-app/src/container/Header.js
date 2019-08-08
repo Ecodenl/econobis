805f778fbf1f1b6f2c06aae3c495222fd7b7787b
Rob Rollenberg <rob@jepr.nl>
2019-08-08 16:20:51 +0200

    Added predefined elements in the header

--- customer-portal-app/src/container/Header.js
+++ customer-portal-app/src/container/Header.js
@@ -1,43 +1,22 @@
-import React from 'react'
-import { AuthConsumer } from '../context/AuthContext'
-import { Link } from 'react-router-dom'
-
-const headerStyle = {
-    display: 'flex',
-    backgroundColor: '#26c6da',
-    justifyContent: 'space-between',
-    padding: 10
-}
-
-const linkStyle = {
-    color: 'white',
-    textDecoration: 'underline'
-}
+import React from 'react';
+import { AuthConsumer } from '../context/AuthContext';
+import { Link } from 'react-router-dom';
 
 export default () => (
     <header>
         <AuthConsumer>
             {({ isAuth, login, logout }) => {
                 return (
-                <div style={headerStyle}>
-                    <h3>
-                        <Link style={linkStyle} to="/">
-                            HOME
-                        </Link>
-                    </h3>
-
-                    {isAuth ? (
-                        <ul>
-                            <Link style={linkStyle} to="/dashboard">
-                                Dashboard
-                            </Link>
-                            <button onClick={logout}>logout</button>
-                        </ul>
-                    ) : (
-                        <button onClick={login}>login</button>
-                    )}
-                </div>
-            )}}
+                    <div className="header-deltaw">
+                        <div className="profile-pic" />
+                        {isAuth ? (
+                            <ul>
+                                <button onClick={logout}>logout (temp)</button>
+                            </ul>
+                        ) : null}
+                    </div>
+                );
+            }}
         </AuthConsumer>
     </header>
-)
+);

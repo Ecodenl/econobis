741aab77db459f597fdc9056d0ff7fae48294822
robrollenberg <rob@jepr.nl>
2019-08-09 11:42:19 +0200

    Added select input component

---
+++ customer-portal-app/src/components/form/Select.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Select = ({ field, className, id, options, optionLabel, error, emptyOption, placeholder }) => {
+    return (
+        <select className={className} id={id} {...field} error={error}>
+            {emptyOption ? <option value="">{placeholder ? `-- ${placeholder} --` : ''}</option> : null}
+            {options.map(option => {
+                return (
+                    <option key={option.id} value={option.id}>
+                        {option[optionLabel]}
+                    </option>
+                );
+            })}
+        </select>
+    );
+};
+
+Select.defaultProps = {
+    className: 'select-field w-select',
+    readOnly: false,
+    required: '',
+    error: false,
+    optionLabel: 'name',
+    emptyOption: true,
+    placeholder: '',
+};
+
+Select.propTypes = {
+    field: PropTypes.object.isRequired,
+    className: PropTypes.string,
+    id: PropTypes.string,
+    options: PropTypes.array.isRequired,
+    optionLabel: PropTypes.string,
+    error: PropTypes.bool,
+    emptyOption: PropTypes.bool,
+    placeholder: PropTypes.string,
+};
+
+export default Select;

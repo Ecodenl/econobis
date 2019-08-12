9362a7a3b576de58478cf2406332da9c40ded12a
Wim <wim@mosmania.nl>
2019-08-12 16:34:48 +0200

    Changes to conversions for project version.

--- database/migrations/2019_08_06_104016_add_current_obligations_pcr_loan_to_contact_table.php
+++ database/migrations/2019_08_06_104016_add_current_obligations_pcr_loan_to_contact_table.php
@@ -19,37 +19,6 @@ class AddCurrentObligationsPcrLoanToContactTable extends Migration
             $table->integer('postalcode_link_capital_current')->default(0)->after('participations_current');
         });
 
-        $contacts = \App\Eco\Contact\Contact::all();
-
-        foreach ($contacts as $contact){
-            $obligations = 0;
-            $participations = 0;
-            $pcr = 0;
-            $loan = 0;
-
-            foreach ($contact->participations as $participation){
-                $projectCodeRef = $participation->project->projectType->code_ref;
-                switch ($projectCodeRef) {
-                    case 'obligation':
-                        $obligations += $participation->participations_definitive;
-                        break;
-                    case 'capital':
-                        $participations += $participation->participations_definitive;
-                        break;
-                    case 'postalcode_link_capital':
-                        $pcr += $participation->participations_definitive;
-                        break;
-                    case 'loan':
-                        $loan += $participation->amount_definitive;
-                        break;
-                }
-            }
-            $contact->obligations_current = $obligations;
-            $contact->participations_current = $participations;
-            $contact->postalcode_link_capital_current = $pcr;
-            $contact->loan_current = $loan;
-            $contact->save();
-        }
     }
 
     /**
@@ -59,10 +28,10 @@ class AddCurrentObligationsPcrLoanToContactTable extends Migration
      */
     public function down()
     {
-        Schema::table('contacts', function (Blueprint $table) {
-            $table->dropColumn('obligations_current');
-            $table->dropColumn('postalcode_link_capital_current');
-            $table->dropColumn('loan_current');
-        });
+//        Schema::table('contacts', function (Blueprint $table) {
+//            $table->dropColumn('obligations_current');
+//            $table->dropColumn('postalcode_link_capital_current');
+//            $table->dropColumn('loan_current');
+//        });
     }
 }

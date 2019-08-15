a913a8557042f9675be0cee248d84f537f0cab78
Rob Rollenberg <rob@jepr.nl>
2019-08-15 13:22:09 +0200

    In project details only basic information from project revenue is visible

---
+++ app/Http/Resources/Project/GridProjectRevenue.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Resources\Project;
+
+use App\Http\Resources\EnumWithIdAndName\FullEnumWithIdAndName;
+use App\Http\Resources\GenericResource;
+use App\Http\Resources\User\FullUser;
+use Illuminate\Http\Resources\Json\Resource;
+
+class GridProjectRevenue extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request
+     *
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return
+            [
+                'id' => $this->id,
+                'typeId' => $this->type_id,
+                'type' => GenericResource::make($this->whenLoaded('type')),
+                'projectId' => $this->project_id,
+                'confirmed' => $this->confirmed,
+                'dateBegin' => $this->date_begin,
+                'dateEnd' => $this->date_end,
+                'dateReference' => $this->date_reference,
+                'dateConfirmed' => $this->date_confirmed,
+                'payoutTypeId' => $this->payout_type_id,
+                'kwhResult' => $this->kwh_result,
+                'revenue' => $this->revenue,
+                'datePayed' => $this->date_payed,
+                'categoryId' => $this->category_id,
+                'category' => GenericResource::make($this->whenLoaded('category')),
+                'payoutKwh' => $this->payout_kwh,
+            ];
+    }
+}

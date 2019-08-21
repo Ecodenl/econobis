e4e22fb28d1d0877efc979b746b84a8dfc682335
Rob Rollenberg <rob@jepr.nl>
2019-08-21 08:12:36 +0200

    Rebuild js

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,5 +19,5 @@
 </script>
 
 <div id="root"></div>
-<script type="text/javascript" src="./js/vendors~bundle.56fa349af2c3a6a1f659.js"></script><script type="text/javascript" src="./js/bundle.06ffd00eeb1b89fe7f48.js"></script></body>
+<script type="text/javascript" src="./js/vendors~bundle.56fa349af2c3a6a1f659.js"></script><script type="text/javascript" src="./js/bundle.5193cb603ca905c32bbc.js"></script></body>
 </html>

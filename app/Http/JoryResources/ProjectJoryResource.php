ff2b28a9e84a2105ec8ab825a5638f4694dee3a5
robrollenberg <rob@jepr.nl>
2019-08-30 09:36:44 +0200

    Changed visible fields for portal user

--- app/Http/JoryResources/ProjectJoryResource.php
+++ app/Http/JoryResources/ProjectJoryResource.php
@@ -17,55 +17,30 @@ class ProjectJoryResource extends JoryResource
     protected function configureForPortal(): void
     {
         // Fields
-        $this->field('address')->filterable()->sortable();
-        $this->field('administration_id')->filterable()->sortable();
         $this->field('amount_definitive')->filterable()->sortable();
         $this->field('amount_granted')->filterable()->sortable();
         $this->field('amount_interessed')->filterable()->sortable();
         $this->field('amount_of_loan_needed')->filterable()->sortable();
         $this->field('amount_optioned')->filterable()->sortable();
-        $this->field('city')->filterable()->sortable();
         $this->field('code')->filterable()->sortable();
-        $this->field('created_at')->filterable()->sortable();
-        $this->field('created_by_id')->filterable()->sortable();
         $this->field('date_end')->filterable()->sortable();
         $this->field('date_end_registrations')->filterable()->sortable();
-        $this->field('date_entry')->filterable()->sortable();
-        $this->field('date_interest_bearing')->filterable()->sortable();
-        $this->field('date_interest_bearing_kwh')->filterable()->sortable();
         $this->field('date_production')->filterable()->sortable();
         $this->field('date_start')->filterable()->sortable();
         $this->field('date_start_registrations')->filterable()->sortable();
-        $this->field('deleted_at')->filterable()->sortable();
         $this->field('description')->filterable()->sortable();
-        $this->field('ean')->filterable()->sortable();
-        $this->field('ean_manager')->filterable()->sortable();
-        $this->field('ean_supply')->filterable()->sortable();
         $this->field('id')->filterable()->sortable();
-        $this->field('is_membership_required')->filterable()->sortable();
-        $this->field('is_participation_transferable')->filterable()->sortable();
-        $this->field('kwh_start_high_next_revenue')->filterable()->sortable();
-        $this->field('kwh_start_low_next_revenue')->filterable()->sortable();
         $this->field('max_participations')->filterable()->sortable();
         $this->field('max_participations_youth')->filterable()->sortable();
         $this->field('min_participations')->filterable()->sortable();
         $this->field('name')->filterable()->sortable();
-        $this->field('owned_by_id')->filterable()->sortable();
         $this->field('participation_worth')->filterable()->sortable();
         $this->field('participations_definitive')->filterable()->sortable();
         $this->field('participations_granted')->filterable()->sortable();
         $this->field('participations_interessed')->filterable()->sortable();
         $this->field('participations_optioned')->filterable()->sortable();
         $this->field('postal_code')->filterable()->sortable();
-        $this->field('postalcode_link')->filterable()->sortable();
-        $this->field('power_kw_available')->filterable()->sortable();
-        $this->field('project_status_id')->filterable()->sortable();
         $this->field('project_type_id')->filterable()->sortable();
-        $this->field('tax_referral')->filterable()->sortable();
-        $this->field('total_participations')->filterable()->sortable();
-        $this->field('updated_at')->filterable()->sortable();
-        $this->field('updated_by_id')->filterable()->sortable();
-        $this->field('warranty_origin')->filterable()->sortable();
 
         // Relations
 //        $this->relation('administration');

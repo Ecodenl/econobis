dfda84989f03be11a4e1c4b65bd3862e7f8eb148
Wim <wim@mosmania.nl>
2019-09-04 13:23:08 +0200

    Register step Three changed. Make checkboxes on/off working.

--- customer-portal-app/public/js/server-data.js
+++ customer-portal-app/public/js/server-data.js
@@ -1,5 +1,7 @@
 window.__SERVER_DATA__ = {
-    base_url: "http://localhost:8888/econobis/public/portal",
+    base_url: 'http://framework/econobis/public/portal',
     client_id: 2,
-    client_key: "sdzX088pM1j3IyrzMHwcdNvAtrC134Z0eRjXkz3y"
+    client_key: 'sdzX088pM1j3IyrzMHwcdNvAtrC134Z0eRjXkz3y',
+    // client_key: "sdzX088pM1j3IyrzMHwcdNvAtrC134Z0eRjXkz3y"
+    // client_key: "8bJx8qGqAn1Wr9pxleYVOMwWUJhk8Qk7ImGzKpkh"
 };
--- customer-portal-app/src/container/register/capital/steps/StepThree.js
+++ customer-portal-app/src/container/register/capital/steps/StepThree.js
@@ -2,55 +2,86 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { Field, Formik } from 'formik';
+import * as Yup from 'yup';
 
-function StepThree({ previous, next }) {
-    const field = {value: 0};
+function StepThree({ previous, next, initialRegisterValues, handleSubmitRegisterValues }) {
+    const validationSchema = Yup.object({
+        didAgreeTerms: Yup.boolean(),
+        didUnderstandInfo: Yup.boolean(),
+    });
+    // todo controle op booleans allebei aangevinkt voordat je verder mag
 
     return (
-        <div>
-            <div className="w-row">
-                <div className="w-col w-col-6">
-                    <p>
-                        Om deel te kunnen nemen dien je akkoord te gaan met de algemene voorwaarden en dien je te bevestigen dat je de projectinformatie hebt gelezen en begrepen.
-                    </p>
-                    <label className="w-checkbox checkbox-fld">
-                        <input
-                            type="checkbox"
-                            {...field}
-                            id="did_agree_avg"
-                            checked={field.value}
-                            className="w-checkbox-input checkbox"
-                        />
-                        <span htmlFor="did_agree_avg" className="checkbox-label w-form-label">
-                                        Ik ga akkoord met de <a href='#'>voorwaarden</a>
-                                                    </span>
-                    </label>
+        <Formik
+            validationSchema={validationSchema}
+            onSubmit={function(values, actions) {
+                handleSubmitRegisterValues(values);
+                next();
+            }}
+            initialValues={initialRegisterValues}
+        >
+            {({ handleSubmit, handleChange, handleBlur, values, touched, isValid, errors }) => (
+                <>
+                    <div>
+                        <div className="w-row">
+                            <div className="w-col w-col-6">
+                                <p>
+                                    Om deel te kunnen nemen dien je akkoord te gaan met de algemene voorwaarden en dien
+                                    je te bevestigen dat je de projectinformatie hebt gelezen en begrepen.
+                                </p>
+                                <Field
+                                    name="didAgreeTerms"
+                                    render={({ field }) => (
+                                        <label className="w-checkbox checkbox-fld">
+                                            <input
+                                                type="checkbox"
+                                                {...field}
+                                                id="did_agree_terms"
+                                                checked={field.value}
+                                                className="w-checkbox-input checkbox"
+                                            />
+                                            <span htmlFor="did_agree_terms" className="checkbox-label w-form-label">
+                                                Ik ga akkoord met de <a href="#">voorwaarden</a>
+                                            </span>
+                                        </label>
+                                    )}
+                                />
 
-                    <label className="w-checkbox checkbox-fld">
-                        <input
-                            type="checkbox"
-                            {...field}
-                            id="did_agree_avg"
-                            checked={field.value}
-                            className="w-checkbox-input checkbox"
-                        />
-                        <span htmlFor="did_agree_avg" className="checkbox-label w-form-label">
-                                        Ik heb de <a href='#'>projectinformatie</a> (inclusief de daarin beschreven risico’s) behorende bij het project gelezen en begrepen
-                                                    </span>
-                    </label>
-                </div>
-            </div>
-            <Row className="justify-content-end">
-                <ButtonGroup aria-label="Steps">
-                    <Button className={'w-button'} size="sm" onClick={previous}>
-                        Terug
-                    </Button>
-                    <Button className={'w-button'} size="sm" onClick={next}>
-                        Ga naar inschrijfformulier
-                    </Button>
-                </ButtonGroup>
-            </Row>
-        </div>
+                                <Field
+                                    name="didUnderstandInfo"
+                                    render={({ field }) => (
+                                        <label className="w-checkbox checkbox-fld">
+                                            <input
+                                                type="checkbox"
+                                                {...field}
+                                                id="did_understand_info"
+                                                checked={field.value}
+                                                className="w-checkbox-input checkbox"
+                                            />
+                                            <span htmlFor="did_understand_info" className="checkbox-label w-form-label">
+                                                Ik heb de <a href="#">projectinformatie</a> (inclusief de daarin
+                                                beschreven risico’s) behorende bij het project gelezen en begrepen
+                                            </span>
+                                        </label>
+                                    )}
+                                />
+                            </div>
+                        </div>
+                        <Row className="justify-content-end">
+                            <ButtonGroup aria-label="Steps">
+                                <Button className={'w-button'} size="sm" onClick={previous}>
+                                    Terug
+                                </Button>
+                                <Button className={'w-button'} size="sm" onClick={handleSubmit}>
+                                    Ga naar inschrijfformulier
+                                </Button>
+                            </ButtonGroup>
+                        </Row>
+                    </div>
+                </>
+            )}
+        </Formik>
     );
 }
 

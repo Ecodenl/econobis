26f36482f3c984c23017958b4e64c9c1505cf705
Rob Rollenberg <rob@jepr.nl>
2019-09-18 12:19:45 +0200

    Error message is a fixed message

--- customer-portal-app/src/container/authorization/login/index.js
+++ customer-portal-app/src/container/authorization/login/index.js
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
-import { Redirect } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 
-import { AuthConsumer } from '../../context/AuthContext';
-import LogoImage from '../../images/logo.png';
+import { AuthConsumer } from '../../../context/AuthContext';
+import LogoImage from '../../../images/logo.png';
 import LoginForm from './Form';
-import AuthAPI from '../../api/auth/AuthAPI';
+import AuthAPI from '../../../api/auth/AuthAPI';
 import Alert from 'react-bootstrap/Alert';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 export default props => {
-    const [error, setError] = useState('');
+    const [showError, toggleError] = useState(false);
 
     const [redirectToReferrer, toggleRedirect] = useState(false);
     let { from } = props.location.state || { from: { pathname: '/gegevens' } };
@@ -19,11 +19,12 @@ export default props => {
     function handleSubmit(values, actions, login) {
         AuthAPI.login(values)
             .then(payload => {
+                toggleError(false);
                 login(payload.data, () => toggleRedirect(true));
             })
             .catch(error => {
                 // If login fails show error and then set submitting back to false
-                setError('Gebruikte logingegevens zijn onjuist');
+                toggleError(true);
                 actions.setSubmitting(false);
             });
     }
@@ -47,15 +48,15 @@ export default props => {
                                 <Col xs="12" sm="6" md="4" lg="3" xl="2">
                                     <img src={LogoImage} alt="" className="image" />
                                     <LoginForm handleSubmit={handleSubmit} login={login} />
-                                    {error ? (
+                                    {showError ? (
                                         <Alert className={'p-1 m-1 text-danger'} variant={'danger'}>
-                                            {error}
+                                            Gebruikte logingegevens zijn onjuist!
                                         </Alert>
                                     ) : null}
                                     <Row className="justify-content-center">
-                                        <a href="wachtwoord-vergeten.html" className="login-link">
+                                        <Link to={'/wachtwoord-vergeten'} className="login-link">
                                             Wachtwoord vergeten?
-                                        </a>
+                                        </Link>
                                     </Row>
                                 </Col>
                             </Row>

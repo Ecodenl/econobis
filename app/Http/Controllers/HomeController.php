721777806f593741654b8966b882e71a4ad4dea8
Wim <wim@mosmania.nl>
2020-02-10 09:40:59 +0100

    npm build client-app for Laravel 6 test, build version Fixed error HomeController for welcome blade. Version set to 3.2.0.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,11 +8,10 @@ class HomeController extends Controller
 {
     public function welcome()
     {
-        $clientKey = ['clientKey' => DB::table('oauth_clients')->where('id', config('app.oauth_client_id'))->first()->secret];
+        $clientKey = DB::table('oauth_clients')->where('id', config('app.oauth_client_id'))->first()->secret;
         
-        return view('welcome')
-            ->with(compact([
-                'clientKey'
-            ]));
+        return view('welcome', [
+            'clientKey' => $clientKey
+        ]);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,5 +19,5 @@
 </script>
 
 <div id="root"></div>
-<script type="text/javascript" src="./js/vendors~bundle.4ff3c61f92c949eee71b.js"></script><script type="text/javascript" src="./js/bundle.4591bfbf7b38d8b5af6b.js"></script></body>
+<script type="text/javascript" src="./js/vendors~bundle.cc603c114d7ea55ee105.js"></script><script type="text/javascript" src="./js/bundle.19ebec4f0ef946b5eac3.js"></script></body>
 </html>

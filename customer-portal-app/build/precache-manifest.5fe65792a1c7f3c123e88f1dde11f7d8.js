90b76ee8e0417b9a8c41abb07cb4e86211743aed
Wim <wim@mosmania.nl>
2020-03-02 12:35:27 +0100

    npm build customer-portal-app

---
+++ customer-portal-app/build/precache-manifest.5fe65792a1c7f3c123e88f1dde11f7d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d739bcd0b8b60c1a4584bb8de322804",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e46f21dd2fc62fa3acaf",
+    "url": "./static/css/2.3efbf22a.chunk.css"
+  },
+  {
+    "revision": "214f218a943362effe26",
+    "url": "./static/css/main.72fcfd7f.chunk.css"
+  },
+  {
+    "revision": "e46f21dd2fc62fa3acaf",
+    "url": "./static/js/2.8b13c060.chunk.js"
+  },
+  {
+    "revision": "5745072e9e581138ba1885b5c1a6cab6",
+    "url": "./static/js/2.8b13c060.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "214f218a943362effe26",
+    "url": "./static/js/main.d244bc93.chunk.js"
+  },
+  {
+    "revision": "11374bf3899fe6465ae3",
+    "url": "./static/js/runtime-main.66dc3cad.js"
+  },
+  {
+    "revision": "9de140622de93832ef8b9926b088c35b",
+    "url": "./static/media/logo.9de14062.png"
+  }
+]);
--- customer-portal-app/build/service-worker.js
+++ customer-portal-app/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.78f4b5cc7c3878e0f60cb1527ac9c9de.js"
+  "./precache-manifest.5fe65792a1c7f3c123e88f1dde11f7d8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

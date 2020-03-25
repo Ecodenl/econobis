28a392f18a5bb95c985c5828dc9680f24da4a1f4
Wim <wim@mosmania.nl>
2020-03-25 08:53:13 +0100

    npm build client-app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,5 +19,5 @@
 </script>
 
 <div id="root"></div>
-<script type="text/javascript" src="./js/vendors~bundle.7dacbd3431ed57c1e29b.js"></script><script type="text/javascript" src="./js/bundle.f516433d9fa7dd63c915.js"></script></body>
+<script type="text/javascript" src="./js/vendors~bundle.612a667c07607d9c7576.js"></script><script type="text/javascript" src="./js/bundle.3d3bc9ebaf882e6fad2b.js"></script></body>
 </html>

79f003e161e1afb2321e0e4c248f2ff58a0f3f2a
Wim <wim@mosmania.nl>
2020-05-07 14:39:18 +0200

    When creating document do also translate 'mutaties' mergefields.

--- app/Helpers/Template/TemplateVariableHelper.php
+++ app/Helpers/Template/TemplateVariableHelper.php
@@ -1648,6 +1648,7 @@ class TemplateVariableHelper
         $html = TemplateVariableHelper::replaceTemplateVariables($html, 'taak', $document->task);
         $html = TemplateVariableHelper::replaceTemplateVariables($html, 'project', $document->project);
         $html = TemplateVariableHelper::replaceTemplateVariables($html, 'deelname', $document->participant);
+        $html = TemplateVariableHelper::replaceTemplateVariables($html, 'mutaties', $document->participant->mutations);
         $html = TemplateVariableHelper::replaceTemplateVariables($html, 'order', $document->order);
 
         //Als laatste verwijder alle niet bestaande tags

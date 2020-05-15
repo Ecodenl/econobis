9e9ac64e21afa7209fabec777dda8b4851ecc367
Wim <wim@mosmania.nl>
2020-05-15 12:10:45 +0200

    Carbon create from format: 'd-m-Y H:i:s e+' didn't work. Other attemp: when parse date didn't work first time, try without (GMT+02:00).

--- app/Eco/Mailbox/MailFetcher.php
+++ app/Eco/Mailbox/MailFetcher.php
@@ -170,9 +170,9 @@ class MailFetcher
             $dateSent = Carbon::parse( $emailData->date ) ;
         } catch(\Exception $ex) {
             try {
-                $dateSent = Carbon::createFromFormat( 'd-m-Y H:i:s e+', $emailData->date ) ;
+                $dateSent = Carbon::parse( str_replace(" (GMT+02:00)", "", $emailData->date) );
             } catch(\Exception $ex2) {
-                Log::error("Failed to retrieve date sent (" . $emailData->date . ")from email: " . $ex2->getMessage());
+                Log::error("Failed to retrieve date sent (" . $emailData->date . ") from email (" . $emailData->id . ") in mailbox (" . $this->mailbox->id . "). Error: " . $ex2->getMessage());
                 echo "Failed to retrieve date sent from email: " . $ex2->getMessage();
                 die();
             }

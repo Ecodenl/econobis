2181eb5771173b9717a4838d0df50217eb3db588
Wim <wim@mosmania.nl>
2020-08-06 09:37:47 +0200

    Build client-app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,5 +19,5 @@
 </script>
 
 <div id="root"></div>
-<script type="text/javascript" src="./js/vendors~bundle.7c01d274e1afdee52cb6.js"></script><script type="text/javascript" src="./js/bundle.9b942f45a90ecc335a71.js"></script></body>
+<script type="text/javascript" src="./js/vendors~bundle.7c01d274e1afdee52cb6.js"></script><script type="text/javascript" src="./js/bundle.84c112d9a814dbab29a2.js"></script></body>
 </html>

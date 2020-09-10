e33214cf78d5386ed81d552d099985b9dc4907ed
Wim <wim@mosmania.nl>
2020-09-10 15:12:58 +0200

    Build client-app and set version to 3.3.1.10

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,5 +19,5 @@
 </script>
 
 <div id="root"></div>
-<script type="text/javascript" src="./js/vendors~bundle.7c01d274e1afdee52cb6.js"></script><script type="text/javascript" src="./js/bundle.8d1fa0dacd2c9a581a3e.js"></script></body>
+<script type="text/javascript" src="./js/vendors~bundle.002416e200e595c935e3.js"></script><script type="text/javascript" src="./js/bundle.e00420940493896d841f.js"></script></body>
 </html>

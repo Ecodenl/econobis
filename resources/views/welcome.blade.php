36387ce0b0878aef516616961a0e6c0e54649bac
Wim <wim@mosmania.nl>
2020-10-07 15:39:10 +0200

    npm build client-app en set version to 3.3.1.17

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,5 +19,5 @@
 </script>
 
 <div id="root"></div>
-<script type="text/javascript" src="./js/vendors~bundle.002416e200e595c935e3.js"></script><script type="text/javascript" src="./js/bundle.4996c8413914fe548e98.js"></script></body>
+<script type="text/javascript" src="./js/vendors~bundle.7c01d274e1afdee52cb6.js"></script><script type="text/javascript" src="./js/bundle.4320404f3f33c84de0b8.js"></script></body>
 </html>

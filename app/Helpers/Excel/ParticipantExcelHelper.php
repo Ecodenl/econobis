f7d6c8c3237273c41e59200ef09c6cd0e7fd4224
samjurriaans <sam.jurriaans1991@gmail.com>
2020-11-12 11:37:17 +0100

    added unique mutationstatusses as participationstatus field

--- app/Helpers/Excel/ParticipantExcelHelper.php
+++ app/Helpers/Excel/ParticipantExcelHelper.php
@@ -364,6 +364,7 @@ class ParticipantExcelHelper
 
                 $rowData[93] = "";
                 $rowData[94] = "";
+                $rowData[95] = $participant->getUniqueMutationStatusesAttribute();
                 $rowData[96] = $participant->participations_interessed;
                 $rowData[97] = $participant->amount_interessed;
                 $rowData[98] = "";

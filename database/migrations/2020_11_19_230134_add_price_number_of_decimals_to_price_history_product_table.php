354b75c01816e99829c57748969b0d5972817982
Mosmania <wim@mosmania.nl>
2020-11-27 15:09:50 +0100

    No conversion for price_number_of_decimals. Default always 2.

--- database/migrations/2020_11_19_230134_add_price_number_of_decimals_to_price_history_product_table.php
+++ database/migrations/2020_11_19_230134_add_price_number_of_decimals_to_price_history_product_table.php
@@ -16,18 +16,6 @@ class AddPriceNumberOfDecimalsToPriceHistoryProductTable extends Migration
         Schema::table('price_history_product', function (Blueprint $table) {
             $table->tinyInteger('price_number_of_decimals')->default(2)->after('input_incl_vat');
         });
-        $prices = \App\Eco\Product\PriceHistory::all();
-        foreach ($prices as $price){
-            $decimals = strlen(substr(strrchr($price->price_incl_vat, "."), 1));
-            if($decimals<2){
-                $decimals = 2;
-            }
-            if($decimals>6){
-                $decimals = 6;
-            }
-            $price->price_number_of_decimals = $decimals;
-            $price->save();
-        }
 
     }
 

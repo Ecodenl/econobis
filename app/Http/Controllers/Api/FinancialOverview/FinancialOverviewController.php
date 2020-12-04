ccd83bf0d058f37252ceb7109b5b189e58e20d2a
Mosmania <wim@mosmania.nl>
2020-12-04 17:09:33 +0100

    Started with procedure calculate participation values (start en end)

--- app/Http/Controllers/Api/FinancialOverview/FinancialOverviewController.php
+++ app/Http/Controllers/Api/FinancialOverview/FinancialOverviewController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Api\FinancialOverview;
 
 use App\Eco\FinancialOverview\FinancialOverview;
+use App\Eco\FinancialOverview\FinancialOverviewParticipantProject;
 use App\Eco\FinancialOverview\FinancialOverviewProject;
+use App\Eco\Project\ProjectType;
 use App\Helpers\Delete\Models\DeleteFinancialOverview;
 use App\Helpers\FinancialOverview\FinancialOverviewHelper;
 use App\Helpers\RequestInput\RequestInput;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\GenericResource;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use JosKolenberg\LaravelJory\Facades\Jory;
@@ -79,17 +82,57 @@ class FinancialOverviewController extends Controller
     {
         $projects = $this->getNewProjectsForFinancialOverview($financialOverview);
 
-        foreach ($projects as $project){
-            FinancialOverviewProject::create([
+        foreach ($projects as $project) {
+            $financialOverviewProject = FinancialOverviewProject::create([
                 'financial_overview_id' => $financialOverview->id,
                 'project_id' => $project->id,
                 'definitive' => false,
-            ]);        }
+            ]);
+
+            $participants = $project->participantsProject;
+
+            foreach ($participants as $participant) {
+                //todo WM: hier bepalen start_value en end_value
+                $startValue = $this->calculateParticipationsValue($participant, Carbon::createFromDate($financialOverview->year, 1, 1));
+                $endValue = $this->calculateParticipationsValue($participant, Carbon::createFromDate($financialOverview->year, 12, 31));
+
+                FinancialOverviewParticipantProject::create([
+                    'financial_overview_project_id' => $financialOverviewProject->id,
+                    'participant_project_id' => $participant->id,
+                    'start_value' => $startValue,
+                    'end_value' => $endValue,
+                ]);
+
+            }
+        }
     }
 
     public function getNewProjectsForFinancialOverview(FinancialOverview $financialOverview)
     {
         return FinancialOverviewHelper::getNewProjectsForFinancialOverview($financialOverview);
+    }
+
+    protected function calculateParticipationsValue($participant, $dateReference)
+    {
+        $projectTypeCodeRef = (ProjectType::where('id', $participant->project->project_type_id)->first())->code_ref;
+
+        $mutations = $participant->mutationsDefinitive()
+            ->whereDate('date_entry', '<=', $dateReference);
+
+        $participationsValue = 0;
+
+        if($projectTypeCodeRef === 'obligation' || $projectTypeCodeRef === 'capital' || $projectTypeCodeRef === 'postalcode_link_capital') {
+            $measureType = 'quantity';
+        }
+
+        if($projectTypeCodeRef === 'loan') {
+            $measureType = 'amount';
+        }
+
+        foreach ($mutations->get() as $mutation) {
+            $participationsValue += $mutation[$measureType] ;
+        }
+        return $participationsValue;
     }
 
 

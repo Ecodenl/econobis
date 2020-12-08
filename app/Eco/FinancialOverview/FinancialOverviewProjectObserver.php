f369d37a584e28704c86a8c079b8226b466921c2
Mosmania <wim@mosmania.nl>
2020-12-08 13:52:34 +0100

    When after deleting financial overview project there are no more concept remaining financial overview (remaining all definitive), then set financial overview also definitive = true

--- app/Eco/FinancialOverview/FinancialOverviewProjectObserver.php
+++ app/Eco/FinancialOverview/FinancialOverviewProjectObserver.php
@@ -25,4 +25,23 @@ class FinancialOverviewProjectObserver
             $financialOverview->save();
         }
     }
+    public function deleted(FinancialOverviewProject $financialOverviewProject)
+    {
+        $financialOverviewId = $financialOverviewProject->financial_overview_id;
+
+        $numberOfFOProjects = FinancialOverviewProject::where('financial_overview_id', $financialOverviewId)->count();
+
+        // if there are remaining financial overview projects
+        if($numberOfFOProjects > 0){
+            // and they are all set to definitive, than set financial overview also to definitive
+            $numberOfFOProjectsNotDefinitive = FinancialOverviewProject::where('financial_overview_id', $financialOverviewId)
+                ->where('definitive', false)->count();
+            if($numberOfFOProjectsNotDefinitive == 0) {
+                $financialOverviewDefinitive = true;
+                $financialOverview = FinancialOverview::find($financialOverviewId);
+                $financialOverview->definitive = $financialOverviewDefinitive;
+                $financialOverview->save();
+            }
+        }
+    }
 }

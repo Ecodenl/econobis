b53bee6b11608cfba080ce2fd32633fe2934beda
Mosmania <wim@mosmania.nl>
2020-12-08 10:44:46 +0100

    Cleunup some unnecessary code

--- client-app/src/container/financial/overview/details/FinancialOverviewDetailsForm.js
+++ client-app/src/container/financial/overview/details/FinancialOverviewDetailsForm.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import { isEmpty } from 'lodash';
 
 import FinancialOverviewDetailsFormGeneral from './general/FinancialOverviewDetailsFormGeneral';

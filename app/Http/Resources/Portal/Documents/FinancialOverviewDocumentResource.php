65ce7f9d8b53ee26401e22d8a96cd68cacd63bd4
Mosmania <wim@mosmania.nl>
2020-12-16 13:53:33 +0100

    Created portal resource and route voor financial overview documents contact.

---
+++ app/Http/Resources/Portal/Documents/FinancialOverviewDocumentResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\Portal\Documents;
+
+use Illuminate\Http\Resources\Json\Resource;
+
+class FinancialOverviewDocumentResource extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request
+     *
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'description' => $this->financialOverview->description,
+            'name' => $this->name,
+            'filename' => $this->filename,
+            'dateSent' => $this->date_sent,
+        ];
+    }
+}
--- routes/portal.php
+++ routes/portal.php
@@ -22,6 +22,7 @@ Route::middleware(['auth:api', 'scopes:use-portal'])
         Route::post('/portal-user/change-password', 'PortalUser\PortalUserController@changePassword');
 
         Route::post('/contact/{contact}', 'Contact\ContactController@update');
+        Route::get('/contact/{contact}/financial-overview-documents', 'Contact\ContactController@financialOverviewDocuments');
 
         Route::post('/contact/{contact}/{project}/preview-document', 'Contact\ContactController@previewDocument');
 

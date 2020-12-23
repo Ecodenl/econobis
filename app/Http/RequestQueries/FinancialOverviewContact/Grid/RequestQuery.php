d817f93ba131ea97f02e7a97049f01561053e4ee
Mosmania <wim@mosmania.nl>
2020-12-23 09:43:59 +0100

    For resending after error sending status is error-sending. Show these financial overview contacts also in grid when "to select" view (email or post) is shown.

--- app/Http/RequestQueries/FinancialOverviewContact/Grid/RequestQuery.php
+++ app/Http/RequestQueries/FinancialOverviewContact/Grid/RequestQuery.php
@@ -28,7 +28,7 @@ class RequestQuery extends \App\Helpers\RequestQuery\RequestQuery
             ->select('financial_overview_contacts.*')->where('financial_overview_contacts.financial_overview_id', $this->request->input('financialOverviewId'));
 
         if($this->request->input('onlyEmailFinancialOverviewContacts') == "true" || $this->request->input('onlyPostFinancialOverviewContacts') == "true") {
-            $query->where('financial_overview_contacts.status_id', 'to-send');
+            $query->whereIn('financial_overview_contacts.status_id', ['to-send', 'error-sending']);
         }
         return $query;
     }

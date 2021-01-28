a91c55216c7d7d99c7c93945b2a4cc0f36e6fe2f
Mosmania <wim@mosmania.nl>
2021-01-28 16:32:32 +0100

    Filter financial overview participantprojects for participants that are terminated before begindate of financial overview and filter for participants where value results (start en and end) are all 0.

--- app/Http/Controllers/Api/FinancialOverview/FinancialOverviewParticipantProjectController.php
+++ app/Http/Controllers/Api/FinancialOverview/FinancialOverviewParticipantProjectController.php
@@ -19,33 +19,43 @@ class FinancialOverviewParticipantProjectController extends Controller
     public function createParticipantProjectsForFinancialOverview(FinancialOverviewProject $financialOverviewProject)
     {
         $financialOverview = $financialOverviewProject->financialOverview;
-        $participants = $financialOverviewProject->project->participantsProject;
 
         $startDate = Carbon::createFromDate($financialOverview->year, 1, 1);
         $endDate = Carbon::createFromDate($financialOverview->year, 1, 1)->addYear();
+
+        $participants = $financialOverviewProject->project->participantsProject->filter(function($participantProject) use ($startDate) {
+            return ($participantProject->date_terminated == null) ||
+                ($participantProject->date_terminated > Carbon::parse($startDate)->startOfDay()->toDateString());
+        });
+
         foreach ($participants as $participant) {
             //calculate start_value en end_value of participation
 
             $startValue = $this->calculateParticipationsValue($participant, $startDate);
             $endValue = $this->calculateParticipationsValue($participant, $endDate);
-            FinancialOverviewParticipantProject::create([
-                'financial_overview_project_id' => $financialOverviewProject->id,
-                'participant_project_id' => $participant->id,
-                'quantity_start_value' => $startValue['quantity'],
-                'quantity_end_value' => $endValue['quantity'],
-                'bookworth_start_value' => $startValue['bookworth'],
-                'bookworth_end_value' => $endValue['bookworth'],
-                'amount_start_value' => $startValue['amount'],
-                'amount_end_value' => $endValue['amount'],
-            ]);
 
-            FinancialOverviewContact::updateOrCreate([
-                //Add unique field to match here
-                'financial_overview_id'   => $financialOverviewProject->financialOverview->id,
-                'contact_id'   => $participant->contact_id,
-            ],[
-                'status_id' => 'concept',
-            ]);
+            if($startValue['quantity'] != 0 || $startValue['bookworth'] != 0 || $startValue['amount'] != 0
+                || $endValue['quantity'] != 0 || $endValue['bookworth'] != 0 || $endValue['amount'] != 0){
+                FinancialOverviewParticipantProject::create([
+                    'financial_overview_project_id' => $financialOverviewProject->id,
+                    'participant_project_id' => $participant->id,
+                    'quantity_start_value' => $startValue['quantity'],
+                    'quantity_end_value' => $endValue['quantity'],
+                    'bookworth_start_value' => $startValue['bookworth'],
+                    'bookworth_end_value' => $endValue['bookworth'],
+                    'amount_start_value' => $startValue['amount'],
+                    'amount_end_value' => $endValue['amount'],
+                ]);
+
+                FinancialOverviewContact::updateOrCreate([
+                    //Add unique field to match here
+                    'financial_overview_id'   => $financialOverviewProject->financialOverview->id,
+                    'contact_id'   => $participant->contact_id,
+                ],[
+                    'status_id' => 'concept',
+                ]);
+            }
+
         }
     }
 

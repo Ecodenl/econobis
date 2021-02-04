0a54d0334e97e061963706e4d4be5ff5c0d37cb7
Mosmania <wim@mosmania.nl>
2021-02-04 12:52:24 +0100

    When return to login page (after logout for instance) switch back to default themesettings.

--- customer-portal-app/src/route/PublicRoute.js
+++ customer-portal-app/src/route/PublicRoute.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React, { useContext, useEffect } from 'react';
+import { Route, useLocation } from 'react-router-dom';
+import { ThemeSettingsContext } from '../context/ThemeSettingsContext';
 
-const PublicRoute = ({ component: Component, ...rest }) => (
-    <Route render={props => <Component {...props} />} {...rest} />
-);
+const PublicRoute = ({ component: Component, ...rest }) => {
+    const location = useLocation();
+    const { switchToDefaultThemeSettings } = useContext(ThemeSettingsContext);
+
+    useEffect(() => {
+        switchToDefaultThemeSettings();
+    }, [location.pathname]);
+
+    return <Route render={props => <Component {...props} />} {...rest} />;
+};
 
 export default PublicRoute;

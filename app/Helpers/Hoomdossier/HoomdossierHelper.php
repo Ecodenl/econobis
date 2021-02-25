f57f9b3090f6e71777ae8477f57b8a5a912578fa
Mosmania <wim@mosmania.nl>
2021-02-25 10:03:03 +0100

    Cleanup code

--- app/Helpers/Hoomdossier/HoomdossierHelper.php
+++ app/Helpers/Hoomdossier/HoomdossierHelper.php
@@ -34,15 +34,8 @@ class HoomdossierHelper
 
         // Send to hoomdossier url
         $hoomResponse = $this->sendToHoomdossier();
-// todo WM: opschonen dd
-//dd($hoomResponse);
-
-//        $test = json_encode(["account_id" => 1336,
-//            "user_id" => 1685]);
-//        dd(json_decode($test)->account_id);
 
         $hoomAccountId = json_decode($hoomResponse)->account_id;
-//        dd($hoomAccountId);
 
         // When success save hoomdossier id
         $this->contact->hoom_account_id = $hoomAccountId;
@@ -114,9 +107,7 @@ class HoomdossierHelper
 
         $payload = [
             'extra' => ['contact_id' => $this->contact->id],
-// todo WM: weer terugzetten juiste vulling email
-//            'email' => $this->contact->primaryEmailAddress->email ? $this->contact->primaryEmailAddress->email : '',
-            'email' => $this->contact->primaryEmailAddress->email,
+            'email' => $this->contact->primaryEmailAddress->email ? $this->contact->primaryEmailAddress->email : '',
             'first_name' => $this->contact->person->first_name ? $this->contact->person->first_name : '',
             'last_name' => $lastName ? $lastName : '',
             'postal_code' => $this->contact->primaryAddress->postal_code ? $this->contact->primaryAddress->postal_code : '',
@@ -126,44 +117,17 @@ class HoomdossierHelper
             'city' => $this->contact->primaryAddress->city ? $this->contact->primaryAddress->city : '',
             'phone_number' => $this->contact->primaryphoneNumber ? $this->contact->primaryphoneNumber->number : '',
         ];
-// todo WM: opschonen dd
-//dd(json_encode($payload));
         $client = new Client;
         $headers = [
             'Authorization' => 'Bearer ' . $this->cooperation->hoom_key,
             'Accept'        => 'application/json',
-//            'data-raw'      => '{"extra":{"contact_id":62},"email":"","first_name":"Wim","last_name":"Test1","postal_code":"1111 AA","number":1,"house_number_extension":"a","street":"straat","city":"woonplaats","phone_number":""}',
         ];
-        $body = ['json' => $payload];
 
-// todo WM: uitzoeken hoe we nu body/json goed kunnen meesturen in post
-//        $body = ['body'      => '{
-//    "email": "",
-//    "first_name": "Wim",
-//    "extra": {
-//        "contact_id": "128"
-//    },
-//    "last_name": "Mosman",
-//    "postal_code": "",
-//    "number": "",
-//    "house_number_extension": "",
-//    "street": "",
-//    "city": "",
-//    "phone_number": ""
-//}' ];
-//dd($headers);
         try {
-//            $response = $client->request('POST', $this->cooperation->hoom_link, ['headers' => $headers, 'json' => $payload]);
             $response = $client->post($this->cooperation->hoom_link, ['headers' => $headers, 'json' => $payload]);
-//            dd($response->getBody());
             return $response->getBody();
         } catch (RequestException $e) {
             if ($e->hasResponse()) {
-//                dd($e->getRequest()->getBody()->getContents());
-//                dd($e->getResponse()->getBody());
-//                dd(Psr7\str($e->getMessage()));
-//                abort($e->getCode(), Psr7\str($e->getResponse()));
-//                dd(json_decode($e->getResponse()->getBody()));
                 abort($e->getCode(), $e->getResponse()->getBody());
             } else {
                 abort($e->getCode(), 'Er is iets misgegaan met het verzenden naar Hoomdossier');

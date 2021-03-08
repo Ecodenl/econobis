97dea4a21170a6607f25ab397f8ecae4863d00c0
Mosmania <wim@mosmania.nl>
2021-03-08 07:23:55 +0100

    Rebuild client-app and set version to 3.4.2.12

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,5 +19,5 @@
 </script>
 
 <div id="root"></div>
-<script type="text/javascript" src="./js/vendors~bundle.d1fb79797bdca01bfc95.js"></script><script type="text/javascript" src="./js/bundle.63cc027791ebb2250819.js"></script></body>
+<script type="text/javascript" src="./js/vendors~bundle.d1fb79797bdca01bfc95.js"></script><script type="text/javascript" src="./js/bundle.e02fc49ec8dc2c94bbaf.js"></script></body>
 </html>

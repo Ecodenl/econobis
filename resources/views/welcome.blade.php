6004b1a4a1efd59dd84ef02290fba02ab0a2f398
Mosmania <wim@mosmania.nl>
2021-04-06 14:44:47 +0200

    Rebuild client-app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,5 +19,5 @@
 </script>
 
 <div id="root"></div>
-<script type="text/javascript" src="./js/vendors~bundle.2ddae662630333f4b736.js"></script><script type="text/javascript" src="./js/bundle.4230bd8bc56c3fec2279.js"></script></body>
+<script type="text/javascript" src="./js/vendors~bundle.2ddae662630333f4b736.js"></script><script type="text/javascript" src="./js/bundle.0e8c9701910077160763.js"></script></body>
 </html>

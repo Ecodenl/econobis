9acaa18c58f5138a601af70245076cf113cedfbd
Mosmania <wim@mosmania.nl>
2021-04-23 16:29:07 +0200

    If more than 3 contacts attached to email only show the first 3 and then text "en nog x andere". Furthermore check en delete commands created for checking and deleting wrong email/contacts links. These links are wrong when to, cc and bcc are all empty in email.

---
+++ app/Console/Commands/deleteWrongContactEmailRecords.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Eco\Email\Email;
+use App\Eco\Email\EmailAttachment;
+use App\Eco\Mailbox\Mailbox;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+
+class deleteWrongContactEmailRecords extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'email:deleteWrongContactEmailRecords';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Verwijderen op foute email/contact koppelingen.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     *
+     */
+    public function handle()
+    {
+        $emailsWithoutToCcAndBcc = Email::where('folder', 'inbox')->where('to', '[]')->where('cc', '[]')->where('bcc', '[]')->get();
+        foreach ($emailsWithoutToCcAndBcc as $emailWithoutToCcAndBcc) {
+            $emailWithoutToCcAndBcc->contacts()->detach();
+        }
+        dd('Einde Verwijderen foute email/contact koppelingen.');
+    }
+
+}

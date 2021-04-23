3d5a7db6002d84939e477fc6c278971e46858fc3
Mosmania <wim@mosmania.nl>
2021-04-23 16:39:44 +0200

    Rebuild client-app and set version to 3.6.2.2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,5 +19,5 @@
 </script>
 
 <div id="root"></div>
-<script type="text/javascript" src="./js/vendors~bundle.1435fff38c3637c06125.js"></script><script type="text/javascript" src="./js/bundle.648372ec89f30c285e82.js"></script></body>
+<script type="text/javascript" src="./js/vendors~bundle.1435fff38c3637c06125.js"></script><script type="text/javascript" src="./js/bundle.2149212d5688d14e4f03.js"></script></body>
 </html>

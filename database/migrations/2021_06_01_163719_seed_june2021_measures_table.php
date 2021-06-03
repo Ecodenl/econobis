3e4f9d6a3bbcc44ba2d9c97f540d370c716f09e8
Mosmania <wim@mosmania.nl>
2021-06-03 07:27:36 +0200

    New measure added

---
+++ database/migrations/2021_06_01_163719_seed_june2021_measures_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class SeedJune2021MeasuresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        //Isolatieadvies
+        $categorizedMeasures = [
+            'Energieconciërge' => [
+                'Huisbezoek E-team',
+            ],
+        ];
+
+        $id = 16;
+        foreach ($categorizedMeasures as $category => $measures) {
+            // Get the category
+            $catId = DB::table('measure_categories')->where('name', $category)->first()->id;
+            // Create the measures
+            foreach($measures as $measure) {
+                DB::table('measures')->insertGetId(
+                    ['name'=> $measure,
+                        'measure_category_id' => $catId,
+                        'number' => 'M2021-' . $id ]
+                );
+                $id++;
+            }
+
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}

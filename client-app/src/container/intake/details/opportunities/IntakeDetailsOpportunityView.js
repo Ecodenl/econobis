f2708d866d401239ef347066841e47de4cb0c328
Levi Roelofsma <levi.roelofsma1@gmail.com>
2021-06-18 15:08:40 +0200

    Added measures field to related opp of intake

--- client-app/src/container/intake/details/opportunities/IntakeDetailsOpportunityView.js
+++ client-app/src/container/intake/details/opportunities/IntakeDetailsOpportunityView.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { hashHistory } from 'react-router';
 
 const IntakeDetailsOpportunityView = props => {
-    const { id, number, createdAt, measureCategory, status, quotationRequests } = props.opportunity;
+    const { id, number, createdAt, measureCategory, measures, status, quotationRequests } = props.opportunity;
 
     return (
         <div
@@ -14,8 +14,9 @@ const IntakeDetailsOpportunityView = props => {
         >
             <div onClick={() => hashHistory.push(`/kans/${id}`)}>
                 <div className="col-sm-2">{number}</div>
-                <div className="col-sm-3">{createdAt ? moment(createdAt).format('L') : ''}</div>
-                <div className="col-sm-3">{measureCategory ? measureCategory.name : ''}</div>
+                <div className="col-sm-2">{createdAt ? moment(createdAt).format('L') : ''}</div>
+                <div className="col-sm-2">{measureCategory ? measureCategory.name : ''}</div>
+                <div className="col-sm-2 pre-wrap">{measures.length ? measures.map(measure => { return measure.name }).join(',\n') : ''}</div>
                 <div className="col-sm-2">{status ? status.name : ''}</div>
                 <div className="col-sm-2">{quotationRequests.length}</div>
             </div>

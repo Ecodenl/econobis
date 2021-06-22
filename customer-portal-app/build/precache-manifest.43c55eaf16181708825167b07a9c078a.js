18643d080cd4afed2491145da12e11f7c151b772
Mosmania <wim@mosmania.nl>
2021-06-22 08:49:30 +0200

    Rebuild customer-portal-app and set version to 3.6.5.4

---
+++ customer-portal-app/build/precache-manifest.43c55eaf16181708825167b07a9c078a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6485b5b61bd9ca4fb2f5841409a33754",
+    "url": "./index.html"
+  },
+  {
+    "revision": "01f7b475b4b0096e869e",
+    "url": "./static/css/2.36592687.chunk.css"
+  },
+  {
+    "revision": "15dadd92afd10e2121cf",
+    "url": "./static/css/main.7d381eb4.chunk.css"
+  },
+  {
+    "revision": "01f7b475b4b0096e869e",
+    "url": "./static/js/2.49aac270.chunk.js"
+  },
+  {
+    "revision": "4147234baeb9508e33cff26d1243715f",
+    "url": "./static/js/2.49aac270.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15dadd92afd10e2121cf",
+    "url": "./static/js/main.e133e94d.chunk.js"
+  },
+  {
+    "revision": "11374bf3899fe6465ae3",
+    "url": "./static/js/runtime-main.66dc3cad.js"
+  },
+  {
+    "revision": "195ac77a0a5837cdacb001af45aa0fa7",
+    "url": "./static/media/logo.195ac77a.png"
+  }
+]);

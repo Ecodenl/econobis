ec4300ad0e43c601620b65031672d173dcc18699
Mosmania <wim@mosmania.nl>
2021-06-24 12:37:29 +0200

    Add numberOfInvoiceReminders to administrations table and numberOfReminders to invoices table.

---
+++ database/migrations/2021_06_24_112935_add_number_of_reminders_to_invoices_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNumberOfRemindersToInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('invoices', function (Blueprint $table) {
+            $table->smallInteger('number_of_reminders')->default(3)->after('date_collection');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (Schema::hasColumn('invoices', 'number_of_reminders')) {
+            Schema::table('invoices', function (Blueprint $table) {
+                $table->dropColumn('number_of_reminders');
+            });
+        }
+    }
+}

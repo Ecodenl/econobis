b6601b531a648d4dc0233cb7dadd1c7a31ac4006
Mosmania <wim@mosmania.nl>
2021-06-24 12:58:25 +0200

    Renamed numberOfReminders to numberOfInvoiceReminders in invoices table and set nullable and default null. Add numberOfInvoiceReminders to orders table.

--- database/migrations/2021_06_24_125336_add_number_of_reminders_to_orders_table.php
+++ database/migrations/2021_06_24_125336_add_number_of_reminders_to_orders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddNumberOfRemindersToInvoicesTable extends Migration
+class AddNumberOfRemindersToOrdersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,8 @@ class AddNumberOfRemindersToInvoicesTable extends Migration
      */
     public function up()
     {
-        Schema::table('invoices', function (Blueprint $table) {
-            $table->smallInteger('number_of_invoice_reminders')->nullable()->default(null)->after('date_collection');
+        Schema::table('orders', function (Blueprint $table) {
+            $table->smallInteger('number_of_invoice_reminders')->nullable()->default(null)->after('invoice_text');
         });
     }
 
@@ -25,8 +25,8 @@ class AddNumberOfRemindersToInvoicesTable extends Migration
      */
     public function down()
     {
-        if (Schema::hasColumn('invoices', 'number_of_invoice_reminders')) {
-            Schema::table('invoices', function (Blueprint $table) {
+        if (Schema::hasColumn('orders', 'number_of_invoice_reminders')) {
+            Schema::table('orders', function (Blueprint $table) {
                 $table->dropColumn('number_of_invoice_reminders');
             });
         }

f4c8e1f59c132f64d8a572f7b6982c673b052ca2
Rob Rollenberg <rob@jepr.nl>
2021-09-23 15:01:41 +0200

    Not returning fullcontact but only the full name, which improves the speed

--- app/Http/Resources/Order/GridOrder.php
+++ app/Http/Resources/Order/GridOrder.php
@@ -9,7 +9,6 @@
 namespace App\Http\Resources\Order;
 
 
-use App\Http\Resources\Contact\FullContact;
 use App\Http\Resources\EnumWithIdAndName\FullEnumWithIdAndName;
 use Illuminate\Http\Resources\Json\Resource;
 
@@ -22,8 +21,9 @@ class GridOrder extends Resource
             'number' => $this->number,
             'dateNextInvoice' => $this->date_next_invoice,
             'subject' => $this->subject,
-
-            'contact' => FullContact::make($this->whenLoaded('contact')),
+            'contact' => [
+                'fullName' => $this->contact->full_name,
+            ],
 
             'totalInclVatInclReductionPerYear' => $this->getTotalInclVatInclReductionPerYearAttribute(),
 

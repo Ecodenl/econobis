70f016f41cf4b93bd5352808b7ac8c01db0dfd8b
Mosmania <wim@mosmania.nl>
2021-09-27 14:36:12 +0200

    Use Illuminate\Http\Resources\Json\JsonResource instead of Illuminate\Http\Resources\Json\Resource.

--- app/Http/Resources/Contact/GridContactGroupContacts.php
+++ app/Http/Resources/Contact/GridContactGroupContacts.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Resources\Contact;
 
-use Illuminate\Http\Resources\Json\Resource;
+use Illuminate\Http\Resources\Json\JsonResource;
 
-class GridContactGroupContacts extends Resource
+class GridContactGroupContacts extends JsonResource
 {
     /**
      * Transform the resource into an array.

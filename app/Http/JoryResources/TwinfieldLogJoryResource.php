a2e2b30d3e2d42aadee5f1e9aa890f5cee3c78ea
Mosmania <wim@mosmania.nl>
2021-10-15 16:58:42 +0200

    Merged changes from Levi: Added Twinfield logs view and moved Processen along with Twinfield under Logs dropdown in navigation menu.

---
+++ app/Http/JoryResources/TwinfieldLogJoryResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\JoryResources;
+
+use App\Eco\Twinfield\TwinfieldLog;
+use App\Http\JoryResources\Base\JoryResource;
+
+class TwinfieldLogJoryResource extends JoryResource
+{
+    protected $modelClass = TwinfieldLog::class;
+
+    protected function configureForApp(): void
+    {
+        // Fields
+        $this->field('id')->filterable()->sortable();
+        $this->field('invoice_id')->filterable()->sortable();
+        $this->field('contact_id')->filterable()->sortable();
+        $this->field('message_text')->filterable()->sortable();
+        $this->field('message_type')->filterable()->sortable();
+        $this->field('user_id')->filterable()->sortable();
+        $this->field('is_error')->filterable()->sortable();
+        $this->field('created_at')->filterable()->sortable();
+        $this->field('updated_at')->filterable()->sortable();
+
+        // Custom attribute
+        $this->field('twinfield_log_message_type_name');
+    }
+
+    protected function configureForPortal(): void
+    {
+    }
+}

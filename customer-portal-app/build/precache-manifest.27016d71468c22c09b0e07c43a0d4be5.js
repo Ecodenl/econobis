bd4e137c27ac30967129f6403867aa763a795436
Mosmania <wim@mosmania.nl>
2021-12-27 11:42:19 +0100

    Build customer-portal-app more.

---
+++ customer-portal-app/build/precache-manifest.27016d71468c22c09b0e07c43a0d4be5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13b99b3675b11eb5220ab45f78c3f59c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bf996efcc2d6e69467ed",
+    "url": "./static/css/2.36592687.chunk.css"
+  },
+  {
+    "revision": "b46d5617c12e940e043f",
+    "url": "./static/css/main.abcb16d9.chunk.css"
+  },
+  {
+    "revision": "bf996efcc2d6e69467ed",
+    "url": "./static/js/2.82ae2c54.chunk.js"
+  },
+  {
+    "revision": "4147234baeb9508e33cff26d1243715f",
+    "url": "./static/js/2.82ae2c54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b46d5617c12e940e043f",
+    "url": "./static/js/main.9a18c519.chunk.js"
+  },
+  {
+    "revision": "11374bf3899fe6465ae3",
+    "url": "./static/js/runtime-main.66dc3cad.js"
+  },
+  {
+    "revision": "195ac77a0a5837cdacb001af45aa0fa7",
+    "url": "./static/media/logo.195ac77a.png"
+  }
+]);

b4ddf89f5b1c7227b8d28a851de4be2a15b92348
Mosmania <wim@mosmania.nl>
2021-12-29 12:33:05 +0100

    Sort on name, added emailattachment

--- app/Eco/Document/DocumentCreatedFrom.php
+++ app/Eco/Document/DocumentCreatedFrom.php
@@ -23,20 +23,21 @@ class DocumentCreatedFrom extends EnumWithIdAndName
     protected static function seed()
     {
         return [
-            new static('document', 'Document'),
-            new static('project', 'Project'),
-            new static('participant', 'Deelnemer'),
             new static('administration', 'Administratie'),
-            new static('contact', 'Contact'),
             new static('campagne', 'Campagne'),
+            new static('contact', 'Contact'),
             new static('contactgroup', 'Contactgroep'),
-            new static('order', 'Order'),
-            new static('housingfile', 'Woningdossier'),
+            new static('participant', 'Deelnemer'),
+            new static('document', 'Document'),
+            new static('emailattachment', 'E-mail bijlage'),
             new static('intake', 'Intake'),
             new static('measure', 'Maatregel'),
             new static('opportunity', 'Kans'),
             new static('quotationreguest', 'Offerteverzoek'),
+            new static('order', 'Order'),
+            new static('project', 'Project'),
             new static('task', 'Taak'),
+            new static('housingfile', 'Woningdossier'),
         ];
     }
 }

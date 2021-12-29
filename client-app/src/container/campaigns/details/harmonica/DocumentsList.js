6b365066eb7b575a85513e7b9a8fe0e07f1e3c4d
Mosmania <wim@mosmania.nl>
2021-12-29 12:37:35 +0100

    Bugfix. Open item document (not taak).

--- client-app/src/container/campaigns/details/harmonica/DocumentsList.js
+++ client-app/src/container/campaigns/details/harmonica/DocumentsList.js
@@ -4,7 +4,7 @@ import moment from 'moment';
 
 function DocumentsList({ relatedDocuments }) {
     function openItem(id) {
-        hashHistory.push(`/taak/${id}`);
+        hashHistory.push(`/document/${id}`);
     }
 
     if (relatedDocuments.length === 0) return <div>Geen documenten gevonden.</div>;

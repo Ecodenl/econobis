19434bfbb59f099f2b98e1bc7f88efd25f3204e5
Mosmania <wim@mosmania.nl>
2022-01-27 08:06:16 +0100

    Cleanup code. Imports not used removed.

--- app/Http/Resources/Project/GridProject.php
+++ app/Http/Resources/Project/GridProject.php
@@ -2,19 +2,7 @@
 
 namespace App\Http\Resources\Project;
 
-use App\Http\Resources\Address\FullAddress;
-use App\Http\Resources\Campaign\FullCampaign;
-use App\Http\Resources\Contact\FullContact;
-use App\Http\Resources\Document\FullDocument;
-use App\Http\Resources\GenericResource;
-use App\Http\Resources\Measure\FullMeasure;
-use App\Http\Resources\Opportunity\FullOpportunity;
-use App\Http\Resources\Task\FullTask;
-use App\Http\Resources\User\FullUser;
 use Illuminate\Http\Resources\Json\JsonResource;
-use App\Eco\Contact\Contact;
-use App\Eco\Address\Address;
-use App\Eco\Measure\Measure;
 
 class GridProject extends JsonResource
 {

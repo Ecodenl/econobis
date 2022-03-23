d0bf3f3555437446b8fa341931287b5ee9108f50
Mosmania <wim@mosmania.nl>
2022-03-23 13:42:49 +0100

    Build client-app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,5 +20,5 @@
 </script>
 
 <div id="root"></div>
-<script type="text/javascript" src="./js/vendors~bundle.67fd6b6351840f675283.js"></script><script type="text/javascript" src="./js/bundle.ec497e353af2f577102c.js"></script></body>
+<script type="text/javascript" src="./js/vendors~bundle.67fd6b6351840f675283.js"></script><script type="text/javascript" src="./js/bundle.608d0d7accf7eb235199.js"></script></body>
 </html>

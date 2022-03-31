d068f56fa82c7551057270171ec88739de00e1e6
Mosmania <wim@mosmania.nl>
2022-03-31 12:32:45 +0200

    Return resource ProjectRevenueDistributionPeek for getDistributionKwhForReportEnergySupplierAttribute instead of only id's.

--- app/Eco/RevenuesKwh/RevenuePartsKwh.php
+++ app/Eco/RevenuesKwh/RevenuePartsKwh.php
@@ -2,6 +2,8 @@
 
 namespace App\Eco\RevenuesKwh;
 
+use App\Eco\Project\ProjectRevenueDistribution;
+use App\Http\Resources\Project\ProjectRevenueDistributionPeek;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Venturecraft\Revisionable\RevisionableTrait;
@@ -166,7 +168,12 @@ class RevenuePartsKwh extends Model
     {
         $upToPartsKwhIds = RevenuePartsKwh::where('revenue_id', $this->revenue_id)->where('date_end', '<=', $this->date_end)->pluck('id')->toArray();
         $distributionKwhIds = RevenueDistributionPartsKwh::whereIn('parts_id', $upToPartsKwhIds)->where('is_visible', 1)->whereNull('date_energy_supplier_report')->whereNotNull('es_id')->where('delivered_kwh', '!=', 0)->pluck('distribution_id')->toArray();
-        return array_unique($distributionKwhIds);
+
+        $distributions = new ProjectRevenueDistribution();
+        foreach(array_chunk($distributionKwhIds,900) as $chunk){
+            $distributions = $distributions->orWhereIn('id', $chunk);
+        }
+        return ProjectRevenueDistributionPeek::collection($distributions->get());
     }
 
     public function calculator()

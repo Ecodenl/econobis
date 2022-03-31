620070e50bc5d2b511b3d4fd6087ba0e07e73979
Mosmania <wim@mosmania.nl>
2022-03-31 10:42:22 +0200

    Build client-app and customer-portal-app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,5 +20,5 @@
 </script>
 
 <div id="root"></div>
-<script type="text/javascript" src="./js/vendors~bundle.c7c631855ec315d6829f.js"></script><script type="text/javascript" src="./js/bundle.50bea020044f57c0c225.js"></script></body>
+<script type="text/javascript" src="./js/vendors~bundle.c7c631855ec315d6829f.js"></script><script type="text/javascript" src="./js/bundle.25d4aff890cb440d8c69.js"></script></body>
 </html>

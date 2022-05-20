a68f1c83193167729bee4a5b468c34d3a1c64200
Mosmania <wim@mosmania.nl>
2022-05-20 16:14:53 +0200

    Remove space from postal code in existing addresses

---
+++ database/migrations/2022_05_20_160650_recalculate2_postal_codes.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Recalculate2PostalCodes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        $addresses = \App\Eco\Address\Address::all();
+
+        foreach ($addresses as $address){
+
+            if(preg_match('/^\d{4}\s[A-Za-z]{2}$/', $address->postal_code)){
+                $address->postal_code = preg_replace('/\s+/', '', $address->postal_code);
+            }
+
+            $address->save();
+        }
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}

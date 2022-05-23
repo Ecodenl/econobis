0579ad3ddfa98cd36256d99c44376ad7cc4c9968
Mosmania <wim@mosmania.nl>
2022-05-23 08:06:20 +0200

    New tables:address_energy_consumption_gas and address_energy_consumption_electricity

---
+++ database/migrations/2022_05_20_163932_create_address_energy_consumption_electricity_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressEnergyConsumptionElectricityTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('address_energy_consumption_electricity', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('address_id');
+            $table->foreign('address_id', 'address_es_address_id_foreign')
+                ->references('id')->on('addresses')
+                ->onDelete('restrict');
+            $table->double('consumption_high', 10, 0)->nullable();
+            $table->double('consumption_low', 10, 0)->nullable();
+            $table->double('return_high', 10, 0)->nullable();
+            $table->double('return_low', 10, 0)->nullable();
+            $table->double('proposed_variable_rate_high', 11, 2)->nullable();
+            $table->double('proposed_variable_rate_low', 11, 2)->nullable();
+            $table->double('proposed_fixed_rate_high', 11, 2)->nullable();
+            $table->double('proposed_fixed_rate_low', 11, 2)->nullable();
+            $table->double('total_variable_costs_high', 11, 2)->nullable();
+            $table->double('total_variable_costs_low', 11, 2)->nullable();
+            $table->double('total_fixed_costs_high', 11, 2)->nullable();
+            $table->double('total_fixed_costs_low', 11, 2)->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('address_energy_consumption_electricity');
+    }
+}

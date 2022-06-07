32554214a3f05afc0b6e3cb6bfacd92def16b456
Mosmania <wim@mosmania.nl>
2022-06-07 08:50:57 +0200

    Build customer-portal-app forgotten uversioned files

---
+++ customer-portal-app/build/precache-manifest.83e82ee24a90daa315b52152b5c9c62c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0c253245d00a7e0af123f55903584d1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "08719ef51525f265fc1c",
+    "url": "./static/css/2.36592687.chunk.css"
+  },
+  {
+    "revision": "66edeeb4a5ae35650e6c",
+    "url": "./static/css/main.dc22dbba.chunk.css"
+  },
+  {
+    "revision": "08719ef51525f265fc1c",
+    "url": "./static/js/2.e50366f0.chunk.js"
+  },
+  {
+    "revision": "4147234baeb9508e33cff26d1243715f",
+    "url": "./static/js/2.e50366f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66edeeb4a5ae35650e6c",
+    "url": "./static/js/main.8de82260.chunk.js"
+  },
+  {
+    "revision": "11374bf3899fe6465ae3",
+    "url": "./static/js/runtime-main.66dc3cad.js"
+  },
+  {
+    "revision": "195ac77a0a5837cdacb001af45aa0fa7",
+    "url": "./static/media/logo.195ac77a.png"
+  }
+]);

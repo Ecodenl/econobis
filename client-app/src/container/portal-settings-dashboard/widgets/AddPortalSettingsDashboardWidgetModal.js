f2206d836c528d3906c64e49e6026d78559d9b33
Mosmania <wim@mosmania.nl>
2022-06-09 12:24:44 +0200

    Change position and size for modal portal layout add widget.

--- client-app/src/container/portal-settings-dashboard/widgets/AddPortalSettingsDashboardWidgetModal.js
+++ client-app/src/container/portal-settings-dashboard/widgets/AddPortalSettingsDashboardWidgetModal.js
@@ -149,102 +149,105 @@ class AddPortalSettingsDashboardWidgetModal extends Component {
         const acceptedFiles = ['image/png', 'image/jpeg'];
 
         return (
-            <Modal
-                closeModal={this.props.toggleModal}
-                showConfirmAction={true}
-                title={this.props.title}
-                buttonConfirmText={'Toevoegen'}
-                confirmAction={this.addWidgetAction}
-            >
-                <div className={'row'}>
-                    <Col sm={12}>
-                        <InputText
-                            divSize={'col-sm-12'}
-                            labelSize={'col-sm-12'}
-                            size={'col-sm-12'}
-                            label={'Titel'}
-                            name={'title'}
-                            value={widget.title}
-                            onChangeAction={this.handleInputChange}
-                            required={'required'}
-                            error={errors.title}
-                            errorMessage={errorMessage.title}
+            <div className={'portal-layout-add-widget'}>
+                <Modal
+                    modalClassName={'modal-portal-layout-add-widget'}
+                    closeModal={this.props.toggleModal}
+                    showConfirmAction={true}
+                    title={this.props.title}
+                    buttonConfirmText={'Toevoegen'}
+                    confirmAction={this.addWidgetAction}
+                >
+                    <div className={'row'}>
+                        <Col sm={12}>
+                            <InputText
+                                divSize={'col-sm-12'}
+                                labelSize={'col-sm-12'}
+                                size={'col-sm-12'}
+                                label={'Titel'}
+                                name={'title'}
+                                value={widget.title}
+                                onChangeAction={this.handleInputChange}
+                                required={'required'}
+                                error={errors.title}
+                                errorMessage={errorMessage.title}
+                            />
+                        </Col>
+                        <Col sm={12} style={{ paddingLeft: '30px', paddingRight: '30px' }}>
+                            <InputTextArea
+                                sizeInput={'col-sm-12'}
+                                sizeLabel={'col-sm-12'}
+                                size={'col-sm-12'}
+                                label={'Tekst'}
+                                name={'text'}
+                                value={widget.text}
+                                onChangeAction={this.handleInputChange}
+                                required={'required'}
+                                error={errors.text}
+                                errorMessage={errorMessage.text}
+                            />
+                        </Col>
+                        <Col sm={12} style={{ paddingLeft: '30px', paddingRight: '30px' }}>
+                            <label className={'col-sm-12 required'}>Afbeelding</label>
+                            {!!widget.imageName && <b>&nbsp;Geselecteerd bestand: {widget.imageName}</b>}
+                            <Dropzone
+                                accept={acceptedFiles}
+                                multiple={false}
+                                className="dropzone"
+                                onDropAccepted={this.onDropAccepted.bind(this)}
+                                onDropRejected={this.onDropRejected.bind(this)}
+                                maxSize={6000000}
+                            >
+                                <p>Klik hier voor het uploaden van een bestand</p>
+                                <p>
+                                    <strong>of</strong> sleep het bestand hierheen
+                                </p>
+                            </Dropzone>
+                            {errors.image && (
+                                <div className={'col-sm-12'}>
+                                    <span className="has-error-message"> {errorMessage.image}</span>
+                                </div>
+                            )}
+                        </Col>
+                        <Col sm={5}>
+                            <InputText
+                                divSize={'col-sm-12'}
+                                labelSize={'col-sm-12'}
+                                size={'col-sm-12'}
+                                label={'Knoptekst'}
+                                name={'buttonText'}
+                                value={widget.buttonText}
+                                onChangeAction={this.handleInputChange}
+                                required={'required'}
+                                error={errors.buttonText}
+                                errorMessage={errorMessage.buttonText}
+                            />
+                        </Col>
+                        <Col sm={7}>
+                            <InputText
+                                divSize={'col-sm-12'}
+                                labelSize={'col-sm-12'}
+                                size={'col-sm-12'}
+                                label={'Knoplink'}
+                                name={'buttonLink'}
+                                value={widget.buttonLink}
+                                onChangeAction={this.handleInputChange}
+                                required={'required'}
+                                error={errors.buttonLink}
+                                errorMessage={errorMessage.buttonLink}
+                            />
+                        </Col>
+                    </div>
+                    {showCropImageModal && (
+                        <PortalLogoLayoutNewCrop
+                            closeShowCrop={this.closeShowCropWidgetImage}
+                            image={widget.image}
+                            imageLayoutItemName={'image-widget'}
+                            cropLogo={this.cropWidgetImage}
                         />
-                    </Col>
-                    <Col sm={12} style={{ paddingLeft: '30px', paddingRight: '30px' }}>
-                        <InputTextArea
-                            sizeInput={'col-sm-12'}
-                            sizeLabel={'col-sm-12'}
-                            size={'col-sm-12'}
-                            label={'Tekst'}
-                            name={'text'}
-                            value={widget.text}
-                            onChangeAction={this.handleInputChange}
-                            required={'required'}
-                            error={errors.text}
-                            errorMessage={errorMessage.text}
-                        />
-                    </Col>
-                    <Col sm={12} style={{ paddingLeft: '30px', paddingRight: '30px' }}>
-                        <label className={'col-sm-12 required'}>Afbeelding</label>
-                        {!!widget.imageName && <b>&nbsp;geslecteerd bestand: {widget.imageName}</b>}
-                        <Dropzone
-                            accept={acceptedFiles}
-                            multiple={false}
-                            className="dropzone"
-                            onDropAccepted={this.onDropAccepted.bind(this)}
-                            onDropRejected={this.onDropRejected.bind(this)}
-                            maxSize={6000000}
-                        >
-                            <p>Klik hier voor het uploaden van een bestand</p>
-                            <p>
-                                <strong>of</strong> sleep het bestand hierheen
-                            </p>
-                        </Dropzone>
-                        {errors.image && (
-                            <div className={'col-sm-12'}>
-                                <span className="has-error-message"> {errorMessage.image}</span>
-                            </div>
-                        )}
-                    </Col>
-                    <Col sm={5}>
-                        <InputText
-                            divSize={'col-sm-12'}
-                            labelSize={'col-sm-12'}
-                            size={'col-sm-12'}
-                            label={'Knoptekst'}
-                            name={'buttonText'}
-                            value={widget.buttonText}
-                            onChangeAction={this.handleInputChange}
-                            required={'required'}
-                            error={errors.buttonText}
-                            errorMessage={errorMessage.buttonText}
-                        />
-                    </Col>
-                    <Col sm={7}>
-                        <InputText
-                            divSize={'col-sm-12'}
-                            labelSize={'col-sm-12'}
-                            size={'col-sm-12'}
-                            label={'Knoplink'}
-                            name={'buttonLink'}
-                            value={widget.buttonLink}
-                            onChangeAction={this.handleInputChange}
-                            required={'required'}
-                            error={errors.buttonLink}
-                            errorMessage={errorMessage.buttonLink}
-                        />
-                    </Col>
-                </div>
-                {showCropImageModal && (
-                    <PortalLogoLayoutNewCrop
-                        closeShowCrop={this.closeShowCropWidgetImage}
-                        image={widget.image}
-                        imageLayoutItemName={'image-widget'}
-                        cropLogo={this.cropWidgetImage}
-                    />
-                )}
-            </Modal>
+                    )}
+                </Modal>
+            </div>
         );
     }
 }

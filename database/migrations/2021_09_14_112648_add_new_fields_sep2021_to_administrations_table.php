86d773ac586978fde19ee6fc1d402005883c07d7
Mosmania <wim@mosmania.nl>
2022-08-01 10:05:00 +0200

    Cleanup code

--- database/migrations/2021_09_14_112648_add_new_fields_sep2021_to_administrations_table.php
+++ database/migrations/2021_09_14_112648_add_new_fields_sep2021_to_administrations_table.php
@@ -17,8 +17,24 @@ class AddNewFieldsSep2021ToAdministrationsTable extends Migration
     {
         Schema::table('administrations', function (Blueprint $table) {
             $table->date('date_sync_twinfield_invoices')->nullable()->default(null)->after('date_sync_twinfield_payments');
-            $table->string('prefix_invoice_number', 5)->default('F')->after('administration_number')->after('number_of_invoice_reminders');
+//            $table->string('prefix_invoice_number', 5)->default('F')->after('administration_number')->after('number_of_invoice_reminders');
         });
+
+        if (!Schema::hasColumn('administrations', 'prefix_invoice_number')) {
+
+            Schema::table('administrations', function (Blueprint $table) {
+                $table->string('prefix_invoice_number', 5)->default('F')->after('administration_number')->after('number_of_invoice_reminders');
+            });
+            $administrations = Administration::withTrashed()->get();
+            foreach ($administrations as $administration){
+                if(empty($administration->prefix_invoice_number)){
+                    $administration->prefix_invoice_number = 'F';
+                    $administration->save();
+                }
+            }
+
+        }
+
 
         $administrations = Administration::all();
 

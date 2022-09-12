52d428797ea2f1ffe25db8c659f67debd8715d74
Mosmania <wim@mosmania.nl>
2022-09-12 15:03:49 +0200

    Add offline_access to scopes azure for getting refresh_token.

--- config/azure.php
+++ config/azure.php
@@ -10,7 +10,7 @@ return [
 //  'appSecret'         => instelbaar per mailbox
 //  'authTenant'        => hebben we deze nodig en zoja instelbaar per mailbox?
   'redirectUri'       => 'api/oauth/ms-azure/callback',
-  'scopes'            => 'user.read mail.read mail.send',
+  'scopes'            => 'user.read mail.read mail.send offline_access',
   'authority'         => 'https://login.microsoftonline.com/common',
   'authorizeEndpoint' => '/oauth2/v2.0/authorize',
   'tokenEndpoint'     => '/oauth2/v2.0/token',

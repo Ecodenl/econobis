75fd97c6133df105285542f2fba8e8fb29fa2317
Mosmania <wim@mosmania.nl>
2022-10-17 10:41:02 +0200

    New view permissions (for contact harmonica items).

--- database/migrations/2022_10_14_084119_okt2022add_new_roles.php
+++ database/migrations/2022_10_14_084119_okt2022add_new_roles.php
@@ -30,6 +30,17 @@ class Okt2022addNewRoles extends Migration
             'menu_workflow',
             'menu_general_settings',
             'menu_portal_settings',
+            'view_email',
+            'view_task',
+            'view_note',
+            'view_participation',
+            'view_intake',
+            'view_opportunity',
+            'view_housing_file',
+            'view_contact_group',
+            'view_order',
+            'view_invoice',
+            'view_financial_overview_contact',
         ];
         foreach ($permissions as $permissionName) {
             \Spatie\Permission\Models\Permission::create([
@@ -71,8 +82,28 @@ class Okt2022addNewRoles extends Migration
         $superuserRole = Role::findByName('Key user');
         $superuserRole->syncPermissions(Permission::all());
 
-        //make roles
-        $roles = [
+        //make roles inzake menu permissions
+        $menuRoles = [
+            'Medewerker' => ['view_email', 'view_task', 'view_note', 'view_participation', 'view_intake', 'view_opportunity', 'view_housing_file', 'view_contact_group', 'view_order', 'view_invoice', 'view_financial_overview_contact'],
+            'Medewerker 2' => ['view_email', 'view_task', 'view_note', 'view_participation', 'view_intake', 'view_opportunity', 'view_housing_file', 'view_contact_group', 'view_order', 'view_invoice', 'view_financial_overview_contact'],
+            'Projectmedewerker' => ['view_email', 'view_task', 'view_note', 'view_participation', 'view_intake', 'view_opportunity', 'view_housing_file', 'view_contact_group', 'view_order', 'view_invoice', 'view_financial_overview_contact'],
+            'Financieel medewerker' => ['view_email', 'view_task', 'view_note', 'view_participation', 'view_intake', 'view_opportunity', 'view_housing_file', 'view_contact_group', 'view_order', 'view_invoice', 'view_financial_overview_contact',            ],
+            'Financieel controller' => ['view_email', 'view_task', 'view_note', 'view_participation', 'view_intake', 'view_opportunity', 'view_housing_file', 'view_contact_group', 'view_order', 'view_invoice', 'view_financial_overview_contact',            ],
+            'Participatie medewerker' => ['view_email', 'view_task', 'view_note', 'view_participation', 'view_intake', 'view_opportunity', 'view_housing_file', 'view_contact_group', 'view_order', 'view_invoice', 'view_financial_overview_contact',            ],
+            'Energie adviseur' => ['view_email', 'view_task', 'view_note', 'view_participation', 'view_intake', 'view_opportunity', 'view_housing_file', 'view_contact_group', 'view_order', 'view_invoice', 'view_financial_overview_contact',            ],
+            'Marketing medewerker' => ['view_email', 'view_task', 'view_note', 'view_participation', 'view_intake', 'view_opportunity', 'view_housing_file', 'view_contact_group', 'view_order', 'view_invoice', 'view_financial_overview_contact',            ],
+            'Energiemanager' => ['view_email', 'view_task', 'view_note', 'view_intake', 'view_opportunity', 'view_housing_file', 'view_contact_group'],
+            'Energiecoordinator' => ['view_email', 'view_task', 'view_note', 'view_intake', 'view_opportunity', 'view_housing_file', 'view_contact_group'],
+        ];
+
+        foreach($menuRoles as $menuRoleName => $permissions) {
+            $role =  \Spatie\Permission\Models\Role::findByName($menuRoleName);
+            $role->syncPermissions($permissions);
+
+        }
+
+        //make roles inzake nieuw view permissions
+        $newViewRoles = [
             'Medewerker' => ['menu_contacts', 'menu_projects', 'menu_energy_saving', 'menu_contact_groups', 'menu_email', 'menu_marketing', 'menu_tasks','menu_agenda','menu_processes','menu_documents','menu_general_settings'],
             'Medewerker 2' => ['menu_contacts', 'menu_projects', 'menu_energy_saving', 'menu_contact_groups', 'menu_email', 'menu_marketing', 'menu_tasks','menu_agenda','menu_processes','menu_documents','menu_general_settings'],
             'Projectmedewerker' => ['menu_contacts', 'menu_projects', 'menu_energy_saving', 'menu_contact_groups', 'menu_email', 'menu_marketing', 'menu_tasks','menu_agenda','menu_processes','menu_documents','menu_general_settings'],
@@ -85,8 +116,8 @@ class Okt2022addNewRoles extends Migration
             'Energiecoordinator' => ['menu_contacts', 'menu_projects', 'menu_energy_saving', 'menu_contact_groups', 'menu_email', 'menu_marketing', 'menu_tasks','menu_agenda','menu_processes','menu_documents','menu_general_settings'],
         ];
 
-        foreach($roles as $roleName => $permissions) {
-            $role =  \Spatie\Permission\Models\Role::findByName($roleName);
+        foreach($newViewRoles as $newViewRoleName => $permissions) {
+            $role =  \Spatie\Permission\Models\Role::findByName($newViewRoleName);
             $role->syncPermissions($permissions);
 
         }

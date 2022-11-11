ca9091953b4cf0a002977e693273da813f0ee967
Wim Mosman <46068567+Mosmania@users.noreply.github.com>
2022-11-11 09:36:42 +0100

    Update 2019_12_27_154346_change_energy_suppliers_does_pcr.php
    Bestaande migrations hoeven niet aangepast te worden.

--- database/migrations/2019_12_27_154346_change_energy_suppliers_does_pcr.php
+++ database/migrations/2019_12_27_154346_change_energy_suppliers_does_pcr.php
@@ -25,6 +25,9 @@ class ChangeEnergySuppliersDoesPcr extends Migration
             ->where('name', 'Main energie')
             ->update(['does_postal_code_links' => 1]);
         DB::table('energy_suppliers')
+           ->where('name', 'NL Energie')
+            ->update(['does_postal_code_links' => 1]);
+        DB::table('energy_suppliers')
             ->where('name', 'Vattenfall')
             ->update(['does_postal_code_links' => 1]);
         DB::table('energy_suppliers')

caaf7e2cfcdd3aeb0b240b7ca5a0d7b6c00f7deb
Mosmania <wim@mosmania.nl>
2022-11-30 16:31:56 +0100

    Moved fields answer, floor, side and typeBrand to new rows and show only if they are filled in.

--- client-app/src/container/housing-file/details/housing-file-specifications/HousingFileSpecificationView.js
+++ client-app/src/container/housing-file/details/housing-file-specifications/HousingFileSpecificationView.js
@@ -8,31 +8,89 @@ const HousingFileSpecificationView = props => {
     const { id, measure, status, measureDate, answer, floor, side, typeBrand } = props.housingFileSpecification;
 
     return (
-        <div
-            className={`row border ${props.highlightLine}`}
-            onMouseEnter={() => props.onLineEnter()}
-            onMouseLeave={() => props.onLineLeave()}
-        >
-            <div>
-                <div className="col-sm-2">{measure.name}</div>
-                <div className="col-sm-1">{status ? status.name : ''}</div>
-                <div className="col-sm-1">{measureDate && moment(measureDate).format('L')}</div>
-                <div className="col-sm-2">{answer ? answer : ''}</div>
-                <div className="col-sm-1">{floor ? floor.name : ''}</div>
-                <div className="col-sm-1">{side ? side.name : ''}</div>
-                <div className="col-sm-1">{typeBrand ? typeBrand : ''}</div>
-                <div className="col-sm-2">{measure.measureCategory && measure.measureCategory.name}</div>
+        <>
+            <div
+                className={`row border ${props.highlightLine}`}
+                onMouseEnter={() => props.onLineEnter()}
+                onMouseLeave={() => props.onLineLeave()}
+            >
+                <div>
+                    <div className="col-sm-3">{measure.name}</div>
+                    <div className="col-sm-3">{measure.measureCategory && measure.measureCategory.name}</div>
+                    <div className="col-sm-3">{status ? status.name : ''}</div>
+                    <div className="col-sm-2">{measureDate && moment(measureDate).format('L')}</div>
+                    {/*<div className="col-sm-2">{answer ? answer : ''}</div>*/}
+                    {/*<div className="col-sm-1">{floor ? floor.name : ''}</div>*/}
+                    {/*<div className="col-sm-1">{side ? side.name : ''}</div>*/}
+                    {/*<div className="col-sm-1">{typeBrand ? typeBrand : ''}</div>*/}
+                </div>
+                <div className="col-sm-1">
+                    {props.permissions.manageHousingFile && props.showActionButtons ? (
+                        <a role="button" onClick={props.toggleDelete}>
+                            <span className="glyphicon glyphicon-trash mybtn-danger" />{' '}
+                        </a>
+                    ) : (
+                        ''
+                    )}
+                </div>
             </div>
-            <div className="col-sm-1">
-                {props.permissions.manageHousingFile && props.showActionButtons ? (
-                    <a role="button" onClick={props.toggleDelete}>
-                        <span className="glyphicon glyphicon-trash mybtn-danger" />{' '}
-                    </a>
-                ) : (
-                    ''
-                )}
-            </div>
-        </div>
+            {answer ? (
+                <div
+                    className={`row border ${props.highlightLine}`}
+                    onMouseEnter={() => props.onLineEnter()}
+                    onMouseLeave={() => props.onLineLeave()}
+                >
+                    <div>
+                        <div className="col-sm-1">&nbsp;</div>
+                        <div className="col-sm-2">Waarde:</div>
+                        <div className="col-sm-8">{answer}</div>
+                        <div className="col-sm-1">&nbsp;</div>
+                    </div>
+                </div>
+            ) : null}
+            {floor ? (
+                <div
+                    className={`row border ${props.highlightLine}`}
+                    onMouseEnter={() => props.onLineEnter()}
+                    onMouseLeave={() => props.onLineLeave()}
+                >
+                    <div>
+                        <div className="col-sm-1">&nbsp;</div>
+                        <div className="col-sm-2">Verdieping:</div>
+                        <div className="col-sm-8">{floor}</div>
+                        <div className="col-sm-1">&nbsp;</div>
+                    </div>
+                </div>
+            ) : null}
+            {side ? (
+                <div
+                    className={`row border ${props.highlightLine}`}
+                    onMouseEnter={() => props.onLineEnter()}
+                    onMouseLeave={() => props.onLineLeave()}
+                >
+                    <div>
+                        <div className="col-sm-1">&nbsp;</div>
+                        <div className="col-sm-2">Zijde:</div>
+                        <div className="col-sm-8">{side}</div>
+                        <div className="col-sm-1">&nbsp;</div>
+                    </div>
+                </div>
+            ) : null}
+            {typeBrand ? (
+                <div
+                    className={`row border ${props.highlightLine}`}
+                    onMouseEnter={() => props.onLineEnter()}
+                    onMouseLeave={() => props.onLineLeave()}
+                >
+                    <div>
+                        <div className="col-sm-1">&nbsp;</div>
+                        <div className="col-sm-2">Type/merk:</div>
+                        <div className="col-sm-8">{typeBrand}</div>
+                        <div className="col-sm-1">&nbsp;</div>
+                    </div>
+                </div>
+            ) : null}
+        </>
     );
 };
 

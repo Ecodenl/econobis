c104063fe27e87a13269e60f8aa9b7be129208d6
Mosmania <wim@mosmania.nl>
2022-12-07 15:45:39 +0100

    Changed styled div for datepicker.

--- client-app/src/components/dataTable/DataTableFilterDateStartEnd.js
+++ client-app/src/components/dataTable/DataTableFilterDateStartEnd.js
@@ -76,27 +76,27 @@ export default DataTableFilterDateStartEnd;
 
 const StyledWrapper = styled.div`
     display: flex;
-
-    // & .DayPicker-Day--selected:not(.DayPicker-Day--start):not(.DayPicker-Day--end):not(.DayPicker-Day--outside) {
-    //     background-color: #f0f8ff !important;
-    //     color: #4a90e2;
-    // }
-
-    & .DayPicker-Day {
-        border-radius: 0 !important;
-    }
-    & .DayPicker-Day--start {
-        border-top-left-radius: 50% !important;
-        border-bottom-left-radius: 50% !important;
-    }
-    & .DayPicker-Day--end {
-        border-top-right-radius: 50% !important;
-        border-bottom-right-radius: 50% !important;
-    }
-    & .DayPickerInput-Overlay {
-        width: 600px;
-    }
-    // .InputFromTo-to .DayPickerInput-Overlay {
-    //     margin-left: -198px;
-    // }
+    //
+    //     // & .DayPicker-Day--selected:not(.DayPicker-Day--start):not(.DayPicker-Day--end):not(.DayPicker-Day--outside) {
+    //     //     background-color: #f0f8ff !important;
+    //     //     color: #4a90e2;
+    //     // }
+    //
+    //     & .DayPicker-Day {
+    //         border-radius: 0 !important;
+    //     }
+    //     & .DayPicker-Day--start {
+    //         border-top-left-radius: 50% !important;
+    //         border-bottom-left-radius: 50% !important;
+    //     }
+    //     & .DayPicker-Day--end {
+    //         border-top-right-radius: 50% !important;
+    //         border-bottom-right-radius: 50% !important;
+    //     }
+    //     // & .DayPickerInput-Overlay {
+    //     //     width: 200px;
+    //     // }
+    //     // .InputFromTo-to .DayPickerInput-Overlay {
+    //     //     margin-left: -198px;
+    //     // }
 `;

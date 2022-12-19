bdfa96d11c8684b530255f9da55863f2ba2738f7
Jos Kolenberg <jos@kolenbergsoftwareontwikkeling.nl>
2022-12-19 13:27:03 +0100

    Added plan quotation request button to opportunity

--- client-app/src/container/opportunities/details/quotation-request/OpportunityDetailsQuotationRequests.js
+++ client-app/src/container/opportunities/details/quotation-request/OpportunityDetailsQuotationRequests.js
@@ -1,71 +1,103 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import OpportunityDetailsQuotationRequestsList from './OpportunityDetailsQuotationRequestsList';
 import Panel from '../../../../components/panel/Panel';
 import PanelBody from '../../../../components/panel/PanelBody';
 import PanelHeader from '../../../../components/panel/PanelHeader';
-import { connect } from 'react-redux';
-import { hashHistory } from 'react-router';
+import {connect} from 'react-redux';
+import {hashHistory} from 'react-router';
+import DistrictAPI from "../../../../api/district/DistrictAPI";
+import OpportunityDetailsQuotationRequestPlanByDistrictModal
+    from "./OpportunityDetailsQuotationRequestPlanByDistrictModal";
 
 class OpportunityDetailsQuotationRequests extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            districts: [],
+            setShowPlanByDistrictModal: false,
+        }
+    }
+
+    componentDidMount() {
+        DistrictAPI.fetchDistricts().then((data) => {
+            this.setState({districts: data});
+        });
     }
 
     render() {
-        const { opportunityActions, opportunityId } = this.props;
+        const {opportunityActions, opportunityId} = this.props;
 
         return (
-            <Panel>
-                <PanelHeader>
-                    <span className="h5 text-bold">Acties</span>
-                    {this.props.permissions.manageQuotationRequest && opportunityActions.length == 1 ? (
-                        <a
-                            role="button"
-                            className="pull-right"
-                            onClick={() =>
-                                hashHistory.push(
-                                    `/offerteverzoek/nieuw/kans/${opportunityId}/actie/${opportunityActions[0].id}`
-                                )
-                            }
-                        >
-                            <span className="glyphicon glyphicon-plus" />
-                        </a>
-                    ) : null}
-
-                    {this.props.permissions.manageQuotationRequest && opportunityActions.length > 1 ? (
-                        <div className="nav navbar-nav btn-group pull-right" role="group">
-                            <button className="btn btn-link" data-toggle="dropdown">
-                                <span className="glyphicon glyphicon-plus" />
-                            </button>
-                            <ul className="dropdown-menu">
-                                {opportunityActions.map((opportunityAction, i) => {
-                                    return (
-                                        <li>
-                                            <a
-                                                role={'button'}
-                                                title={opportunityAction.name}
-                                                onClick={() =>
-                                                    hashHistory.push(
-                                                        `/offerteverzoek/nieuw/kans/${opportunityId}/actie/${opportunityAction.id}`
-                                                    )
-                                                }
-                                            >
-                                                {opportunityAction.name}
-                                            </a>
-                                        </li>
-                                    );
-                                })}
-                            </ul>
+            <>
+                <Panel>
+                    <PanelHeader>
+                        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+                            <span className="h5 text-bold">Acties</span>
+                            <div>
+                                {this.state.districts.length > 0 && (
+                                    <>
+                                        <button className="btn btn-link"
+                                                onClick={() => this.setState({setShowPlanByDistrictModal: true})}
+                                        >
+                                            <span className="glyphicon glyphicon-calendar"/>
+                                        </button>
+                                    </>
+                                )}
+                                {this.props.permissions.manageQuotationRequest && opportunityActions.length == 1 ? (
+                                    <button className="btn btn-link" onClick={() =>
+                                        hashHistory.push(
+                                            `/offerteverzoek/nieuw/kans/${opportunityId}/actie/${opportunityActions[0].id}`
+                                        )
+                                    }
+                                    >
+                                        <span className="glyphicon glyphicon-plus"/>
+                                    </button>
+                                ) : null}
+                                {this.props.permissions.manageQuotationRequest && opportunityActions.length > 1 ? (
+                                    <div className="nav navbar-nav btn-group pull-right" role="group">
+                                        <button className="btn btn-link" data-toggle="dropdown">
+                                            <span className="glyphicon glyphicon-plus"/>
+                                        </button>
+                                        <ul className="dropdown-menu">
+                                            {opportunityActions.map((opportunityAction, i) => {
+                                                return (
+                                                    <li>
+                                                        <a
+                                                            role={'button'}
+                                                            title={opportunityAction.name}
+                                                            onClick={() =>
+                                                                hashHistory.push(
+                                                                    `/offerteverzoek/nieuw/kans/${opportunityId}/actie/${opportunityAction.id}`
+                                                                )
+                                                            }
+                                                        >
+                                                            {opportunityAction.name}
+                                                        </a>
+                                                    </li>
+                                                );
+                                            })}
+                                        </ul>
+                                    </div>
+                                ) : null}
+                            </div>
                         </div>
-                    ) : null}
-                </PanelHeader>
-                <PanelBody>
-                    <div className="col-md-12">
-                        <OpportunityDetailsQuotationRequestsList />
-                    </div>
-                </PanelBody>
-            </Panel>
+                    </PanelHeader>
+                    <PanelBody>
+                        <div className="col-md-12">
+                            <OpportunityDetailsQuotationRequestsList/>
+                        </div>
+                    </PanelBody>
+                </Panel>
+                {this.state.setShowPlanByDistrictModal && (
+                    <OpportunityDetailsQuotationRequestPlanByDistrictModal
+                        districts={this.state.districts}
+                        onCancel={() => this.setState({setShowPlanByDistrictModal: false})}
+                        opportunityId={opportunityId}
+                    />
+                )}
+            </>
         );
     }
 }

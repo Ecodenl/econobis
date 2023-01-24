c4524153d6c25998d6eece7ee5f3ae6fac52c0ba
patrickkoeman <121806193+patrickkoeman@users.noreply.github.com>
2023-01-24 10:21:17 +0100

    changed ChangeQuotationRequestStatus migration name to ChangeQuotationRequestStatusNames because of duplicate migration name

--- database/migrations/2023_01_23_165252_change_quotation_request_status_names.php
+++ database/migrations/2023_01_23_165252_change_quotation_request_status_names.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ChangeQuotationRequestStatus extends Migration
+class ChangeQuotationRequestStatusNames extends Migration
 {
     /**
      * Run the migrations.

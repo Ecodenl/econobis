967f25fe5aefec06d7252a8e7e40d0a2fcd82bad
Wim Mosman <46068567+Mosmania@users.noreply.github.com>
2023-02-21 17:09:09 +0100

    Update 2023_02_16_124630_seed_quotation_request_status_table.php

--- database/migrations/2023_02_16_124630_seed_quotation_request_status_table.php
+++ database/migrations/2023_02_16_124630_seed_quotation_request_status_table.php
@@ -13,10 +13,12 @@ class SeedQuotationRequestStatusTable extends Migration
      */
     public function up(): void
     {
+        $budgetAanvraagAction = OpportunityAction::where('code_ref', 'subsidy-request')->first();
+
         DB::table('quotation_request_status')->insert([
             [
                 'name' => 'Budgetaanvraag verstuurd naar bewoner',
-                'opportunity_action_id' => 3,
+                'opportunity_action_id' => $budgetAanvraagAction->id,
                 'uses_wf' => 0,
                 'email_template_id_wf' => null,
                 'number_of_days_to_send_email' => 0,
@@ -26,7 +28,7 @@ class SeedQuotationRequestStatusTable extends Migration
                 'is_pending_status' => 0
             ], [
                 'name' => 'Subsidieaanvraag in behandeling',
-                'opportunity_action_id' => 3,
+                'opportunity_action_id' => $budgetAanvraagAction->id,
                 'uses_wf' => 0,
                 'email_template_id_wf' => null,
                 'number_of_days_to_send_email' => 0,
@@ -36,7 +38,7 @@ class SeedQuotationRequestStatusTable extends Migration
                 'is_pending_status' => 0
             ], [
                 'name' => 'Subsidieaanvraag akkoord',
-                'opportunity_action_id' => 3,
+                'opportunity_action_id' => $budgetAanvraagAction->id,
                 'uses_wf' => 0,
                 'email_template_id_wf' => null,
                 'number_of_days_to_send_email' => 0,
@@ -46,7 +48,7 @@ class SeedQuotationRequestStatusTable extends Migration
                 'is_pending_status' => 0
             ], [
                 'name' => 'Subsidieaanvraag niet akkoord',
-                'opportunity_action_id' => 3,
+                'opportunity_action_id' => $budgetAanvraagAction->id,
                 'uses_wf' => 0,
                 'email_template_id_wf' => null,
                 'number_of_days_to_send_email' => 0,

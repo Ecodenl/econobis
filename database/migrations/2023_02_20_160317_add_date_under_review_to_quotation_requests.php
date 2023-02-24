96137e96434f7e6f98ac526beb726eecaafe3db7
Mosmania <wim@mosmania.nl>
2023-02-24 15:45:00 +0100

    Delete double quotation_request_status 'Subsidieaanvraag in behandeling'

--- database/migrations/2023_02_20_160317_add_date_under_review_to_quotation_requests.php
+++ database/migrations/2023_02_20_160317_add_date_under_review_to_quotation_requests.php
@@ -15,11 +15,6 @@ class AddDateUnderReviewToQuotationRequests extends Migration
     {
         Schema::table('quotation_requests', function (Blueprint $table) {
             $table->date('date_under_review')->nullable()->default(null)->after('date_released');
-
-            DB::table('quotation_request_status')->insert([
-                    ['name' => 'Subsidieaanvraag in behandeling', 'opportunity_action_id' => 2, 'uses_wf' => 0, 'email_template_id_wf' => null, 'number_of_days_to_send_email' => 0, 'created_at' => \Carbon\Carbon::now(), 'updated_at' => \Carbon\Carbon::now(), 'order' => 1],
-                ]
-            );
         });
     }
 

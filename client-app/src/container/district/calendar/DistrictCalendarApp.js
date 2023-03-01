ec619b3ad5ce65b9047fb3eb962624b610eb21e5
Mosmania <wim@mosmania.nl>
2023-03-01 11:17:51 +0100

    Invalid dateformat for min prop BigCalendar. Now set to "YYYY-MM-DD 06:00:00" instead of "D MMM YYYY, 6:00"

--- client-app/src/container/district/calendar/DistrictCalendarApp.js
+++ client-app/src/container/district/calendar/DistrictCalendarApp.js
@@ -1,19 +1,19 @@
-import React, {useEffect, useState} from "react";
-import BigCalendar from "react-big-calendar";
-import moment from "moment";
+import React, { useEffect, useState } from 'react';
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import './DistrictCalendar.css';
 import Panel from '../../../components/panel/Panel';
 import PanelBody from '../../../components/panel/PanelBody';
-import ButtonIcon from "../../../components/button/ButtonIcon";
-import {browserHistory, hashHistory} from "react-router";
-import DistrictAPI from "../../../api/district/DistrictAPI";
-import InputToggle from "../../../components/form/InputToggle";
-import InputSelect from "../../../components/form/InputSelect";
+import ButtonIcon from '../../../components/button/ButtonIcon';
+import { browserHistory, hashHistory } from 'react-router';
+import DistrictAPI from '../../../api/district/DistrictAPI';
+import InputToggle from '../../../components/form/InputToggle';
+import InputSelect from '../../../components/form/InputSelect';
 
 moment.locale('nl');
 
-const DistrictCalendarApp = (props) => {
+const DistrictCalendarApp = props => {
     const [view, setView] = useState('week');
     const [date, setDate] = useState(new Date());
     const [events, setEvents] = useState([]);
@@ -52,7 +52,9 @@ const DistrictCalendarApp = (props) => {
                     coach: item.coach,
                     title: item.coach.fullName,
                     start: new Date(item.datePlanned),
-                    end: moment(item.datePlanned).add(item.durationMinutes, 'm').toDate(),
+                    end: moment(item.datePlanned)
+                        .add(item.durationMinutes, 'm')
+                        .toDate(),
                 };
             });
 
@@ -69,59 +71,62 @@ const DistrictCalendarApp = (props) => {
             setEvents(quotationRequests.concat(availabilities));
             setDistrict(data.district);
         });
-    }
+    };
 
-    const selectEventHandler = (event) => {
+    const selectEventHandler = event => {
         switch (event.type) {
             case 'quotationRequest':
                 hashHistory.push(`/offerteverzoek/${event.id}`);
         }
-    }
+    };
 
-    const eventPropGetter = (event) => {
-        return {className: getEventClass(event)};
-    }
+    const eventPropGetter = event => {
+        return { className: getEventClass(event) };
+    };
 
-    const getEventClass = (event) => {
+    const getEventClass = event => {
         switch (event.type) {
             case 'quotationRequest':
                 return 'quotation-request';
             case 'availability':
                 return 'availability';
         }
-    }
+    };
 
     const getFilteredEvents = () => {
         return events.filter(event => {
-            if(!showAvailabilities && event.type === 'availability') {
+            if (!showAvailabilities && event.type === 'availability') {
                 return false;
             }
 
-            if(filterCoachId && event.coach.id !== parseInt(filterCoachId)) {
+            if (filterCoachId && event.coach.id !== parseInt(filterCoachId)) {
                 return false;
             }
 
             return true;
         });
-    }
+    };
 
     const getCoaches = () => {
         // Filter unique coaches (id and fullName) from events ordered by fullName
-        return events.reduce((carry, event) => {
-            if(!carry.find(coach => coach.id === event.coach.id)) {
-                carry.push(event.coach);
-            }
+        return events
+            .reduce((carry, event) => {
+                if (!carry.find(coach => coach.id === event.coach.id)) {
+                    carry.push(event.coach);
+                }
 
-            return carry;
-        }, []).sort((a, b) => {
-            return a.fullName.localeCompare(b.fullName);
-        }).map(coach => {
-            return {
-                id: coach.id,
-                name: coach.fullName,
-            };
-        });
-    }
+                return carry;
+            }, [])
+            .sort((a, b) => {
+                return a.fullName.localeCompare(b.fullName);
+            })
+            .map(coach => {
+                return {
+                    id: coach.id,
+                    name: coach.fullName,
+                };
+            });
+    };
 
     return (
         <Panel>
@@ -136,19 +141,33 @@ const DistrictCalendarApp = (props) => {
                         <h3 className="text-center table-title">Afspraakkalender {district.name}</h3>
                     </div>
                     <div className="col-md-4">
-                        <div style={{display: 'flex', flexDirection: 'column'}}>
-                            <div style={{display: 'flex'}}>
-                                <InputSelect size={'col-12 col-sm-12'} label={'Coach'} name={'coach_id'} options={getCoaches()} value={filterCoachId} onChangeAction={(e) => setFilterCoachId(e.target.value)} placeholder="Alle" />
+                        <div style={{ display: 'flex', flexDirection: 'column' }}>
+                            <div style={{ display: 'flex' }}>
+                                <InputSelect
+                                    size={'col-12 col-sm-12'}
+                                    label={'Coach'}
+                                    name={'coach_id'}
+                                    options={getCoaches()}
+                                    value={filterCoachId}
+                                    onChangeAction={e => setFilterCoachId(e.target.value)}
+                                    placeholder="Alle"
+                                />
                             </div>
                             <div>
-                                <InputToggle divSize={'col-12 col-sm-12'} label="Toon beschikbaarheid" name={'show_availabilities'} value={showAvailabilities} onChangeAction={(e) => setShowAvailabilities(e.target.checked)} />
+                                <InputToggle
+                                    divSize={'col-12 col-sm-12'}
+                                    label="Toon beschikbaarheid"
+                                    name={'show_availabilities'}
+                                    value={showAvailabilities}
+                                    onChangeAction={e => setShowAvailabilities(e.target.checked)}
+                                />
                             </div>
                         </div>
                     </div>
                 </div>
                 <BigCalendar
                     events={getFilteredEvents()}
-                    style={{height: 'calc(100vh - 200px)'}}
+                    style={{ height: 'calc(100vh - 200px)' }}
                     step={15}
                     timeslots={4}
                     views={['month', 'week', 'day']}
@@ -172,11 +191,11 @@ const DistrictCalendarApp = (props) => {
                         time: 'Tijd',
                         showMore: total => `+${total} meer`,
                     }}
-                    min={new Date(`${moment().format('D MMM YYYY')}, 6:00`)}
+                    min={new Date(`${moment().format('YYYY-MM-DD')} 06:00:00`)}
                 />
             </PanelBody>
         </Panel>
     );
-}
+};
 
 export default DistrictCalendarApp;

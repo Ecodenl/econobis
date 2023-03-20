2a607a91f81d529b232aafe4fab70dec884a254b
Mosmania <wim@mosmania.nl>
2023-03-20 16:34:03 +0100

    Add validatePost and doPost functionality for endPointAfspraak

--- app/Http/Controllers/Api/Hoomdossier/EndPointAfspraakController.php
+++ app/Http/Controllers/Api/Hoomdossier/EndPointAfspraakController.php
@@ -8,12 +8,16 @@
 
 namespace App\Http\Controllers\Api\Hoomdossier;
 
+use App\Eco\Contact\Contact;
+use App\Eco\Opportunity\OpportunityAction;
+use App\Eco\QuotationRequest\QuotationRequest;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Response;
 
 class EndPointAfspraakController extends EndPointHoomDossierController
 {
+    protected $coach = null;
     protected $logs = [];
 
     public function post(string $apiKey, Request $request)
@@ -33,6 +37,7 @@ class EndPointAfspraakController extends EndPointHoomDossierController
                 }
 
                 $this->processAccountRelatedData($dataContent->account_related);
+                $this->validatePost($dataContent);
                 $this->doPost($dataContent);
             });
         } catch (HoomdossierException $e) {
@@ -47,11 +52,46 @@ class EndPointAfspraakController extends EndPointHoomDossierController
         return Response::json($this->logs);
     }
 
+    protected function validatePost($dataContent)
+    {
+        if(!$dataContent->appointment_date) {
+            $this->error('Geen appointment_date meegegeven', 404);
+        }
+        if(!$this->cooperation->hoom_campaign_id) {
+            $this->error('Geen Hoomdossier campagne voor cooperatie gevonden', 404);
+        }
+        if(!$dataContent->coaches) {
+            $this->error('Geen coach meegegeven', 404);
+        }
+        $firstCoachId = $dataContent->coaches[0]->contact_id;
+        $coach = Contact::find($firstCoachId);
+        if(!$coach || !$coach->isCoach()) {
+            $this->error('Gekoppelde coach is geen coach in Econobis', 404);
+        } else {
+            $this->coach = $coach;
+        }
+    }
     protected function doPost($dataContent)
     {
-        $this->log('Binnenkomende payload (zie laravel log)');
-        Log::info(json_encode($dataContent));
-        $this->log('Hier check en verwerkingen inzake endpoint afspraak.');
+        $bezoekAction = OpportunityAction::where('code_ref', 'visit')->first();
+
+        $quotationRequest = QuotationRequest::where('opportunity_action_id', $bezoekAction->id)
+            ->where('contact_id', $this->coach->id)
+            ->WhereHas('opportunity', function($query){
+                $query->WhereHas('intake', function($query2){
+                    $query2->where('contact_id', $this->contact->id)
+                        ->where('campaign_id', $this->cooperation->hoom_campaign_id);
+                });
+            })
+            ->orderby('id', 'desc')->first();
+        if(!$quotationRequest) {
+            $this->error('Afspraak niet gevonden in Econobis', 404);
+        }
+
+        $quotationRequest->date_planned = Carbon::parse($dataContent->appointment_date);
+        $quotationRequest->save();
+
+        $this->log('Afspraak ' .  Carbon::parse($quotationRequest->date_planned)->format('d-m-Y H:i'). ' bijgewerkt voor bezoek coach ' . $this->coach->full_name_fnf . ' bij bewoner ' . $this->contact->full_name_fnf);
     }
 
 }

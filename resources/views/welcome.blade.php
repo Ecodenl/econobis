77f503f580700d97b4b9cb6dae45ae0dcb975d2e
Mosmania <wim@mosmania.nl>
2023-04-13 09:43:05 +0200

    Build client-app and customer-portal-app and set version to 4.4.0.x

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,24 +1,5 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Econobis</title>
-
-</head>
-<body>
-<script type="text/javascript" src="./js/polyfill.js"></script>
-
-<script type="text/javascript">
-    // Set some global JS variables
+<!doctype html><html lang="{{ app()->getLocale() }}"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1"><title>Econobis</title><script defer="defer" src="./js/6988.39fb25a7cb6d525d1a85.js"></script><script defer="defer" src="./js/bundle.c1f8091c248a3c33e849.js"></script></head><body><script src="./js/polyfill.js"></script><script>// Set some global JS variables
     var URL_APP ="{{ config('app.url') }}";
     var URL_API ="{{ config('app.url_api') }}";
     var CLIENT_ID ="{{ config('app.oauth_client_id') }}";
-    var CLIENT_KEY ="{{ $clientKey }}";
-</script>
-
-<div id="root"></div>
-<script type="text/javascript" src="./js/vendors~bundle.014aacba3563be36938b.js"></script><script type="text/javascript" src="./js/bundle.582955001b2adcef8b77.js"></script></body>
-</html>
+    var CLIENT_KEY ="{{ $clientKey }}";</script><div id="root"></div></body></html>

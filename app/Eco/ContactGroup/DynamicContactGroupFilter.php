bc4c466b98dfb131107a369ce9646b55b039effe
patrickkoeman <121806193+patrickkoeman@users.noreply.github.com>
2023-05-10 16:17:35 +0200

    added didUnderstandInfo to the $yesNoFields array for dynamic group filter data return so its Ja instead of 1 in the filter list

--- app/Eco/ContactGroup/DynamicContactGroupFilter.php
+++ app/Eco/ContactGroup/DynamicContactGroupFilter.php
@@ -35,7 +35,7 @@ class DynamicContactGroupFilter extends Model
             if (preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/", $this->data))  return Carbon::parse($this->data)->format('d-m-Y');
 
             // Booleans omzetten
-            $yesNoFields = ['didAcceptAgreement', 'didAgreeAvg', 'portalUser', 'housingFile'];
+            $yesNoFields = ['didAcceptAgreement', 'didAgreeAvg', 'portalUser', 'housingFile','didUnderstandInfo'];
             if (in_array($this->field, $yesNoFields)) return $this->data ? 'Ja' : 'Nee';
 
             // opportunityMeasureCategory omzetten

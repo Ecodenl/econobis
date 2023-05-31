2232cbcd70685605165d94ae4c2528041199ad0a
Mosmania <wim@mosmania.nl>
2023-05-31 14:39:50 +0200

    Fix check and recover also for is_energy_supplier_switch, is_end_participation, is_end_total_period and is_end_year_period, not only for is_visible. Recover only if difference found. Cleanup code. Reschedule new command.

--- app/Console/Commands/checkAndFixWrongRevenueDistributionPartsKwhIndicatorFields.php
+++ app/Console/Commands/checkAndFixWrongRevenueDistributionPartsKwhIndicatorFields.php
@@ -8,24 +8,25 @@ use App\Helpers\Email\EmailHelper;
 use App\Http\Resources\Email\Templates\GenericMailWithoutAttachment;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 
-class checkAndFixWrongRevenueDistributionPartsKwhIndicatorFields extends Command
+class checkWrongRevenueDistributionPartsKwhIndicatorFields extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'revenue:checkAndFixWrongRevenueDistributionPartsKwhIndicatorFields {--recover=true}';
-    protected $mailTo = 'patrick.koeman@xaris.nl';
+    protected $signature = 'revenue:checkWrongRevenueDistributionPartsKwhIndicatorFields {--recover=false}';
+    protected $mailTo = 'wim.mosman@xaris.nl';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Check and fix wrong revenue distribution parts kwh indicator fields';
+    protected $description = 'Wrong revenue distribution parts kwh indicator fields';
 
     /**
      * Create a new command instance.
@@ -47,55 +48,76 @@ class checkAndFixWrongRevenueDistributionPartsKwhIndicatorFields extends Command
         // met of zonder herstel?
         $doRecover = $this->option('recover') == 'true';
 
-        $revenueDistributionPartKwhWrong = "";
+        Log::info($this->description . ($doRecover ? ' MET HERSTEL!' : '') );
+
+        $wrongRevenueDistributionPartsKwh = [];
 
         foreach(RevenueDistributionPartsKwh::get() as $revenueDistributionPartKwh) {
+            $isEnergySupplierSwitch = false;
+            $isEndParticipation = false;
+            $isEndTotalPeriod = false;
+            $isEndYearPeriod = false;
+            $isVisible = false;
+
             if (AddressEnergySupplier::where('address_id', $revenueDistributionPartKwh->distributionKwh->participation->address_id)->where('energy_supplier_id', $revenueDistributionPartKwh->es_id)->where('end_date', $revenueDistributionPartKwh->partsKwh->date_end)->exists()) {
-                $revenueDistributionPartKwh->is_energy_supplier_switch = true;
-            } else {
-                $revenueDistributionPartKwh->is_energy_supplier_switch = false;
+                $isEnergySupplierSwitch = true;
             }
             if ($revenueDistributionPartKwh->distributionKwh->participation->date_terminated == $revenueDistributionPartKwh->partsKwh->date_end) {
-                $revenueDistributionPartKwh->is_end_participation = true;
-            } else {
-                $revenueDistributionPartKwh->is_end_participation = false;
+                $isEndParticipation = true;
             }
             if ($revenueDistributionPartKwh->partsKwh->date_end && $revenueDistributionPartKwh->partsKwh->date_end == $revenueDistributionPartKwh->partsKwh->revenuesKwh->date_end) {
-                $revenueDistributionPartKwh->is_end_total_period = true;
-            } else {
-                $revenueDistributionPartKwh->is_end_total_period = false;
+                $isEndTotalPeriod = true;
             }
             if ($revenueDistributionPartKwh->partsKwh->date_end && Carbon::parse($revenueDistributionPartKwh->partsKwh->date_end)->day == 31 && Carbon::parse($revenueDistributionPartKwh->partsKwh->date_end)->month == 12) {
-                $revenueDistributionPartKwh->is_end_year_period = true;
-            } else {
-                $revenueDistributionPartKwh->is_end_year_period = false;
+                $isEndYearPeriod = true;
             }
 
-            if ($revenueDistributionPartKwh->is_visible === 0) {
-                $is_visible = false;
-            } else {
-                $is_visible = true;
+            if ($isEnergySupplierSwitch || $isEndParticipation || $isEndTotalPeriod || $isEndYearPeriod) {
+                $isVisible = true;
             }
 
-            if($this->determineIsVisible($revenueDistributionPartKwh) !== $is_visible) {
-                $revenueDistributionPartKwhWrong .=
-                    "part_id: " .  $revenueDistributionPartKwh->id . "<br>" .
-                    "distribution_id: " .  $revenueDistributionPartKwh->distribution_id . "<br>" .
-                    "part_date_begin: " .  $revenueDistributionPartKwh->partsKwh->date_begin . "<br>" .
-                    "part_date_end: " .  $revenueDistributionPartKwh->partsKwh->date_end . "<br><br>"
-                ;
+            if($revenueDistributionPartKwh->is_energy_supplier_switch != $isEnergySupplierSwitch
+                || $revenueDistributionPartKwh->is_end_participation != $isEndParticipation
+                || $revenueDistributionPartKwh->is_end_total_period != $isEndTotalPeriod
+                || $revenueDistributionPartKwh->is_end_year_period != $isEndYearPeriod
+                || $revenueDistributionPartKwh->is_visible != $isVisible
+            ) {
+                $wrong  = ($revenueDistributionPartKwh->is_energy_supplier_switch != $isEnergySupplierSwitch ? 'ES' : ' ');
+                $wrong  .= '|' . ($revenueDistributionPartKwh->is_end_participation != $isEndParticipation ? 'ED' : ' ');
+                $wrong  .= '|' . ($revenueDistributionPartKwh->is_end_total_period != $isEndTotalPeriod ? 'TP' : ' ');
+                $wrong  .= '|' . ($revenueDistributionPartKwh->is_end_year_period != $isEndYearPeriod ? 'EJ' : ' ');
+                $wrong  .= '|' . ($revenueDistributionPartKwh->is_visible != $isVisible ? 'V' : ' ');
+
+                $wrongRevenueDistributionPartsKwh[] = [
+                    "revenue_id" => $revenueDistributionPartKwh->revenue_id,
+                    "project_id" => $revenueDistributionPartKwh->revenuesKwh->project_id,
+                    "distribution_id" => $revenueDistributionPartKwh->distribution_id,
+                    "part_id" => $revenueDistributionPartKwh->partsKwh->id,
+                    "distribution_part_id" => $revenueDistributionPartKwh->id,
+                    "part_date_begin" => $revenueDistributionPartKwh->partsKwh->date_begin,
+                    "part_date_end" => $revenueDistributionPartKwh->partsKwh->date_end,
+                    "wrong" => $wrong,
+                ];
+                if($doRecover){
+                    $revenueDistributionPartKwh->is_energy_supplier_switch = $isEnergySupplierSwitch;
+                    $revenueDistributionPartKwh->is_end_participation = $isEndParticipation;
+                    $revenueDistributionPartKwh->is_end_total_period = $isEndTotalPeriod;
+                    $revenueDistributionPartKwh->is_end_year_period = $isEndYearPeriod;
+                    $revenueDistributionPartKwh->is_visible = $this->determineIsVisible($revenueDistributionPartKwh);
+                    $revenueDistributionPartKwh->save();
+                }
             }
 
-            if($doRecover){
-                $revenueDistributionPartKwh->is_visible = $this->determineIsVisible($revenueDistributionPartKwh);
-                $revenueDistributionPartKwh->save();
-            }
         }
 
-        if($revenueDistributionPartKwhWrong != "") {
-            $subject = 'Wrong revenue distribution parts! - ' . \Config::get('app.APP_COOP_NAME');
-            $this->sendMail($subject, $revenueDistributionPartKwhWrong, $doRecover);
+        if(!empty($wrongRevenueDistributionPartsKwh)) {
+            $this->sendMail($wrongRevenueDistributionPartsKwh, $doRecover);
+            Log::info('Wrong revenue distribution parts kwh indicator fields gevonden, mail gestuurd');
+        } else {
+            Log::info('Geen wrong revenue distribution parts kwh indicator fields gevonden');
         }
+
+        Log::info('Check wrong revenue distribution parts kwh indicator fields klaar');
     }
 
 
@@ -114,18 +136,32 @@ class checkAndFixWrongRevenueDistributionPartsKwhIndicatorFields extends Command
         }
     }
 
-    private function sendMail($subject, $revenueDistributionPartKwhWrong, $doRecover)
+    private function sendMail($wrongRevenueDistributionPartsKwh, $doRecover)
     {
         (new EmailHelper())->setConfigToDefaultMailbox();
 
+        $subject = $this->description . ' (' . count($wrongRevenueDistributionPartsKwh) . ') - ' . \Config::get('app.APP_COOP_NAME');
+
+        $wrongRevenueDistributionPartsKwhHtml = "";
         if($doRecover){
-            $recoverHtml = "<p>MET HERSTEL!</p>";
-        } else {
-            $recoverHtml = "<p>ZONDER HERSTEL!</p>";
+            $wrongRevenueDistributionPartsKwhHtml .= "<p>MET HERSTEL!</p>";
+        }
+
+        foreach($wrongRevenueDistributionPartsKwh as $wrongRevenueDistributionPartKwh) {
+            $wrongRevenueDistributionPartsKwhHtml .=
+                "<>revenue_id: " . $wrongRevenueDistributionPartKwh['revenue_id'] . ", " .
+                "project_id: " . $wrongRevenueDistributionPartKwh['project_id'] . ", " .
+                "distribution_id: " . $wrongRevenueDistributionPartKwh['distribution_id'] . ", " .
+                "part_id: " . $wrongRevenueDistributionPartKwh['part_id'] . ", " .
+                "distribution_part_id: " . $wrongRevenueDistributionPartKwh['distribution_part_id'] . ", " .
+                "part_date_begin: " . $wrongRevenueDistributionPartKwh['part_date_begin'] . ", " .
+                "part_date_end: " . $wrongRevenueDistributionPartKwh['part_date_end'] . ", " .
+                "wrong: " . $wrongRevenueDistributionPartKwh['wrong'] . "</p>"
+            ;
         }
 
         $mail = Mail::to($this->mailTo);
-        $htmlBody = '<!DOCTYPE html><html><head><meta http-equiv="content-type" content="text/html;charset=UTF-8"/><title>Wrong distribution parts</title></head><body><p>'. $subject . '</p><p>' . \Config::get("app.name") .'</p><p>'. $revenueDistributionPartKwhWrong .'</p><p>'. $recoverHtml .'</p></body></html>';
+        $htmlBody = '<!DOCTYPE html><html><head><meta http-equiv="content-type" content="text/html;charset=UTF-8"/><title>.$subject.</title></head><body><p>'. $subject . '</p>' . $wrongRevenueDistributionPartsKwhHtml . '</body></html>';
 
         $mail->subject = $subject;
         $mail->html_body = $htmlBody;

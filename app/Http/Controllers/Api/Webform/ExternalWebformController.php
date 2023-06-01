80d3cbdd9ba017a4d96e454203b64d71d3c205b8
patrickkoeman <121806193+patrickkoeman@users.noreply.github.com>
2023-06-01 09:01:01 +0200

    fixed sending email to $webform->email_address_error_report by using a dummy user

--- app/Http/Controllers/Api/Webform/ExternalWebformController.php
+++ app/Http/Controllers/Api/Webform/ExternalWebformController.php
@@ -2885,7 +2885,11 @@ class ExternalWebformController extends Controller
                         $users = $webform->responsibleTeam->users;
                     }
                 } else {
-                    //$users->push($webform->email_address_error_report);
+                    $dummyUser = (new User)->forceFill([
+                        'id' => 99999999999999,
+                        'email' => $webform->email_address_error_report,
+                    ]);
+                    $users->push($dummyUser);
                 }
 
                 (new EmailHelper())->setConfigToDefaultMailbox();

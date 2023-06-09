c82d5f45347363d9f5e948dd50672328cc436f9b
patrickkoeman <121806193+patrickkoeman@users.noreply.github.com>
2023-06-09 15:53:24 +0200

    handling contacts in chuncks in report:contactGroupsContacts

--- app/Console/Commands/contactGroupsContactsForReport.php
+++ app/Console/Commands/contactGroupsContactsForReport.php
@@ -53,18 +53,20 @@ class contactGroupsContactsForReport extends Command
         foreach($contactGroups as $contactGroup) {
             $allContacts = $contactGroup->all_contact_group_contacts;
 
-            foreach($allContacts as $contact) {
-                DB::insert('insert into contact_groups_contacts_for_report (
-                    contact_id,
-                    contact_group_id,
-                    member_to_group_since
-                ) values (?, ?, ?)',
-                    [
-                        $contact->id,
-                        $contactGroup->id,
-                        $contact->member_to_group_since,
-                    ]
-                );
+            foreach($allContacts->chunk(500) as $chunks) {
+                foreach($chunks as $contact) {
+                    DB::insert('insert into contact_groups_contacts_for_report (
+                        contact_id,
+                        contact_group_id,
+                        member_to_group_since
+                    ) values (?, ?, ?)',
+                        [
+                            $contact->id,
+                            $contactGroup->id,
+                            $contact->member_to_group_since,
+                        ]
+                    );
+                }
             }
         }
 

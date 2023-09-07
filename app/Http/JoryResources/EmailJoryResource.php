60a48545ea34ed8b62c5532d1be4c2b3a540ccb4
Jos Kolenberg <jos@kolenbergsoftwareontwikkeling.nl>
2023-09-07 11:53:54 +0200

    Email; bij filteren op contacten ook kijken naar eenmalige contacten

--- app/Http/JoryResources/EmailJoryResource.php
+++ app/Http/JoryResources/EmailJoryResource.php
@@ -56,7 +56,9 @@ class EmailJoryResource extends JoryResource
         $this->field('updated_at')->filterable()->sortable();
 
         $this->filter('contacts.full_name');
+        $this->filter('manualContacts.full_name');
         $this->filter('contacts.contact_id');
+        $this->filter('manualContacts.contact_id');
         $this->filter('mailbox.name');
         $this->filter('responsibleUser.first_name');
         $this->filter('responsibleUser.last_name');

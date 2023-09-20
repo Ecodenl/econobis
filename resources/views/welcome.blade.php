700c216a395ebf95eb2467c7213ee9bd703ceb3e
Mosmania <wim@mosmania.nl>
2023-09-20 16:43:46 +0200

    Build client-app and customer-portal-app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
-<!doctype html><html lang="{{ app()->getLocale() }}"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1"><title>Econobis</title><script defer="defer" src="./js/7787.7ab3f3f9857be8017cd3.js"></script><script defer="defer" src="./js/bundle.cc794504d13b5eca501a.js"></script></head><body><script src="./js/polyfill.js"></script><script>// Set some global JS variables
-    var URL_APP ="{{ config('app.url') }}";
+<!doctype html><html lang="{{ app()->getLocale() }}"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1"><title>Econobis</title><script defer="defer" src="./js/7787.55a0637b39387b527b57.js"></script><script defer="defer" src="./js/bundle.416217201ff9f6a58324.js"></script></head><body><script src="./js/polyfill.js"></script><script>// Set some global JS variables
+    {{--var URL_APP ="{{ config('app.url') }}";--}}
     var URL_API ="{{ config('app.url_api') }}";
     var CLIENT_ID ="{{ config('app.oauth_client_id') }}";
     var CLIENT_KEY ="{{ $clientKey }}";</script><div id="root"></div></body></html>

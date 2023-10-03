2022c5c42a64335188b513b59fecd1df0b4f982e
Mosmania <wim@mosmania.nl>
2023-10-03 13:02:59 +0200

    Build client-app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!doctype html><html lang="{{ app()->getLocale() }}"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1"><title>Econobis</title><script defer="defer" src="./js/7787.7ab3f3f9857be8017cd3.js"></script><script defer="defer" src="./js/bundle.b9dad9e5b0a541eca27d.js"></script></head><body><script src="./js/polyfill.js"></script><script>// Set some global JS variables
+<!doctype html><html lang="{{ app()->getLocale() }}"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1"><title>Econobis</title><script defer="defer" src="./js/7787.abe6e1f1f7f13e294282.js"></script><script defer="defer" src="./js/bundle.830d82897c18c2c13b35.js"></script></head><body><script src="./js/polyfill.js"></script><script>// Set some global JS variables
     var URL_APP ="{{ config('app.url') }}";
     var URL_API ="{{ config('app.url_api') }}";
     var CLIENT_ID ="{{ config('app.oauth_client_id') }}";

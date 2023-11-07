d44f10e144e46d322ee6f361f58920247b56f1c4
Mosmania <wim@mosmania.nl>
2023-11-07 15:10:17 +0100

    New participant mutation type 'result_deposit' also for projectypes loan and obligation

--- database/migrations/2023_09_08_105901_add_result_deposit_types_to_participant_mutation_types_table.php
+++ database/migrations/2023_09_08_105901_add_result_deposit_types_to_participant_mutation_types_table.php
@@ -29,6 +29,20 @@ class AddResultDepositTypesToParticipantMutationTypesTable extends Migration
                 'project_type_id' => ProjectType::where('code_ref', 'postalcode_link_capital')->first()->id,
             ]
         );
+        DB::table('participant_mutation_types')->insert([
+                'name' => 'Resultaat',
+                'description' => 'Resultaat bijschrijven',
+                'code_ref' => 'result_deposit',
+                'project_type_id' => ProjectType::where('code_ref', 'loan')->first()->id,
+            ]
+        );
+        DB::table('participant_mutation_types')->insert([
+                'name' => 'Resultaat',
+                'description' => 'Resultaat bijschrijven',
+                'code_ref' => 'result_deposit',
+                'project_type_id' => ProjectType::where('code_ref', 'obligation')->first()->id,
+            ]
+        );
 
         $projectTypeCapital =ProjectType::where('code_ref', 'capital')->first()->id;
         $resultCapitalMutationType = ParticipantMutationType::where('code_ref', 'result')->where('project_type_id', $projectTypeCapital)->first()->id;
@@ -38,7 +52,15 @@ class AddResultDepositTypesToParticipantMutationTypesTable extends Migration
         $resultPcrMutationType = ParticipantMutationType::where('code_ref', 'result')->where('project_type_id', $projectTypePcr)->first()->id;
         $resultDepositPcrMutationType = ParticipantMutationType::where('code_ref', 'result_deposit')->where('project_type_id', $projectTypePcr)->first()->id;
 
-        $mutations = ParticipantMutation::whereIn('type_id', [$resultCapitalMutationType, $resultPcrMutationType])->get();
+        $projectTypeLoan =ProjectType::where('code_ref', 'loan')->first()->id;
+        $resultLoanMutationType = ParticipantMutationType::where('code_ref', 'result')->where('project_type_id', $projectTypeLoan)->first()->id;
+        $resultDepositLoanMutationType = ParticipantMutationType::where('code_ref', 'result_deposit')->where('project_type_id', $projectTypeLoan)->first()->id;
+
+        $projectTypeObligation =ProjectType::where('code_ref', 'obligation')->first()->id;
+        $resultObligationMutationType = ParticipantMutationType::where('code_ref', 'result')->where('project_type_id', $projectTypeObligation)->first()->id;
+        $resultDepositObligationMutationType = ParticipantMutationType::where('code_ref', 'result_deposit')->where('project_type_id', $projectTypeObligation)->first()->id;
+
+        $mutations = ParticipantMutation::whereIn('type_id', [$resultCapitalMutationType, $resultPcrMutationType, $resultLoanMutationType, $resultObligationMutationType])->get();
 
         foreach ($mutations as $mutation){
             if($mutation->type_id == $resultCapitalMutationType && $mutation->paid_on == 'Bijschrijven' ){
@@ -53,6 +75,20 @@ class AddResultDepositTypesToParticipantMutationTypesTable extends Migration
                     ->where('id', $mutation->id)
                     ->update([
                         'type_id' =>$resultDepositPcrMutationType,
+                    ]);
+            }
+            if($mutation->type_id == $resultLoanMutationType && $mutation->paid_on == 'Bijschrijven' ){
+                DB::table('participant_mutations')
+                    ->where('id', $mutation->id)
+                    ->update([
+                        'type_id' =>$resultDepositLoanMutationType,
+                    ]);
+            }
+            if($mutation->type_id == $resultObligationMutationType && $mutation->paid_on == 'Bijschrijven' ){
+                DB::table('participant_mutations')
+                    ->where('id', $mutation->id)
+                    ->update([
+                        'type_id' =>$resultDepositObligationMutationType,
                     ]);
             }
         }

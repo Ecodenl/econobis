0ec5c229ef1e4006f6c0c1e14376dbf839b40789
patrickkoeman <121806193+patrickkoeman@users.noreply.github.com>
2023-11-09 15:10:27 +0100

    added new last name prefix "Voor den"

---
+++ database/migrations/2023_11_09_150000_seed_voor_den_last_name_prefixe_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Migrations\Migration;
+
+class SeedVoorDenLastNamePrefixeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $prefixes = [
+            'Voor den',
+        ];
+
+        foreach ($prefixes as $prefix) {
+            DB::table('last_name_prefixes')->insert([
+                    ['name' => $prefix,
+                        'created_at' => Carbon::now(),
+                        'updated_at' => Carbon::now()
+                    ]
+                ]
+            );
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $prefixes = [
+            'Voor den',
+        ];
+
+        foreach ($prefixes as $prefix) {
+            DB::table('last_name_prefixes')->where('name', $prefix)->delete();
+        }
+    }
+}

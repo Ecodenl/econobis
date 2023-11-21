2ce72c37f1d9d0ea37e0a26a22baa272e3651b58
Mosmania <wim@mosmania.nl>
2023-11-21 15:56:53 +0100

    Change OpportunityStatus (current in_progress status changed to mandate and remove external_hoom_id, new in_progress status set with external_hoom_id 4 and order 4)

--- database/migrations/2023_11_13_124800_add_new_row_in_opportunity_status_table_november2023.php
+++ database/migrations/2023_11_13_124800_add_new_row_in_opportunity_status_table_november2023.php
@@ -3,6 +3,7 @@
 use App\Eco\Opportunity\OpportunityStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class AddNewRowInOpportunityStatusTableNovember2023 extends Migration
@@ -14,15 +15,22 @@ class AddNewRowInOpportunityStatusTableNovember2023 extends Migration
      */
     public function up()
     {
+        DB::table('opportunity_status')
+            ->where('code_ref', 'in_progress')
+            ->update([
+                'code_ref' => 'mandate',
+                'external_hoom_id' => null
+            ]);
+
         $opportunityStatus = New OpportunityStatus();
         $opportunityStatus->name = 'In uitvoering';
         $opportunityStatus->uses_wf = 0;
         $opportunityStatus->email_template_id_wf = null;
         $opportunityStatus->number_of_days_to_send_email = 0;
         $opportunityStatus->active = 1;
-        $opportunityStatus->external_hoom_id = null;
+        $opportunityStatus->external_hoom_id = 4;
         $opportunityStatus->code_ref = "in_progress";
-        $opportunityStatus->order = 5;
+        $opportunityStatus->order = 4;
         $opportunityStatus->save();
     }
 

3e3c776019a8ec0c18f5e3f606aa15f89e8672a1
Mosmania <wim@mosmania.nl>
2024-02-13 16:12:14 +0100

    Show uses_wf and is_active WF. If uses_wf is false of is_actice is false, show line in red. Cleanup code .

--- client-app/src/container/campaigns/details/campaign-workflows/CampaignDetailsWorkflowView.js
+++ client-app/src/container/campaigns/details/campaign-workflows/CampaignDetailsWorkflowView.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { hashHistory } from 'react-router';
 
 import Icon from 'react-icons-kit';
 import { pencil } from 'react-icons-kit/fa/pencil';
 import { trash } from 'react-icons-kit/fa/trash';
 
 const CampaignDetailsWorkflowView = ({
-    campaignWorkflow: { id, status, emailTemplateWorkflow, numberOfDaysToSendEmail },
+    campaignWorkflow: { id, status, emailTemplateWorkflow, numberOfDaysToSendEmail, isActive },
     highlightLine,
     onLineEnter,
     onLineLeave,
@@ -16,26 +15,29 @@ const CampaignDetailsWorkflowView = ({
     openEdit,
     toggleDelete,
 }) => {
+    const notActiveLine = !status.usesWf || !isActive ? 'text-danger' : '';
     return (
         <div className={`row border ${highlightLine}`} onMouseEnter={onLineEnter} onMouseLeave={onLineLeave}>
-            <div onClick={openEdit}>
+            <div onClick={openEdit} className={`${notActiveLine}`}>
                 <div className="col-sm-3">{status ? status.name : ''}</div>
-                <div className="col-sm-4">{emailTemplateWorkflow ? emailTemplateWorkflow.name : ''}</div>
-                <div className="col-sm-4">{numberOfDaysToSendEmail ? numberOfDaysToSendEmail : 'Direct'}</div>
-            </div>
-            <div className="col-sm-1">
-                {showActionButtons && permissions.manageMarketing ? (
-                    <>
-                        <a role="button" onClick={openEdit}>
-                            <Icon className="mybtn-success" size={14} icon={pencil} />
-                        </a>
-                        <a role="button" onClick={toggleDelete}>
-                            <Icon className="mybtn-danger" size={14} icon={trash} />
-                        </a>
-                    </>
-                ) : (
-                    ''
-                )}
+                <div className="col-sm-3">{emailTemplateWorkflow ? emailTemplateWorkflow.name : ''}</div>
+                <div className="col-sm-2">{numberOfDaysToSendEmail ? numberOfDaysToSendEmail : 'Direct'}</div>
+                <div className="col-sm-1">{status.usesWf ? 'Ja' : 'Nee'}</div>
+                <div className="col-sm-1">{isActive ? 'Ja' : 'Nee'}</div>
+                <div className="col-sm-1">
+                    {showActionButtons && permissions.manageMarketing ? (
+                        <>
+                            <a role="button" onClick={openEdit}>
+                                <Icon className="mybtn-success" size={14} icon={pencil} />
+                            </a>
+                            <a role="button" onClick={toggleDelete}>
+                                <Icon className="mybtn-danger" size={14} icon={trash} />
+                            </a>
+                        </>
+                    ) : (
+                        ''
+                    )}
+                </div>
             </div>
         </div>
     );

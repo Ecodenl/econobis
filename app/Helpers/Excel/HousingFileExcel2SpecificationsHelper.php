bd2c67ed35225ee93f046f8828a6ce47b83c4d5a
patrickkoeman <121806193+patrickkoeman@users.noreply.github.com>
2024-02-29 11:21:25 +0100

    added the columns to the excel export

--- app/Helpers/Excel/HousingFileExcel2SpecificationsHelper.php
+++ app/Helpers/Excel/HousingFileExcel2SpecificationsHelper.php
@@ -30,15 +30,20 @@ class HousingFileExcel2SpecificationsHelper
 
         $headerData = [];
 
-        $headerData[] = 'Woningspecificatie ID';
-        $headerData[] = 'Contact ID';
-        $headerData[] = 'Woningdossier ID';
+//        $headerData[] = 'Woningspecificatie ID';
+//        $headerData[] = 'Contact ID';
+//        $headerData[] = 'Woningdossier ID';
+        $headerData[] = 'Adres';
+        $headerData[] = 'Postcode';
+        $headerData[] = 'Woonplaats';
         $headerData[] = 'Maatregel categorie';
         $headerData[] = 'Maatregel';
         $headerData[] = 'Status';
         $headerData[] = 'Datum realisatie';
+        $headerData[] = 'Waarde';
         $headerData[] = 'Verdieping';
         $headerData[] = 'Zijde';
+        $headerData[] = 'Type/merk';
         $headerData[] = 'Uitvoering';
         $headerData[] = 'Besparing gas';
         $headerData[] = 'Besparing elektriciteit';
@@ -54,19 +59,22 @@ class HousingFileExcel2SpecificationsHelper
             foreach ($chunk as $housingFileSpecification) {
                 $housingFile = $housingFileSpecification->housingFile;
                 $rowData = [];
-                $rowData[0] = $housingFileSpecification->id; //'Woningspecificatie ID'
-                $rowData[1] = $housingFile->address->contact ? $housingFile->address->contact->id : ''; //'Contact ID'
-                $rowData[2] = $housingFile->id; //'Woningdossier ID'
+
+                $rowData[0] = $housingFile->address ? $housingFile->address->fullAddress : '';
+                $rowData[1] = $housingFile->address ? $housingFile->address->postal_code : '';
+                $rowData[2] = $housingFile->address ? $housingFile->address->city : '';
                 $rowData[3] = $housingFileSpecification->measure ? $housingFileSpecification->measure->name : ''; //'Maatregel categorie'
                 $rowData[4] = ($housingFileSpecification->measure && $housingFileSpecification->measure->measureCategory) ? $housingFileSpecification->measure->measureCategory->name : ''; //'Maatregel'
                 $rowData[5] = $housingFileSpecification->status ? $housingFileSpecification->status->name : ''; //'Status'
                 $rowData[6] = $housingFileSpecification->measure_date; //'Datum realisatie'
-                $rowData[7] = $housingFileSpecification->floor ? $housingFileSpecification->floor->name : ''; //'Verdieping'
-                $rowData[8] = $housingFileSpecification->side ? $housingFileSpecification->side->name : ''; //'Zijde'
-                $rowData[9] = $housingFileSpecification->type_of_execution ? $typeOfExecutionOptions[$housingFileSpecification->type_of_execution] : 'Onbekend'; //'Uitvoering'
-                $rowData[10] = $housingFileSpecification->savings_gas; //'Savings gas'
-                $rowData[11] = $housingFileSpecification->savings_electricity; //'Savings elektriciteit'
-                $rowData[12] = $housingFileSpecification->co2_savings; //'Savings Co2 '
+                $rowData[7] = $housingFileSpecification->answer;
+                $rowData[8] = $housingFileSpecification->floor ? $housingFileSpecification->floor->name : ''; //'Verdieping'
+                $rowData[9] = $housingFileSpecification->side ? $housingFileSpecification->side->name : ''; //'Zijde'
+                $rowData[10] = $housingFileSpecification->type_brand;
+                $rowData[11] = $housingFileSpecification->type_of_execution ? $typeOfExecutionOptions[$housingFileSpecification->type_of_execution] : 'Onbekend'; //'Uitvoering'
+                $rowData[12] = $housingFileSpecification->savings_gas; //'Savings gas'
+                $rowData[13] = $housingFileSpecification->savings_electricity; //'Savings elektriciteit'
+                $rowData[14] = $housingFileSpecification->co2_savings; //'Savings Co2 '
 
                 $completeData[] = $rowData;
             }

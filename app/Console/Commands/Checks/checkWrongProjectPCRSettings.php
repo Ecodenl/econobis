2e7382c1790395ec72722022419abf9d3c085f8a
patrickkoeman <121806193+patrickkoeman@users.noreply.github.com>
2024-03-12 16:48:18 +0100

    first option, this has some flawas but is most in line with how Wim wrote it donw in Trello i think

---
+++ app/Console/Commands/Checks/checkWrongProjectPCRSettings.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Console\Commands\Checks;
+
+use App\Eco\Project\Project;
+use App\Http\Resources\Email\Templates\GenericMailWithoutAttachment;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+
+class checkWrongProjectPCRSettings extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'project:checkWrongProjectPCRSettings';
+    protected $mailTo = 'patrick@xaris.nl';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Check wrong Project PCR settings';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        Log::info('Procedure check op ongeldige projectgegevens inzake PCR instellingen gestart');
+
+        $projectsWithWrongPCRSettings = [];
+
+        $projects = Project::all();
+
+        $counter = 1;
+
+        foreach($projects as $project) {
+            if ($project->project_type_id === 8 && ($project->postalcode_link == "" || !preg_match('/^[0-9a-zA-Z,]*$/', $project->postalcode_link))) {
+                //Dit is een PCR project, en de postalcode_link is leeg of niet geldig
+                $projectsWithWrongPCRSettings[$counter]['id'] = $project->id;
+                $projectsWithWrongPCRSettings[$counter]['reason'] = 'Dit is een PCR project en de postalcode_link is leeg of niet geldig';
+            } else if ($project->is_sce_project) {
+                //Dit is een SCE project
+                if($project->postalcode_link == "" || !preg_match('/^[1-9a-zA-Z,]*$/', $project->postalcode_link)) {
+                    //De postalcode_link is leeg of niet geldig
+                    $projectsWithWrongPCRSettings[$counter]['id'] = $project->id;
+                    $projectsWithWrongPCRSettings[$counter]['reason'] = 'Dit is een SCE project en de postalcode_link is leeg of niet geldig';
+                } else if(preg_match('/^[1-9]{4}[A-Za-z]{2}$/', $project->postalcode_link) || preg_match('/^[1-9]{4}$/', $project->postalcode_link)) {
+                    //De postalcode_link is een enkele postcode
+                    if($project->address_number_series == "" || !preg_match('/^[0-9a-zA-Z,:-]*$/', $project->address_number_series)) {
+                        //De address_number_series is leeg of niet geldig
+                        $projectsWithWrongPCRSettings[$counter]['id'] = $project->id;
+                        $projectsWithWrongPCRSettings[$counter]['reason'] = 'Dit is een SCE project en de postalcode_link is een enkele postcode, maar address_number_series is leeg of niet geldig';
+                    }
+                } else if($project->address_number_series != "") {
+                    //De postalcode_link is gevuld met meerdere postcodes en de address_number_series is niet leeg
+                    $projectsWithWrongPCRSettings[$counter]['id'] = $project->id;
+                    $projectsWithWrongPCRSettings[$counter]['reason'] = 'Dit is een SCE project en de postalcode_link is gevuld met meerdere postcodes, maar de address_number_series is niet leeg';
+                } else if ($project->postcode_link == "" && $project->address_number_series == "") {
+                    $projectsWithWrongPCRSettings[$counter]['id'] = $project->id;
+                    $projectsWithWrongPCRSettings[$counter]['reason'] = 'Dit is een SCE project en de postalcode_link en address_number_series zijn leeg';
+                }
+            }
+            $counter++;
+        }
+
+        if(!empty($projectsWithWrongPCRSettings)) {
+            $this->sendMail($projectsWithWrongPCRSettings);
+            Log::info('Ongeldige projectgegevens inzake PCR instellingen, mail gestuurd');
+        } else {
+            Log::info('Geen ongeldige projectgegevens inzake PCR instellingen gevonden');
+        }
+
+        Log::info('Procedure check op ongeldige projectgegevens inzake PCR instellingen klaar');
+
+    }
+
+    private function sendMail($projectsWithWrongPCRSettings)
+    {
+        $subject = 'Ongeldige projectgegevens inzake PCR instellingen! (' . count($projectsWithWrongPCRSettings) . ') - ' . \Config::get('app.APP_COOP_NAME');
+
+        $projectsWithWrongPCRSettingsHtml = "<p>De volgende project id's hebben ongeldige projectgegevens inzake PCR instellingen:</p>";
+        foreach ($projectsWithWrongPCRSettings as $projectWithWrongPCRSettings) {
+            Log::info($projectWithWrongPCRSettings['id']);
+            Log::info($projectWithWrongPCRSettings['reason']);
+            $projectsWithWrongPCRSettingsHtml .=
+                "Project Id: " . $projectWithWrongPCRSettings['id'] . "</br>" .
+                "Reden: " . $projectWithWrongPCRSettings['reason'] . "</br><br>"
+            ;
+        }
+
+        $mail = Mail::to($this->mailTo);
+        $htmlBody = '<!DOCTYPE html><html><head><meta http-equiv="content-type" content="text/html;charset=UTF-8"/><title>'.$subject.'</title></head><body><p>'. $subject . '</p>' . $projectsWithWrongPCRSettingsHtml . '</body></html>';
+
+        $mail->subject = $subject;
+        $mail->html_body = $htmlBody;
+
+        $mail->send(new GenericMailWithoutAttachment($mail, $htmlBody));
+    }
+}
+

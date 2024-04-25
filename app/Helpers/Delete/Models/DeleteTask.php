4f7fe6d1ce82ce51408698e5ff1ebb250e576b25
Mosmania <wim@mosmania.nl>
2024-04-25 14:49:59 +0200

    cleanup code

--- app/Helpers/Delete/Models/DeleteTask.php
+++ app/Helpers/Delete/Models/DeleteTask.php
@@ -59,7 +59,7 @@ class DeleteTask implements DeleteInterface
      */
     public function canDelete()
     {
-        // 22-04-2024: Verwijderen 1 voor 1 (door taak te openen) mag ook ongeacht de status van de taak
+        // 22-04-2024: Verwijderen 1 voor 1 mag ook ongeacht de status van de taak
 //        if(!$this->task->finished){
 //            array_push($this->errorMessage, "Er is nog een taak die niet is afgerond. Zet de taak op afgehandeld en verwijder dan opnieuw.");
 //        }

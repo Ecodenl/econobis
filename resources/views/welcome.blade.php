cfe2102a07a82491dad7e442ba4075d594633941
patrickkoeman <121806193+patrickkoeman@users.noreply.github.com>
2024-04-29 10:36:22 +0200

    new build files, client-app and customer-portal-app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!doctype html><html lang="{{ app()->getLocale() }}"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1"><title>Econobis</title><script defer="defer" src="./js/6928.6389a810b61e56dd4161.js"></script><script defer="defer" src="./js/bundle.8afced3f7c99def2679f.js"></script></head><body><script src="./js/polyfill.js"></script><script>// Set some global JS variables
+<!doctype html><html lang="{{ app()->getLocale() }}"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1"><title>Econobis</title><script defer="defer" src="./js/6928.2dd2fb6a8ccde506aa6f.js"></script><script defer="defer" src="./js/bundle.72ca5f706d9d158a82ca.js"></script></head><body><script src="./js/polyfill.js"></script><script>// Set some global JS variables
     {{--var URL_APP ="{{ config('app.url') }}";--}}
     var URL_API ="{{ config('app.url_api') }}";
     var CLIENT_ID ="{{ config('app.oauth_client_id') }}";

3494073f412db8d4c9ff52bf3f4f1e411877dcd5
Mosmania <wim@mosmania.nl>
2024-07-23 11:46:02 +0200

    Set version to 4.6.3.20

--- config/app.php
+++ config/app.php
@@ -5,7 +5,7 @@ return [
     //Version number displayed in the header, change this with every update
     'version_major' => '4',
     'version_minor' => '6',
-    'version_fix' => '3.19',
+    'version_fix' => '3.20',
 
     //Alfresco envs
     'ALFRESCO_ADMIN_USERNAME' => env('ALFRESCO_ADMIN_USERNAME'),

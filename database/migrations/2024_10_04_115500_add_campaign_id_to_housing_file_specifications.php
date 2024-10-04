7342c426f3fec021caa8eb38e128aec758f13afa
patrickkoeman <121806193+patrickkoeman@users.noreply.github.com>
2024-10-04 11:57:09 +0200

    added migration to add campaign_id column to housing_file_specifications table

---
+++ database/migrations/2024_10_04_115500_add_campaign_id_to_housing_file_specifications.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCampaignIdToHousingFileSpecifications extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('housing_file_specifications', function (Blueprint $table) {
+            $table->unsignedInteger('campaign_id')->after('housing_file_id')->nullable();
+            $table->foreign('campaign_id')
+                ->references('id')->on('campaigns');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('cooperations', function (Blueprint $table) {
+            $table->dropForeign(['campaign_id']);
+            $table->dropColumn('campaign_id');
+        });
+    }
+}

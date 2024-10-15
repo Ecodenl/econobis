66ce58d6011cf3efc2755148ceccdc760801678e
patrickkoeman <121806193+patrickkoeman@users.noreply.github.com>
2024-10-15 14:45:34 +0200

    migration for send_email_reminder field in campaign_workflows table

---
+++ database/migrations/2024_10_15_134000_add_send_email_reminder_to_campaign_workflows_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSendEmailReminderToCampaignWorkflowsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('campaign_workflows', function (Blueprint $table) {
+            $table->boolean('send_email_reminder')->default(0)->after('number_of_days_to_send_email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('campaign_workflows', function (Blueprint $table) {
+            $table->dropColumn('send_email_reminder');
+        });
+    }
+}

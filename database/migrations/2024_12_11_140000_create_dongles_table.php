dce83d57631a844bd3d3aea6fe5adde5a179cd2b
patrickkoeman <121806193+patrickkoeman@users.noreply.github.com>
2024-12-11 15:21:46 +0100

    added dongle table migration

---
+++ database/migrations/2024_12_11_140000_create_dongles_table.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dongles', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->unsignedInteger('address_id');
+            $table->foreign('address_id')->references('id')->on('addresses');
+
+            $table->integer('type_read_out'); #TODO hoe wil je de verschillende types bijhouden, tabel lijkt mij wat overbodig?
+            $table->string('mac_number')->nullable(); #TODO in de documentatie staat number, maar een mac adres kan ook andere karakters dan nummers hebben volgens mij?
+            $table->integer('type_dongle'); #TODO hoe wil je de verschillende types bijhouden, tabel lijkt mij wat overbodig?
+            $table->integer('energie_id');
+            $table->dateTime('date_signed');
+            $table->dateTime('date_start');
+            $table->dateTime('date_end');
+
+            $table->unsignedInteger('created_by_id');
+            $table->foreign('created_by_id')->references('id')->on('users');
+
+            $table->unsignedInteger('updated_by_id');
+            $table->foreign('updated_by_id')->references('id')->on('users');
+
+            $table->timestamps();
+
+
+
+
+
+
+            $table->dateTime('end_at')->nullable();
+            $table->string('name');
+            $table->boolean('is_active')->default(false);
+            $table->text('description')->nullable();
+            $table->string('url_page_ref');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dongles');
+    }
+};

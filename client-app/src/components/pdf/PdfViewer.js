b912272b259cd2c007147f7b6e303b6b1783d830
patrickkoeman <121806193+patrickkoeman@users.noreply.github.com>
2025-02-06 14:47:04 +0100

    upgraded react-pdf to ^7.7.3 for the client-app and customer-portal because of a vulnerability in pdf.js found when the Security Assessment was done.
    It was possible to add javascript to a PDF and execute this javascript in the econobis application

--- client-app/src/components/pdf/PdfViewer.js
+++ client-app/src/components/pdf/PdfViewer.js
@@ -1,26 +1,40 @@
-import React, {useEffect, useState} from 'react';
-import {Button} from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
+
+// oud
+// const {Document, Page} = supportsPdfViewer ? require('react-pdf/dist/esm/entry.webpack') : {};
+// pdfjs.GlobalWorkerOptions.workerSrc = '/js/pdf.worker.min.js';
+
+//nieuw
+// ChatGPT: The error occurs because recent versions of react-pdf have changed their package structure and no longer
+// export react-pdf/dist/esm/entry.webpack. Instead, you should import react-pdf directly from the main package.
+import { Document, Page, pdfjs } from 'react-pdf';
+// pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = '/js/pdf.worker.min.js';
+
+//customer portal app
+// import { Document, Page, pdfjs } from 'react-pdf/dist/esm/entry.webpack';
+// pdfjs.GlobalWorkerOptions.workerSrc = '/js/pdf.worker.min.js';
 
 /**
  * Import pdf library dynamically based on support
  */
 const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 const supportsPdfViewer = !isSafari;
-const {Document, Page} = supportsPdfViewer ? require('react-pdf/dist/esm/entry.webpack') : {};
 
 function PdfViewer(props) {
     const [numPages, setNumPages] = useState(null);
     const [pageNumber, setPageNumber] = useState(1);
     const [base64, setBase64] = useState(null);
-    const {file, scale} = props;
+    const { file, scale } = props;
 
-    function onDocumentLoadSuccess({numPages}) {
+    function onDocumentLoadSuccess({ numPages }) {
         setNumPages(numPages);
     }
 
-    const blobToBase64 = function (blob, callback) {
+    const blobToBase64 = function(blob, callback) {
         var reader = new FileReader();
-        reader.onload = function () {
+        reader.onload = function() {
             var dataUrl = reader.result;
             var base64 = dataUrl.split(',')[1];
             callback(base64);
@@ -33,7 +47,7 @@ function PdfViewer(props) {
             return;
         }
 
-        blobToBase64(file, function (base64) {
+        blobToBase64(file, function(base64) {
             setBase64(base64);
         });
     }, [file]);
@@ -41,7 +55,11 @@ function PdfViewer(props) {
     return (
         <div className="pdf_viewer_wrapper">
             {isSafari && base64 && (
-                <iframe src={'data:application/pdf;base64,' + base64} style={{height: 'calc(100vh - 500px)'}} title={'PDF viewer'}/>
+                <iframe
+                    src={'data:application/pdf;base64,' + base64}
+                    style={{ height: 'calc(100vh - 500px)' }}
+                    title={'PDF viewer'}
+                />
             )}
 
             {supportsPdfViewer && (
@@ -57,7 +75,7 @@ function PdfViewer(props) {
                     </Document>
                     <div>
                         <div></div>
-                        <h3 style={{display: 'inline-block', marginTop: '0px'}}>
+                        <h3 style={{ display: 'inline-block', marginTop: '0px' }}>
                             Pagina {pageNumber} van {numPages}
                         </h3>
                     </div>

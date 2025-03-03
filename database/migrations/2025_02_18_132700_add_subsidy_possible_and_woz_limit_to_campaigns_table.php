257e15d586a871169c8db1b9749805f0f24d2c00
Mosmania <wim@mosmania.nl>
2025-03-03 16:30:27 +0100

    Cleanup code

--- database/migrations/2025_02_18_132700_add_subsidy_possible_and_woz_limit_to_campaigns_table.php
+++ database/migrations/2025_02_18_132700_add_subsidy_possible_and_woz_limit_to_campaigns_table.php
@@ -1,12 +1,7 @@
 <?php
 
-use App\Eco\Campaign\Campaign;
-use App\Eco\Cooperation\Cooperation;
-use App\Eco\Opportunity\OpportunityStatus;
-use App\Eco\QuotationRequest\QuotationRequestStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class AddSubsidyPossibleAndWozLimitToCampaignsTable extends Migration

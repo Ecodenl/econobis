46defbaf5d808333abd3794c51ccd4e600a40470
patrickkoeman <121806193+patrickkoeman@users.noreply.github.com>
2025-06-06 13:17:56 +0200

    modal added to ask the user if they are sure

--- client-app/src/container/data-cleanup/list/DataCleanupListItems.js
+++ client-app/src/container/data-cleanup/list/DataCleanupListItems.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { trash } from 'react-icons-kit/fa/trash';
 import Icon from 'react-icons-kit';
 import DataCleanupAPI from '../../../api/data-cleanup/DataCleanupAPI';
+import Modal from '../../../components/modal/Modal';
 
 class DataCleanupListItems extends Component {
     constructor(props) {
@@ -31,6 +32,9 @@ class DataCleanupListItems extends Component {
             opportunitiesCleanupYears: '-',
             participationsWithStatusCleanupYears: '-',
             participationsFinishedCleanupYears: '-',
+
+            showModal: false,
+            modalCleanupType: null, // Track which cleanup action is triggered
         };
     }
 
@@ -71,110 +75,183 @@ class DataCleanupListItems extends Component {
         });
     }
 
-    handleInputChange = event => {
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const name = target.name;
-
+    // Open modal and set which cleanup type
+    openModal = (cleanupType) => {
         this.setState({
-            [name]: value,
+            showModal: true,
+            modalCleanupType: cleanupType,
         });
     };
 
+    // Close modal
+    closeModal = () => {
+        this.setState({
+            showModal: false,
+            modalCleanupType: null,
+        });
+    };
+
+    // Confirm action based on type
+    confirmCleanup = () => {
+        const { modalCleanupType } = this.state;
+        if (!modalCleanupType) return;
+
+        // Call the appropriate cleanup function from props
+        switch (modalCleanupType) {
+            case 'invoices':
+                this.props.cleanupInvoices();
+                break;
+            case 'orders':
+                this.props.cleanupOrders();
+                break;
+            case 'intakes':
+                this.props.cleanupIntakes();
+                break;
+            case 'opportunities':
+                this.props.cleanupOpportunities();
+                break;
+            case 'participationsWithStatus':
+                this.props.cleanupParticipationsWithStatus();
+                break;
+            case 'participationsFinished':
+                this.props.cleanupParticipationsFinished();
+                break;
+            default:
+                break;
+        }
+        this.closeModal();
+    };
+
     render() {
+        const { showModal, modalCleanupType } = this.state;
+
+        // Map cleanupType to readable label for modal
+        const cleanupLabels = {
+            invoices: "Nota's",
+            orders: "Orders",
+            intakes: "Intakes",
+            opportunities: "Kansen",
+            participationsWithStatus: "Deelnames met status interesse, ingeschreven of toegekend",
+            participationsFinished: "Deelnames die zijn beëindigd",
+        };
+
+        const cleanupYears = {
+            invoices: this.state.invoicesCleanupYears,
+            orders: this.state.ordersOneoffCleanupYears,
+            intakes: this.state.intakesCleanupYears,
+            opportunities: this.state.opportunitiesCleanupYears,
+            participationsWithStatus: this.state.participationsWithStatusCleanupYears,
+            participationsFinished: this.state.participationsFinishedCleanupYears,
+        };
+
         return (
-            <table className="table">
-                <thead>
+            <div>
+                {showModal && (
+                    <Modal
+                        show={showModal}
+                        closeModal={this.closeModal}
+                        confirmAction={this.confirmCleanup}
+                        buttonConfirmText="Opschonen"
+                        buttonClassName={'btn-danger'}
+                        title={"Bevestig opschonen " + cleanupLabels[modalCleanupType].toLowerCase()}
+                    >
+                        {modalCleanupType ? (
+                            <div>
+                                Weet u zeker dat u <strong>{cleanupLabels[modalCleanupType].toLowerCase()}</strong> en <strong>ouder zijn dan {cleanupYears[modalCleanupType]} jaar</strong> wilt opschonen?<br/>
+                                <br/>
+                                Deze verwijderactie is niet terug te draaien.
+                            </div>
+                        ) : null}
+                    </Modal>
+                )}
+
+                <table className="table">
+                    <thead>
                     <tr>
                         <th className="col-sm-2"></th>
                         <th className="col-sm-4">Onderdeel</th>
                         <th className="col-sm-1">Items</th>
                         <th className="col-sm-2">Acties</th>
-
                         <th className="col-sm-1">Laatst opgeschoond</th>
                         <th className="col-sm-2"></th>
                     </tr>
-                </thead>
-                <tbody>
+                    </thead>
+                    <tbody>
                     <tr>
                         <td className="col-sm-2"></td>
-                        <td className="col-sm-4">Nota's ouder dan {this.state.invoicesCleanupYears} jaar</td>
+                        <td className="col-sm-4">Nota's ouder dan {cleanupYears['invoices']} jaar</td>
                         <td className="col-sm-1">{this.state.amountOfInvoicesToCleanup}</td>
                         <td className="col-sm-2">
-                            <a role="button" onClick={() => props.cleanupInvoices()}>
+                            <a role="button" onClick={() => this.openModal('invoices')}>
                                 <Icon size={14} icon={trash} />
                             </a>
                         </td>
                         <td className="col-sm-1">{this.state.invoicesLastCleanupDate}</td>
                         <td className="col-sm-2"></td>
                     </tr>
-
                     <tr>
                         <td className="col-sm-2"></td>
-                        <td className="col-sm-4">Orders ouder dan {this.state.ordersOneoffCleanupYears} jaar</td>
+                        <td className="col-sm-4">Orders ouder dan {cleanupYears['orders']} jaar</td>
                         <td className="col-sm-1">{this.state.amountOfOrdersToCleanup}</td>
                         <td className="col-sm-2">
-                            <a role="button" onClick={() => props.cleanupOrders()}>
+                            <a role="button" onClick={() => this.openModal('orders')}>
                                 <Icon size={14} icon={trash} />
                             </a>
                         </td>
                         <td className="col-sm-1">{this.state.ordersOneoffLastCleanupDate}</td>
                         <td className="col-sm-2"></td>
                     </tr>
-
                     <tr>
                         <td className="col-sm-2"></td>
-                        <td className="col-sm-4">Intakes ouder dan {this.state.intakesCleanupYears} jaar</td>
+                        <td className="col-sm-4">Intakes ouder dan {cleanupYears['intakes']} jaar</td>
                         <td className="col-sm-1">{this.state.amountOfIntakesToCleanup}</td>
                         <td className="col-sm-2">
-                            <a role="button" onClick={() => props.cleanupIntakes()}>
+                            <a role="button" onClick={() => this.openModal('intakes')}>
                                 <Icon size={14} icon={trash} />
                             </a>
                         </td>
                         <td className="col-sm-1">{this.state.intakesLastCleanupDate}</td>
                         <td className="col-sm-2"></td>
                     </tr>
-
                     <tr>
                         <td className="col-sm-2"></td>
-                        <td className="col-sm-4">Kansen ouder dan {this.state.opportunitiesCleanupYears} jaar</td>
+                        <td className="col-sm-4">Kansen ouder dan {cleanupYears['opportunities']} jaar</td>
                         <td className="col-sm-1">{this.state.amountOfOpportunitiesToCleanup}</td>
                         <td className="col-sm-2">
-                            <a role="button" onClick={() => props.cleanupOpportunities()}>
+                            <a role="button" onClick={() => this.openModal('opportunities')}>
                                 <Icon size={14} icon={trash} />
                             </a>
                         </td>
                         <td className="col-sm-1">{this.state.opportunitiesLastCleanupDate}</td>
                         <td className="col-sm-2"></td>
                     </tr>
-
                     <tr>
                         <td className="col-sm-2"></td>
-                        <td className="col-sm-4">Deelnames met status interesse, ingeschreven of toegekend ouder dan {this.state.participationsWithStatusCleanupYears} jaar</td>
-                        <td className="col-sm-1">{this.state.participationsWithStatusCleanupYears}</td>
+                        <td className="col-sm-4">Deelnames met status interesse, ingeschreven of toegekend ouder dan {cleanupYears['participationsWithStatus']} jaar</td>
+                        <td className="col-sm-1">{this.state.amountOfParticipationsWithStatusToCleanup}</td>
                         <td className="col-sm-2">
-                            <a role="button" onClick={() => props.cleanupParticipationsWithStatus()}>
+                            <a role="button" onClick={() => this.openModal('participationsWithStatus')}>
                                 <Icon size={14} icon={trash} />
                             </a>
                         </td>
                         <td className="col-sm-1">{this.state.participationsWithStatusLastCleanupDate}</td>
                         <td className="col-sm-2"></td>
                     </tr>
-
                     <tr>
                         <td className="col-sm-2"></td>
-                        <td className="col-sm-4">Deelnames die zijn beëindigd ouder dan {this.state.participationsFinishedCleanupYears} jaar</td>
+                        <td className="col-sm-4">Deelnames die zijn beëindigd ouder dan {cleanupYears['participationsFinished']} jaar</td>
                         <td className="col-sm-1">{this.state.amountOfParticipationsFinishedToCleanup}</td>
                         <td className="col-sm-2">
-                            <a role="button" onClick={() => props.cleanupParticipationsFinished()}>
+                            <a role="button" onClick={() => this.openModal('participationsFinished')}>
                                 <Icon size={14} icon={trash} />
                             </a>
                         </td>
                         <td className="col-sm-1">{this.state.participationsFinishedLastCleanupDate}</td>
                         <td className="col-sm-2"></td>
                     </tr>
-                </tbody>
-            </table>
+                    </tbody>
+                </table>
+            </div>
         );
     }
 }

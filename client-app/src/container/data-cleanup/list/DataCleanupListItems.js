63ac9c0aa158b6303575d6230d5b4e27238e1e39
patrickkoeman <121806193+patrickkoeman@users.noreply.github.com>
2025-06-16 09:57:46 +0200

    indenting fix

--- client-app/src/container/data-cleanup/list/DataCleanupListItems.js
+++ client-app/src/container/data-cleanup/list/DataCleanupListItems.js
@@ -172,88 +172,88 @@ class DataCleanupListItems extends Component {
                 )}
                 <table className="table">
                     <thead>
-                    <tr>
-                        <th className="col-sm-2"></th>
-                        <th className="col-sm-4">Onderdeel</th>
-                        <th className="col-sm-1">Items</th>
-                        <th className="col-sm-1">Acties</th>
-                        <th className="col-sm-2">Laatst opgeschoond</th>
-                        <th className="col-sm-2"></th>
-                    </tr>
+                        <tr>
+                            <th className="col-sm-2"></th>
+                            <th className="col-sm-4">Onderdeel</th>
+                            <th className="col-sm-1">Items</th>
+                            <th className="col-sm-1">Acties</th>
+                            <th className="col-sm-2">Laatst opgeschoond</th>
+                            <th className="col-sm-2"></th>
+                        </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                        <td className="col-sm-2"></td>
-                        <td className="col-sm-4">Nota's ouder dan {cleanupYears['invoices']} jaar</td>
-                        <td className="col-sm-1">{this.state.amountOfInvoicesToCleanup}</td>
-                        <td className="col-sm-1">
-                            <a role="button" onClick={() => this.openModal('invoices')}>
-                                <Icon size={14} icon={trash} />
-                            </a>
-                        </td>
-                        <td className="col-sm-2">{this.state.invoicesLastCleanupDate}</td>
-                        <td className="col-sm-2"></td>
-                    </tr>
-                    <tr>
-                        <td className="col-sm-2"></td>
-                        <td className="col-sm-4">Orders ouder dan {cleanupYears['orders']} jaar</td>
-                        <td className="col-sm-1">{this.state.amountOfOrdersToCleanup}</td>
-                        <td className="col-sm-1">
-                            {/*<a role="button" onClick={() => this.openModal('orders')}>*/}
-                                <Icon size={14} icon={trash} />
-                            {/*</a>*/}
-                        </td>
-                        <td className="col-sm-2">{this.state.ordersOneoffLastCleanupDate}</td>
-                        <td className="col-sm-2"></td>
-                    </tr>
-                    <tr>
-                        <td className="col-sm-2"></td>
-                        <td className="col-sm-4">Intakes ouder dan {cleanupYears['intakes']} jaar</td>
-                        <td className="col-sm-1">{this.state.amountOfIntakesToCleanup}</td>
-                        <td className="col-sm-1">
-                            <a role="button" onClick={() => this.openModal('intakes')}>
-                                <Icon size={14} icon={trash} />
-                            </a>
-                        </td>
-                        <td className="col-sm-2">{this.state.intakesLastCleanupDate}</td>
-                        <td className="col-sm-2"></td>
-                    </tr>
-                    <tr>
-                        <td className="col-sm-2"></td>
-                        <td className="col-sm-4">Kansen ouder dan {cleanupYears['opportunities']} jaar</td>
-                        <td className="col-sm-1">{this.state.amountOfOpportunitiesToCleanup}</td>
-                        <td className="col-sm-1">
-                            <a role="button" onClick={() => this.openModal('opportunities')}>
-                                <Icon size={14} icon={trash} />
-                            </a>
-                        </td>
-                        <td className="col-sm-2">{this.state.opportunitiesLastCleanupDate}</td>
-                        <td className="col-sm-2"></td>
-                    </tr>
-                    <tr>
-                        <td className="col-sm-2"></td>
-                        <td className="col-sm-4">Deelnames met status interesse, ingeschreven of toegekend ouder dan {cleanupYears['participationsWithStatus']} jaar</td>
-                        <td className="col-sm-1">{this.state.amountOfParticipationsWithStatusToCleanup}</td>
-                        <td className="col-sm-1">
-                            {/*<a role="button" onClick={() => this.openModal('participationsWithStatus')}>*/}
-                                <Icon size={14} icon={trash} />
-                            {/*</a>*/}
-                        </td>
-                        <td className="col-sm-2">{this.state.participationsWithStatusLastCleanupDate}</td>
-                        <td className="col-sm-2"></td>
-                    </tr>
-                    <tr>
-                        <td className="col-sm-2"></td>
-                        <td className="col-sm-4">Deelnames die zijn beëindigd ouder dan {cleanupYears['participationsFinished']} jaar</td>
-                        <td className="col-sm-1">{this.state.amountOfParticipationsFinishedToCleanup}</td>
-                        <td className="col-sm-1">
-                            <a role="button" onClick={() => this.openModal('participationsFinished')}>
-                                <Icon size={14} icon={trash} />
-                            </a>
-                        </td>
-                        <td className="col-sm-2">{this.state.participationsFinishedLastCleanupDate}</td>
-                        <td className="col-sm-2"></td>
-                    </tr>
+                        <tr>
+                            <td className="col-sm-2"></td>
+                            <td className="col-sm-4">Nota's ouder dan {cleanupYears['invoices']} jaar</td>
+                            <td className="col-sm-1">{this.state.amountOfInvoicesToCleanup}</td>
+                            <td className="col-sm-1">
+                                <a role="button" onClick={() => this.openModal('invoices')}>
+                                    <Icon size={14} icon={trash} />
+                                </a>
+                            </td>
+                            <td className="col-sm-2">{this.state.invoicesLastCleanupDate}</td>
+                            <td className="col-sm-2"></td>
+                        </tr>
+                        <tr>
+                            <td className="col-sm-2"></td>
+                            <td className="col-sm-4">Orders ouder dan {cleanupYears['orders']} jaar</td>
+                            <td className="col-sm-1">{this.state.amountOfOrdersToCleanup}</td>
+                            <td className="col-sm-1">
+                                {/*<a role="button" onClick={() => this.openModal('orders')}>*/}
+                                    <Icon size={14} icon={trash} />
+                                {/*</a>*/}
+                            </td>
+                            <td className="col-sm-2">{this.state.ordersOneoffLastCleanupDate}</td>
+                            <td className="col-sm-2"></td>
+                        </tr>
+                        <tr>
+                            <td className="col-sm-2"></td>
+                            <td className="col-sm-4">Intakes ouder dan {cleanupYears['intakes']} jaar</td>
+                            <td className="col-sm-1">{this.state.amountOfIntakesToCleanup}</td>
+                            <td className="col-sm-1">
+                                <a role="button" onClick={() => this.openModal('intakes')}>
+                                    <Icon size={14} icon={trash} />
+                                </a>
+                            </td>
+                            <td className="col-sm-2">{this.state.intakesLastCleanupDate}</td>
+                            <td className="col-sm-2"></td>
+                        </tr>
+                        <tr>
+                            <td className="col-sm-2"></td>
+                            <td className="col-sm-4">Kansen ouder dan {cleanupYears['opportunities']} jaar</td>
+                            <td className="col-sm-1">{this.state.amountOfOpportunitiesToCleanup}</td>
+                            <td className="col-sm-1">
+                                <a role="button" onClick={() => this.openModal('opportunities')}>
+                                    <Icon size={14} icon={trash} />
+                                </a>
+                            </td>
+                            <td className="col-sm-2">{this.state.opportunitiesLastCleanupDate}</td>
+                            <td className="col-sm-2"></td>
+                        </tr>
+                        <tr>
+                            <td className="col-sm-2"></td>
+                            <td className="col-sm-4">Deelnames met status interesse, ingeschreven of toegekend ouder dan {cleanupYears['participationsWithStatus']} jaar</td>
+                            <td className="col-sm-1">{this.state.amountOfParticipationsWithStatusToCleanup}</td>
+                            <td className="col-sm-1">
+                                {/*<a role="button" onClick={() => this.openModal('participationsWithStatus')}>*/}
+                                    <Icon size={14} icon={trash} />
+                                {/*</a>*/}
+                            </td>
+                            <td className="col-sm-2">{this.state.participationsWithStatusLastCleanupDate}</td>
+                            <td className="col-sm-2"></td>
+                        </tr>
+                        <tr>
+                            <td className="col-sm-2"></td>
+                            <td className="col-sm-4">Deelnames die zijn beëindigd ouder dan {cleanupYears['participationsFinished']} jaar</td>
+                            <td className="col-sm-1">{this.state.amountOfParticipationsFinishedToCleanup}</td>
+                            <td className="col-sm-1">
+                                <a role="button" onClick={() => this.openModal('participationsFinished')}>
+                                    <Icon size={14} icon={trash} />
+                                </a>
+                            </td>
+                            <td className="col-sm-2">{this.state.participationsFinishedLastCleanupDate}</td>
+                            <td className="col-sm-2"></td>
+                        </tr>
                     </tbody>
                 </table>
             </div>

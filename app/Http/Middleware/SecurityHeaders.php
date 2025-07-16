5f8e4be6e1137b045ba4eb4b68f6e77e44fd2c7c
Mosmania <wim@mosmania.nl>
2025-07-16 12:09:35 +0200

    Some CSP finetuning.

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -18,7 +18,9 @@ class SecurityHeaders
     {
         $response = $next($request);
 
-        $response->headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:; object-src 'none'; frame-ancestors 'none';");
+//        $response->headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self';                 style-src 'self' 'unsafe-inline';                                                                               img-src 'self' data:; font-src 'self' data:; object-src 'none'; frame-ancestors 'none';");
+        $response->headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self';                 style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' data: https://fonts.gstatic.com; img-src 'self' data:;                        object-src 'none'; frame-ancestors 'none';");
+//        $response->headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';                                                                               img-src 'self' data:; font-src 'self' data:; object-src 'none'; frame-ancestors 'none';");
         $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
         $response->headers->set('X-Content-Type-Options', 'nosniff');
         $response->headers->set('X-Frame-Options', 'DENY');

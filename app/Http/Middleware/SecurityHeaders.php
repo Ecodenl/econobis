a40ef536e15c590a2519af917175a1145dc2eeb6
Mosmania <wim@mosmania.nl>
2025-07-31 14:53:39 +0200

    Cleanup code

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -25,7 +25,7 @@ class SecurityHeaders
         $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
         $response->headers->set('X-Content-Type-Options', 'nosniff');
         $response->headers->set('X-Frame-Options', 'DENY');
-        $response->headers->set('X-XSS-Protection', '1; mode=block');
+//        $response->headers->set('X-XSS-Protection', '1; mode=block');
         $response->headers->set('Referrer-Policy', 'no-referrer-when-downgrade');
         $response->headers->set('Permissions-Policy', 'geolocation=(), camera=(), microphone=()');
 
--- client-app/src/welcome.blade.php
+++ client-app/src/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="backend-url" content="{{ config('app.url') }}">
 
-    <title>Econobis test security</title>
+    <title>Econobis</title>
 
 </head>
 <body>

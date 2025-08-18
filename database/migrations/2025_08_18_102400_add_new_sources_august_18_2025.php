ba6739c5bf0b89ba8277e69849dadf88ac38d294
patrickkoeman <121806193+patrickkoeman@users.noreply.github.com>
2025-08-18 10:41:26 +0200

    added migration for 20 extra sources

---
+++ database/migrations/2025_08_18_102400_add_new_sources_august_18_2025.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class AddNewSourcesAugust182025 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $intakeSources = [
+            'Aanmeldingsbron15',
+            'Aanmeldingsbron16',
+            'Aanmeldingsbron17',
+            'Aanmeldingsbron18',
+            'Aanmeldingsbron19',
+            'Aanmeldingsbron20',
+            'Aanmeldingsbron21',
+            'Aanmeldingsbron22',
+            'Aanmeldingsbron23',
+            'Aanmeldingsbron24',
+            'Aanmeldingsbron25',
+            'Aanmeldingsbron26',
+            'Aanmeldingsbron27',
+            'Aanmeldingsbron28',
+            'Aanmeldingsbron29',
+            'Aanmeldingsbron30',
+            'Aanmeldingsbron31',
+            'Aanmeldingsbron32',
+            'Aanmeldingsbron33',
+            'Aanmeldingsbron34'
+        ];
+
+        foreach ($intakeSources as $source) {
+            DB::table('sources')->insert([
+                    ['name' => $source],
+                ]
+            );
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}

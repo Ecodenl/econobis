707dc85481573118dc0eb9726a58e6364b78fad4
patrickkoeman <121806193+patrickkoeman@users.noreply.github.com>
2025-08-19 15:36:51 +0200

    added check so only coaches can see the availability calendar.

--- customer-portal-app/src/container/availability/index.js
+++ customer-portal-app/src/container/availability/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ContactAvailabilityDetailsPlanningPanel from './ContactAvailabilityDetailsPlanningPanel';
+import { PortalUserConsumer } from '../../context/PortalUserContext';
 
-export default function AvailabilityDetails() {
+function AvailabilityDetails({ user }) {
     return (
         <div className={'content-section'}>
             <div className="content-container w-container">
-                <ContactAvailabilityDetailsPlanningPanel />
+                {user.inspectionPersonTypeId === 'coach' ? (
+                    <ContactAvailabilityDetailsPlanningPanel />
+                ) : null}
             </div>
         </div>
     );
+}
+
+export default function AvailabilityDetailsWithContext(props) {
+    return <PortalUserConsumer>{({ user }) => <AvailabilityDetails user={user} />}</PortalUserConsumer>;
 }

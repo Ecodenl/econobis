28ddaede2810d61a4ea56c80de388a518ca98136
patrickkoeman <121806193+patrickkoeman@users.noreply.github.com>
2025-08-22 16:14:07 +0200

    moved the menu item to "Energiebesparing" between "Campagnes" and "Maatregelen" on request of Wim

--- client-app/src/components/navigationSidebar/SidebarMenu.js
+++ client-app/src/components/navigationSidebar/SidebarMenu.js
@@ -248,6 +248,13 @@ const SidebarMenu = ({ permissions, administrations, mailboxesInvalid, useDongle
                             </NavText>
                         </Nav>
                     )}
+                    <Nav id="aanmeldingsbronnen">
+                        <NavText>
+                            <Link className="sidebar-link" to="aanmeldingsbronnen">
+                                Aanmeldingsbronnen
+                            </Link>
+                        </NavText>
+                    </Nav>
                     {permissions.menuMeasures && (
                         <Nav id="measures">
                             <NavText>
@@ -558,13 +565,6 @@ const SidebarMenu = ({ permissions, administrations, mailboxesInvalid, useDongle
                             </NavText>
                         </Nav>
                     )}
-                    <Nav id="aanmeldingsbronnen">
-                        <NavText>
-                            <Link className="sidebar-link" to="aanmeldingsbronnen">
-                                Aanmeldingsbronnen
-                            </Link>
-                        </NavText>
-                    </Nav>
                     {permissions.manageFinancial && (
                         <Nav id="products">
                             <NavText>

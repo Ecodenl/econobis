7682b71a2e15abea0302114c45a774bd55b533ee
patrickkoeman <121806193+patrickkoeman@users.noreply.github.com>
2025-10-01 12:02:38 +0200

    new measure category with measures

---
+++ database/migrations/2025_10_01_113800_seed_oktober_2025_measures_and_measure_categories_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class SeedOktober2025MeasuresAndMeasureCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $categorizedMeasures = [
+            'Energie aansluiting' => [
+                'Afleverset van warmtenet',
+                'Aardgasmeter/-afsluiting',
+                'Afleverset',
+                'Elektriciteitsmeter',
+                'Groepenkast aansluiting (1_35, 3_25, 1/3-fasen)',
+                'Groepen, beveiligingen',
+                'Bedrading (in woning)',
+            ],
+        ];
+
+        $id = 5;
+        foreach ($categorizedMeasures as $category => $measures) {
+            // Create the category
+            $catId = DB::table('measure_categories')->insertGetId(
+                [ 'name' => $category ]
+            );
+
+            // Create the measures
+            foreach($measures as $measure) {
+                DB::table('measures')->insertGetId(
+                    ['name'=> $measure,
+                        'measure_category_id' => $catId,
+                        'number' => 'M2025-' . $id ]
+                );
+                $id++;
+            }
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}

8e3ef930ef66a8a92caf1d8a6545ed0715d84b35
Mosmania <wim@mosmania.nl>
2026-02-13 17:15:55 +0100

    Change tooltip text currentBookWorth.

--- client-app/src/container/participant-project/details/form/edit/ParticipantFormEditCapital.js
+++ client-app/src/container/participant-project/details/form/edit/ParticipantFormEditCapital.js
@@ -35,7 +35,12 @@ function ParticipantFormEditCapital({
                     label={'Huidige boekwaarde per participatie'}
                     value={moneyPresenter(currentBookWorth)}
                     className={'col-sm-6 form-group'}
-                    textToolTip={'Elk jaar verdelen we de totale opgewekte kWh over de deelnemers. O.b.v. de vastgestelde teruggave energiebelasting € / kWh berekenen we de indicatie teruggave energiebelasting per deelnemer (euro per kWh x opgewekte kWh per deelnemer). De totale indicatie teruggave energiebelasting is het totaal per deelnemer van alle bedragen indicatie teruggave energiebelasting opgeteld over de jaren heen.'}
+                    textToolTip={
+                        // origineel         // 'De huidige boekwaarde per participatie is een administratieve boekwaarde van een deelname, die afhankelijk is van de waarde van het project en de gemaakte kosten en wordt vastgesteld o.b.v. de jaarrekening van de coöperatie. De boekwaarde per 1 januari van een jaar gebruik je bij je aangifte inkomstenbelasting.'
+                        'De huidige boekwaarde per participatie is een administratieve boekwaarde van een deelname, die afhankelijk is van de waarde van het project en de gemaakte kosten en wordt vastgesteld op basis van de jaarrekening van de coöperatie. Voor de waardestaat wordt de boekwaarde gehanteerd zoals die geldt op 1 januari om 00:00 uur (dit is de waarde aan het einde van 31 december).'
+                        // of variant        // 'De huidige boekwaarde per participatie is een administratieve boekwaarde van een deelname, gebaseerd op de waarde van het project en de gemaakte kosten zoals vastgesteld in de jaarrekening van de coöperatie. Voor de waardestaat en de aangifte inkomstenbelasting geldt de peildatum van 1 januari om 00:00 uur. Dit komt overeen met de boekwaarde zoals die op 31 december aan het einde van de dag gold.'
+                        // of variant        // 'Voor je belastingaangifte telt de boekwaarde zoals die geldt op 1 januari. Dat is de waarde die aan het einde van het voorgaande jaar (31 december) van toepassing was.'
+                    }
                 />
             </div>
             <div className="row">

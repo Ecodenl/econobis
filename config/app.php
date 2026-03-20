d565d759d25c569dc3a7486b973ed220549c9d57
Mosmania <wim@mosmania.nl>
2026-03-20 11:26:56 +0100

    removed duplicate provider FilesystemServiceProvider

--- config/app.php
+++ config/app.php
@@ -205,7 +205,6 @@ return [
         Illuminate\Database\DatabaseServiceProvider::class,
         Illuminate\Encryption\EncryptionServiceProvider::class,
         Illuminate\Filesystem\FilesystemServiceProvider::class,
-        Illuminate\Filesystem\FilesystemServiceProvider::class,
         Illuminate\Foundation\Providers\FoundationServiceProvider::class,
         Illuminate\Hashing\HashServiceProvider::class,
 //        Illuminate\Mail\MailServiceProvider::class,

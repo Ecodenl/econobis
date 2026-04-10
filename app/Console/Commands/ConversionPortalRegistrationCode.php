b7757209f71d527f687008027c435398e0cd8b77
Mosmania <wim@mosmania.nl>
2026-04-10 11:11:28 +0200

    Start all class and file names with a Capital.

---
+++ app/Console/Commands/ConversionPortalRegistrationCode.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Eco\Contact\Contact;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
+
+class ConversionPortalRegistrationCode extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'contact:conversionPortalRegistrationCode';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Herstel ontbrekende portal registration codes';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->doConversionPortalRegistrationCode();
+        Log::info('Einde Herstel ontbrekende portal registration codes.');
+    }
+
+    /**
+     * @param string $searchField1
+     * @param string $searchField2
+     *
+     * @return array
+     */
+    public function doConversionPortalRegistrationCode()
+    {
+        Log::info("Start Herstel ontbrekende portal registration codes.");
+        $contacts = Contact::all();
+        foreach ($contacts as $contact){
+            if($contact->type_id == 'person' && !$contact->portal_registration_code && $contact->portalUser()->count() == 0)
+            {
+                $contact->portal_registration_code = Str::random(32);
+                $contact->save();
+                Log::info("Contact ". $contact->id . " voorzien van portal registration code.");
+            }
+        }
+    }
+}
---
+++ app/Console/Commands/SetIsCurrentSupplier.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Eco\AddressEnergySupplier\AddressEnergySupplier;
+use App\Eco\Schedule\CommandRun;
+use App\Http\Controllers\Api\AddressEnergySupplier\AddressEnergySupplierController;
+use Illuminate\Console\Command;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
+
+class SetIsCurrentSupplier extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'addressEnergySupplier:setIsCurrentSupplier';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Bepaal of energieleveranciers huidig zijn geworden';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $commandRun = new CommandRun();
+        $commandRun->app_cooperation_name = config('app.APP_COOP_NAME');
+        $commandRun->schedule_run_id = config('app.SCHEDULE_RUN_ID');
+        $commandRun->scheduled_commands_command_ref = $this->signature;
+        $commandRun->start_at = Carbon::now();
+        $commandRun->end_at = null;
+        $commandRun->finished = false;
+        $commandRun->created_in_shared = false;
+        $commandRun->save();
+
+        $addressEnergySupplierController = new AddressEnergySupplierController();
+
+        $addressEnergySuppliers = AddressEnergySupplier::all();
+        foreach ($addressEnergySuppliers as $addressEnergySupplier){
+            $addressEnergySupplierController->determineIsCurrentSupplier($addressEnergySupplier);
+        }
+
+        $commandRun->end_at = Carbon::now();
+        $commandRun->finished = true;
+        $commandRun->save();
+
+        Log::info('Procedure of energieleveranciers huidig zijn geworden klaar');
+    }
+}
